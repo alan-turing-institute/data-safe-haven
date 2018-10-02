c6462163730596ef545930eed3baba6324c5569a
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2018-10-02 16:24:32 +0000

    Create basic Django app

---
+++ webapp/haven/config/settings/dev.py
@@ -0,0 +1,10 @@
+from .base import *  # noqa
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+# Log all emails to console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Don't make life difficult for ourselves with password restrictions on dev
+AUTH_PASSWORD_VALIDATORS = []
