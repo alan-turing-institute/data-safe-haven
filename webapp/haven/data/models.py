1e27e43bae242747d26c0e08c9aabccaf1f89861
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2018-10-10 08:50:09 +0000

    Add tiers and corresponding field on dataset

--- webapp/haven/data/models.py
+++ webapp/haven/data/models.py
@@ -2,8 +2,15 @@ from datetime import datetime
 
 from django.db import models
 
+from .tiers import Tier, TIERS
+
 
 class Dataset(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
+
+    tier = models.PositiveSmallIntegerField(
+        default=Tier.THREE,
+        choices=TIERS,
+    )
---
+++ webapp/haven/data/tiers.py
@@ -0,0 +1,24 @@
+
+class Tier:
+    # Tier zero: public
+    ZERO = 0
+
+    # Tier 1: publishable
+    ONE = 1
+
+    # Tier 2: official
+    TWO = 2
+
+    # Tier 3: sensitive
+    THREE = 3
+
+    # Tier 4 is secret (Out of scope)
+
+
+TIERS = [
+    (Tier.ZERO, 'Tier 0'),
+    (Tier.ONE, 'Tier 1'),
+    (Tier.TWO, 'Tier 2'),
+    (Tier.THREE, 'Tier 3'),
+]
+
