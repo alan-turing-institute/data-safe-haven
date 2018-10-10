16bd9acb4b9425dd116883f1feb7505ed420d137
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2018-10-10 16:26:45 +0000

    Add simple Bootstrap template as a holding page

--- webapp/haven/config/settings/base.py
+++ webapp/haven/config/settings/base.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.path('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
