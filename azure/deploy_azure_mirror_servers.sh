3568e8621b6af14260424336bf90a1f8aec18d68
James Robinson <james.em.robinson@gmail.com>
2018-11-27 11:53:41 +0000

    Working mirror setup. Still needs appropriate network rules and cron entries.

--- azure/deploy_azure_mirror_servers.sh
+++ azure/deploy_azure_mirror_servers.sh
@@ -11,6 +11,7 @@ RESOURCEGROUP="DataSafeHavenMirrors"
 VAULTNAME="safehavenkeys"
 SOURCEIMAGE="Canonical:UbuntuServer:18.04-LTS:latest"
 
+
 # Switch subscription and setup resource group if it does not already exist
 az account set --subscription "$SUBSCRIPTION"
 if [ $(az group exists --name $RESOURCEGROUP) != "true" ]; then
@@ -35,33 +36,33 @@ if [ "$(az keyvault certificate list --vault-name $VAULTNAME | grep "key-CRAN")"
 fi
 
 
-# Set up PyPI mirror
-# ------------------
-VMNAME="ExternalPyPI"
-INITSCRIPT="cloud-init-pypi.yaml"
+# # Set up PyPI mirror
+# # ------------------
+# VMNAME="ExternalMirrorPyPI"
+# INITSCRIPT="cloud-init-pypi.yaml"
 
-# Create the VM based off the selected source image
-echo -e "${RED}Creating VM ${BLUE}$VMNAME${RED}as part of ${BLUE}$RESOURCEGROUP${END}"
-echo -e "${RED}This will be based off the ${BLUE}$SOURCEIMAGE${RED} image${END}"
-KEY_PYPI=$(az keyvault secret list-versions --vault-name $VAULTNAME -n key-PyPI --query "[?attributes.enabled].id" -o tsv)
-VM_SECRET=$(az vm secret format -s "$KEY_PYPI")
+# # Create the VM based off the selected source image
+# echo -e "${RED}Creating VM ${BLUE}$VMNAME${RED}as part of ${BLUE}$RESOURCEGROUP${END}"
+# echo -e "${RED}This will be based off the ${BLUE}$SOURCEIMAGE${RED} image${END}"
+# KEY_PYPI=$(az keyvault secret list-versions --vault-name $VAULTNAME -n key-PyPI --query "[?attributes.enabled].id" -o tsv)
+# VM_SECRET=$(az vm secret format -s "$KEY_PYPI")
 
-az vm create \
-    --resource-group $RESOURCEGROUP \
-    --name $VMNAME \
-    --image $SOURCEIMAGE \
-    --custom-data $INITSCRIPT \
-    --size Standard_F4s_v2 \
-    --admin-username adminpypi \
-    --data-disk-sizes-gb 4095 \
-    --storage-sku Standard_LRS \
-    --secrets "$VM_SECRET"
-echo -e "${RED}Deployed new ${BLUE}$VMNAME${RED} server"
+# az vm create \
+#     --resource-group $RESOURCEGROUP \
+#     --name $VMNAME \
+#     --image $SOURCEIMAGE \
+#     --custom-data $INITSCRIPT \
+#     --size Standard_F4s_v2 \
+#     --admin-username adminpypi \
+#     --data-disk-sizes-gb 4095 \
+#     --storage-sku Standard_LRS \
+#     --secrets "$VM_SECRET"
+# echo -e "${RED}Deployed new ${BLUE}$VMNAME${RED} server"
 
 
 # Set up CRAN mirror
 # ------------------
-VMNAME="ExternalCRAN"
+VMNAME="ExternalMirrorCRAN"
 INITSCRIPT="cloud-init-cran.yaml"
 
 # Create the VM based off the selected source image
@@ -81,31 +82,3 @@ az vm create \
     --storage-sku Standard_LRS \
     --secrets "$VM_SECRET"
 echo -e "${RED}Deployed new ${BLUE}$VMNAME${RED} server"
-
-
-
-
-
-
-
-
-# Generate SSH keys
-# ssh-keygen -t rsa -b 4096 -f key-CRAN
-# ssh-keygen -t rsa -b 4096 -f key-PyPI
-
-# Upload key to vault
-# az keyvault secret set --name key-CRAN --vault-name $VAULTNAME --file key-CRAN
-# az keyvault secret set --name key-PyPI --vault-name $VAULTNAME --file key-PyPI
-
-# Download the private key
-# az keyvault secret download --name key-CRAN --vault-name $VAULTNAME --file key-CRAN
-# az keyvault secret download --name key-PyPI --vault-name $VAULTNAME --file key-PyPI
-
-# # Retrieve secrets from the keyvault
-# if [ ! -f key-PyPI.pub ]; then
-#     az keyvault secret download --vault-name $VAULTNAME -n key-PyPI -e base64 -f key-PyPI.pfx
-#     openssl pkcs12 -in key-PyPI.pfx -out key-PyPI.pem -nocerts -nodes
-#     chmod 0400 key-PyPI.pem
-#     ssh-keygen -f key-PyPI.pem -y > key-PyPI.pub
-#     # rm -f key-PyPI.pfx key-PyPI.pem
-# fi
