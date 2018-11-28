177757a9e432e291bbc5dee43d7f8d2a507c005b
James Robinson <james.em.robinson@gmail.com>
2018-11-28 16:17:55 +0000

    Added copy script for moving VM from one subscription to another

--- azure/build_azure_vm_image.sh
+++ azure/build_azure_vm_image.sh
@@ -31,7 +31,6 @@ while getopts "h:i:r:n:" opt; do
             SOURCEIMAGE=$OPTARG
             ;;
         r)
-            echo "Called r with $OPTARG"
             RESOURCEGROUP=$OPTARG
             ;;
         n)
