3698479ca304101a6e1f8bf367caf2e2d966452c
Martin O'Reilly <developer@mtspace.net>
2019-03-13 18:30:41 +0000

    Refactor DSG DC deployment script to use new DSG config

--- new_dsg_environment/dsg_deploy_scripts/03_create_dc/Create_AD_DC.ps1
+++ new_dsg_environment/dsg_deploy_scripts/03_create_dc/Create_AD_DC.ps1
@@ -1,40 +1,59 @@
+param(
+  [Parameter(Position=0, Mandatory = $true, HelpMessage = "Enter DSG ID (usually a number e.g DSG9 = 9)")]
+  [string]$dsgId
+)
+
 Import-Module Az
+Import-Module $PSScriptRoot/../GeneratePassword.psm1
+Import-Module $PSScriptRoot/../DsgConfig.psm1
 
-$environment = Read-Host -Prompt "Enter environment name ('test' or 'prod')"
-$addressSpacePrefix = Read-Host -Prompt "Enter first three octets of address space e.g. 10.250.x"
-$dsgId = Read-Host -Prompt "Enter DSG ID (usually a number e.g DSG9 = 9)"
-$artifactSasToken = Read-Host -Prompt "Paste an SAS token with blob object read access for the 'dsgartifacts' storage account"
+# Get DSG config
+$config = Get-DsgConfig($dsgId)
 
-$resourceGroupName = "RG_DSG_DC"
-$dcName = "DSG" + $dsgId + "DC"
-$vmSize = "Standard_DS2_v2"
-$ipAddress = $addressSpacePrefix + ".250"
-$vaultName = "dsg-management-" + $environment
-$adminUser = "atiadmin"
-$adminPassword = (Get-AzKeyVaultSecret -vaultName $vaultName -name "admin-dsg9-test-dc").SecretValueText
-$domainName = "DSGROUP" + $dsgId + ".CO.UK"
+# Set deployment parameters not directly set in config file
+$vmSize = "Standard_DS2_v2";
 
-$vnetName = "DSG_DSGROUP" + $dsgId + "_VNET1"
-$vnetResourceGroupName = "RG_DSG_VNET"
-$vnetSubnet = "Subnet-Identity"
-$artifactLocation = "https://dsgxartifacts.blob.core.windows.net"
+# Fetch admin password (or create if not present)
+$adminPassword = (Get-AzKeyVaultSecret -vaultName $config.dsg.keyVault.name -name $config.dsg.dc.admin.passwordSecretName).SecretValueText;
+if ($null -eq $adminPassword) {
+  # Create password locally but round trip via KeyVault to ensure it is successfully stored
+  $newPassword = New-Password;
+  $newPassword = (ConvertTo-SecureString $newPassword -AsPlainText -Force);
+  Set-AzKeyVaultSecret -VaultName $config.dsg.keyVault.name -Name $config.dsg.dc.admin.passwordSecretName -SecretValue $newPassword;
+  $adminPassword = (Get-AzKeyVaultSecret -VaultName $config.dsg.keyVault.name -Name $config.dsg.dc.admin.passwordSecretName).SecretValueText;
+}
+$adminPassword = ConvertTo-SecureString $adminPassword -AsPlainText -Force;
+
+# Switch to management subscription and get SAS token to fetch post-deployment setup scripts from artifact blob storage
+Set-AzContext -SubscriptionId $config.shm.subscriptionName;
+$artifactResourceGroup = $config.shm.storage.artifacts.rg;
+$artifactLocation = "https://" + $config.shm.storage.artifacts.accountName + ".blob.core.windows.net";
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $artifactResourceGroup -AccountName $config.shm.storage.artifacts.accountName).Value[0];
+$storageAccountContext = (New-AzStorageContext -StorageAccountName $config.shm.storage.artifacts.accountName -StorageAccountKey $storageAccountKey);
+$artifactSasToken = (New-AzStorageAccountSASToken -Service Blob,File -ResourceType Service,Container,Object -Permission "rl" -Context $storageAccountContext);
+$artifactSasToken = ConvertTo-SecureString $artifactSasToken -AsPlainText -Force;
+
+# Switch to DSG subscription and deploy
+Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
 
 $params = @{
- "DC Name" = $dcName
+ "DC Name" = $config.dsg.dc.hostname
  "VM Size" = $vmSize
- "IP Address" = $ipAddress
- "Administrator User" = $adminUser
+ "IP Address" = $config.dsg.dc.ip
+ "Administrator User" = $config.dsg.dc.admin.username
  "Administrator Password" = $adminPassword
- "Virtual Network Name" = $vnetName
- "Virtual Network Resource Group" = $vnetResourceGroupName
- "Virtual Network Subnet" = $vnetSubnet
+ "Virtual Network Name" = $config.dsg.network.vnet.name
+ "Virtual Network Resource Group" = $config.dsg.network.vnet.rg
+ "Virtual Network Subnet" = $config.dsg.network.subnets.identity.name
  "Artifacts Location" = $artifactLocation
  "Artifacts Location SAS Token" = $artifactSasToken
- "Domain Name" = $domainName
+ "Domain Name" = $config.dsg.domain.fqdn
 }
 
-Write-Output $params
+$templatePath = Join-Path $PSScriptRoot "dc-master-template.json"
 
-New-AzResourceGroup -Name $resourceGroupName -Location uksouth
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
-  -TemplateFile ./arm-templates/DCServer/dc-master-template.json @params -Verbose
+Write-Output ($params | ConvertTo-JSON -depth 10)
+
+New-AzResourceGroup -Name $config.dsg.dc.rg -Location uksouth
+New-AzResourceGroupDeployment -ResourceGroupName $config.dsg.dc.rg `
+  -TemplateFile $templatePath @params -Verbose
