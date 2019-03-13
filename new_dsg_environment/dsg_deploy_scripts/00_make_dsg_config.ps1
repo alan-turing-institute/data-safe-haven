c50e238675a67546a156e0fb9da9e362c524a2b3
Martin O'Reilly <developer@mtspace.net>
2019-03-13 14:39:33 +0000

    Fix DSG full config JSON outpout depth

--- new_dsg_environment/dsg_deploy_scripts/00_make_dsg_config.ps1
+++ new_dsg_environment/dsg_deploy_scripts/00_make_dsg_config.ps1
@@ -34,6 +34,15 @@ $shmThirdOctet = ([int] $shmPrefixOctets[2])
 $config.shm.subscriptionName = $shmConfigBase.subscriptionName
 $config.shm.id = $shmConfigBase.shId
 
+# --- Domain config ---
+$config.shm.domain = @{}
+$config.shm.domain.fqdn = $shmConfigBase.domain
+$config.shm.domain.netbiosName = $config.shm.domain.fqdn.Split('.')[0].ToUpper()
+$config.shm.domain.dn = "DC=" + ($config.shm.domain.fqdn.replace('.',',DC='))
+$config.shm.domain.serviceOuPath = "OU=Safe Haven Service Accounts," + $config.shm.domain.dn
+$config.shm.domain.userOuPath = "OU=Safe Haven Research Users," + $config.shm.domain.dn
+$config.shm.domain.securityOuPath = "OU=Safe Haven Security Groups," + $config.shm.domain.dn
+
 # --- Network config ---
 $config.shm.network = @{
     vnet = @{}
@@ -44,19 +53,17 @@ $config.shm.network.subnets.identity = @{}
 $config.shm.network.subnets.identity.prefix = $shmBasePrefix + "." + $shmThirdOctet
 $config.shm.network.subnets.identity.cidr = $config.shm.network.subnets.identity.prefix + ".0/24"
 
-# --- Domain config ---
-$config.shm.domain = @{}
-$config.shm.domain.fqdn = $shmConfigBase.domain
-$config.shm.domain.netbiosName = $config.shm.domain.fqdn.Split('.')[0].ToUpper()
-$config.shm.domain.dn = "DC=" + ($config.shm.domain.fqdn.replace('.',',DC='))
-$config.shm.domain.serviceOuPath = "OU=Safe Haven Service Accounts," + $config.shm.domain.dn
-$config.shm.domain.userOuPath = "OU=Safe Haven Research Users," + $config.shm.domain.dn
-$config.shm.domain.securityOuPath = "OU=Safe Haven Security Groups," + $config.shm.domain.dn
-
 # --- Domain controller config ---
 $config.shm.dc = @{}
 $config.shm.dc.hostname = $shmConfigBase.dcHostname
 $config.shm.dc.ip = $config.shm.network.subnets.identity.prefix + ".250"
+
+# --- Storage config --
+$config.shm.storage = @{
+    artifacts = @{}
+}
+$config.shm.storage.artifacts.rg = "RG_DSG_ARTIFACTS"
+$config.shm.storage.artifacts.accountName = "dsgxartifacts"
 
 # -- Secrets config ---
 $config.shm.keyVault = @{}
@@ -78,26 +85,6 @@ $dsgThirdOctet = $dsgPrefixOctets[2]
 $config.dsg.subscriptionName = $dsgConfigBase.subscriptionName
 $config.dsg.id = $dsgConfigBase.dsgId
 
-# --- Network config ---
-$config.dsg.network = @{
-    vnet = @{}
-    subnets = @{
-        identity = @{}
-        rds = @{}
-        data = @{}
-        gateway = @{}
-    }
-}
-$config.dsg.network.vnet.cidr = $dsgBasePrefix + "." + $dsgThirdOctet + ".0/21"
-$config.dsg.network.subnets.identity.prefix =  $dsgBasePrefix + "." + $dsgThirdOctet
-$config.dsg.network.subnets.identity.cidr = $config.dsg.network.subnets.identity.prefix + ".0/24"
-$config.dsg.network.subnets.rds.prefix =  $dsgBasePrefix + "." + ([int] $dsgThirdOctet + 1)
-$config.dsg.network.subnets.rds.cidr = $config.dsg.network.subnets.rds.prefix + ".0/24"
-$config.dsg.network.subnets.data.prefix =  $dsgBasePrefix + "." + ([int] $dsgThirdOctet + 2)
-$config.dsg.network.subnets.data.cidr = $config.dsg.network.subnets.data.prefix + ".0/24" 
-$config.dsg.network.subnets.gateway.prefix =  $dsgBasePrefix + "." + ([int] $dsgThirdOctet + 7)
-$config.dsg.network.subnets.gateway.cidr = $config.dsg.network.subnets.rds.prefix + ".0/24"
-
 # -- Domain config ---
 $config.dsg.domain = @{}
 $config.dsg.domain.fqdn = $dsgConfigBase.domain
@@ -109,12 +96,45 @@ $config.dsg.domain.securityGroups = @{
 $config.dsg.domain.securityGroups.researchUsers.name = "SG " + $config.dsg.domain.netbiosName + " Research Users"
 $config.dsg.domain.securityGroups.researchUsers.description = $config.dsg.domain.securityGroups.researchUsers.name
 
+# --- Network config ---
+$config.dsg.network = @{
+    vnet = @{}
+    subnets = @{
+        identity = @{}
+        rds = @{}
+        data = @{}
+        gateway = @{}
+    }
+}
+$config.dsg.network.vnet.rg = "RG_DSG_VNET"
+$config.dsg.network.vnet.name = "DSG_" + $config.dsg.domain.netbiosName + "_VNET1"
+$config.dsg.network.vnet.cidr = $dsgBasePrefix + "." + $dsgThirdOctet + ".0/21"
+$config.dsg.network.subnets.identity.name = "Subnet-Identity"
+$config.dsg.network.subnets.identity.prefix =  $dsgBasePrefix + "." + $dsgThirdOctet
+$config.dsg.network.subnets.identity.cidr = $config.dsg.network.subnets.identity.prefix + ".0/24"
+$config.dsg.network.subnets.rds.prefix =  $dsgBasePrefix + "." + ([int] $dsgThirdOctet + 1)
+$config.dsg.network.subnets.rds.cidr = $config.dsg.network.subnets.rds.prefix + ".0/24"
+$config.dsg.network.subnets.data.prefix =  $dsgBasePrefix + "." + ([int] $dsgThirdOctet + 2)
+$config.dsg.network.subnets.data.cidr = $config.dsg.network.subnets.data.prefix + ".0/24" 
+$config.dsg.network.subnets.gateway.prefix =  $dsgBasePrefix + "." + ([int] $dsgThirdOctet + 7)
+$config.dsg.network.subnets.gateway.cidr = $config.dsg.network.subnets.rds.prefix + ".0/24"
+
+# --- Secrets ---
+$config.dsg.keyVault = @{
+    name = "dsg-management-" + $config.shm.id # TODO: Once all scripts driven by this config make separate KeyVault per DSG
+}
+
 # --- Domain controller ---
 $config.dsg.dc = @{}
-$config.dsg.dc.hostname = $config.dsg.domain.netbiosName + "_DC"
+$config.dsg.dc.rg = "RG_DSG_DC"
+$config.dsg.dc.hostname = "DSG" + $config.dsg.id + "DC" # TODO: Once all scripts driven by this config, change to: $config.dsg.domain.netbiosName + "_DC"
 $config.dsg.dc.ip = $config.dsg.network.subnets.identity.prefix + ".250"
+$config.dsg.dc.admin = @{
+    username = "atiadmin"
+    passwordSecretName = "dsg" + $config.dsg.id + "-dc-admin-password" # TODO: Current format targeted at using shm keyvault. Update if this changes.
+}
 
-# --- Users ---
+# --- Domain users ---
 $config.dsg.users = @{
     ldap = @{
         gitlab = @{}
@@ -129,6 +149,7 @@ $config.dsg.users.ldap.hackmd.name = $config.dsg.domain.netbiosName + " HackMD L
 $config.dsg.users.ldap.hackmd.samAccountName = $config.dsg.domain.netbiosName.ToLower() + "_hackmd_ldap"
 $config.dsg.users.ldap.dsvm.name = $config.dsg.domain.netbiosName + " DSVM LDAP"
 $config.dsg.users.ldap.dsvm.samAccountName = $config.dsg.domain.netbiosName.ToLower() + "_dsvm_ldap"
+
 # --- Secure servers ---
 
 # Data server
@@ -141,6 +162,7 @@ $config.dsg.users.ldap.dsvm.samAccountName = $config.dsg.domain.netbiosName.ToLo
 
 # --- RDS Servers ---
 
-Write-Host ($config | ConvertTo-Json -depth 10 )
-Out-File -FilePath $dsgFullConfigPath -Encoding "UTF8" -InputObject ($config | ConvertTo-Json)
+$jsonOut = ($config | ConvertTo-Json -depth 10 )
+Write-Host $jsonOut
+Out-File -FilePath $dsgFullConfigPath -Encoding "UTF8" -InputObject $jsonOut
  
