63878ae2fbd119976199e6645015fc14bba7efd8
James Robinson <james.em.robinson@gmail.com>
2019-03-15 18:54:56 +0000

    Fixed source/destination typo

--- new_dsg_environment/azure-vms/deploy_azure_internal_mirror_servers.sh
+++ new_dsg_environment/azure-vms/deploy_azure_internal_mirror_servers.sh
@@ -149,7 +149,7 @@ if [ "$(az network nsg rule show --resource-group $RESOURCEGROUP --nsg-name $NSG
     az network nsg rule create --resource-group $RESOURCEGROUP --nsg-name $NSG_EXTERNAL --direction Outbound --name rsyncOutbound --description "Allow ports 22 and 873 for rsync" --source-address-prefixes $IP_RANGE_SBNT_EXTERNAL --destination-port-ranges 22 873 --protocol TCP --destination-address-prefixes $IP_RANGE_SUBNET_INTERNAL --priority 200
 # ... otherwise we update them, extracting the existing IP ranges first
 else
-    EXISTING_IP_RANGES=$(az network nsg rule show --resource-group $RESOURCEGROUP --nsg-name $NSG_EXTERNAL --name rsyncOutbound --query "[sourceAddressPrefix, sourceAddressPrefixes]" -o tsv | xargs)
+    EXISTING_IP_RANGES=$(az network nsg rule show --resource-group $RESOURCEGROUP --nsg-name $NSG_EXTERNAL --name rsyncOutbound --query "[destinationAddressPrefix, destinationAddressPrefixes]" -o tsv | xargs)
     az network nsg rule update --resource-group $RESOURCEGROUP --nsg-name $NSG_EXTERNAL --name rsyncOutbound --destination-address-prefixes $EXISTING_IP_RANGES $IP_RANGE_SUBNET_INTERNAL
 fi
 
