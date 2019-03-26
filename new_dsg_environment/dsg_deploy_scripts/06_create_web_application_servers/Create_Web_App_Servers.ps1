7b8b234f472ea952cb09a52f313b5b2f6b83a16b
Martin O'Reilly <developer@mtspace.net>
2019-03-26 13:18:41 +0000

    Switch to DSG subscription in Web App server deploy

--- new_dsg_environment/dsg_deploy_scripts/06_create_web_application_servers/Create_Web_App_Servers.ps1
+++ new_dsg_environment/dsg_deploy_scripts/06_create_web_application_servers/Create_Web_App_Servers.ps1
@@ -9,6 +9,10 @@ Import-Module $PSScriptRoot/../DsgConfig.psm1
 # Get DSG config
 $config = Get-DsgConfig($dsgId)
 
+# Temporarily switch to DSG subscription
+$prevContext = Get-AzContext
+Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
+
 # Admin user credentials (must be same as for DSG DC for now)
 $adminUser = $config.dsg.dc.admin.username
 $adminPassword = (Get-AzKeyVaultSecret -vaultName $config.dsg.keyVault.name -name $config.dsg.dc.admin.passwordSecretName).SecretValueText
@@ -72,3 +76,6 @@ $templatePath = Join-Path $PSScriptRoot "linux-master-template.json"
 New-AzResourceGroup -Name $config.dsg.linux.rg -Location $config.dsg.location
 New-AzResourceGroupDeployment -ResourceGroupName $config.dsg.linux.rg `
   -TemplateFile $templatePath @params -Verbose
+
+# Switch back to original subscription
+Set-AzContext -Context $prevContext;
