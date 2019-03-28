672667c9d146ceea3d748a5968d0495780dcce42
James Robinson <james.em.robinson@gmail.com>
2019-03-28 10:37:19 +0000

    Revert "Add production sandbox environment (issue 89)"

--- new_dsg_environment/dsg_deploy_scripts/02_create_vnet/Create_VNET.ps1
+++ new_dsg_environment/dsg_deploy_scripts/02_create_vnet/Create_VNET.ps1
@@ -8,15 +8,10 @@ Import-Module $PSScriptRoot/../DsgConfig.psm1
 
 # Get DSG config
 $config = Get-DsgConfig($dsgId)
-
-# Temporarily switch to DSG subscription
-$prevContext = Get-AzContext
-Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
-
 # Get P2S Root certificate for VNet Gateway
 $cert = (Get-AzKeyVaultSecret -Name $config.shm.keyVault.secretNames.p2sRootCert -VaultName $config.shm.keyVault.name).SecretValue
 
-$vnetCreateParams = @{
+$params = @{
  "Virtual Network Name" = $config.dsg.network.vnet.name
  "P2S VPN Certificate" = $cert
  "Virtual Network Address Space" = $config.dsg.network.vnet.cidr
@@ -27,50 +22,7 @@ $vnetCreateParams = @{
  "DNS Server IP Address" =  $config.dsg.dc.ip
 }
 
-Write-Output $vnetCreateParams
-
-$templatePath = Join-Path $PSScriptRoot "vnet-master-template.json"
-
+Write-Output $params
 New-AzResourceGroup -Name $config.dsg.network.vnet.rg -Location $config.dsg.location
 New-AzResourceGroupDeployment -ResourceGroupName $config.dsg.network.vnet.rg `
-  -TemplateFile $templatePath @vnetCreateParams -Verbose
-
-# Fetch DSG Vnet
-$dsgVnet = Get-AzVirtualNetwork -Name $config.dsg.network.vnet.name `
-                                -ResourceGroupName $config.dsg.network.vnet.rg 
-
-# Temporarily switch to management subscription
-Set-AzContext -SubscriptionId $config.shm.subscriptionName;
-# Fetch SHM Vnet
-$shmVnet = Get-AzVirtualNetwork -Name $config.shm.network.vnet.name `
-                                -ResourceGroupName $config.shm.network.vnet.rg 
-# Add Peering to SHM Vnet
-$shmPeeringParams = @{
-  "Name" = "PEER_" + $config.dsg.network.vnet.name
-  "VirtualNetwork" = $shmVnet
-  "RemoteVirtualNetworkId" = $dsgVnet.Id
-  "BlockVirtualNetworkAccess" = $FALSE
-  "AllowForwardedTraffic" = $FALSE
-  "AllowGatewayTransit" = $FALSE
-  "UseRemoteGateways" = $FALSE
-}
-Write-Output $shmPeeringParams
-Add-AzVirtualNetworkPeering @shmPeeringParams
-
-# Switch back to DSG subscription
-Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
-# Add Peering to DSG Vnet
-$dsgPeeringParams = @{
-  "Name" = "PEER_" + $config.shm.network.vnet.name
-  "VirtualNetwork" = $dsgVnet
-  "RemoteVirtualNetworkId" = $shmVnet.Id
-  "BlockVirtualNetworkAccess" = $FALSE
-  "AllowForwardedTraffic" = $FALSE
-  "AllowGatewayTransit" = $FALSE
-  "UseRemoteGateways" = $FALSE
-}
-Write-Output $dsgPeeringParams
-Add-AzVirtualNetworkPeering @dsgPeeringParams
-
-# Switch back to original subscription
-Set-AzContext -Context $prevContext;
+  -TemplateFile vnet-master-template.json @params -Verbose
