86d3d83c1c223adb616dc0ce35bdf32fb92d2af3
Miguel Morin <32396311+miguelmorin@users.noreply.github.com>
2019-03-29 12:45:50 +0000

    Fix detection of Linux, which is also posix

--- tests/tests.py
+++ tests/tests.py
@@ -4,21 +4,21 @@ import unittest
 
 import numpy as np
 
-VERSIONS_DSG = [(2, 7, 16), (3, 6, 8), (3, 7, 2)]  # tuples with version numbers
-VERSIONS_LOCAL = [(3, 6, 7)]
+PY_VERSIONS_DSG = [(2, 7, 16), (3, 6, 8), (3, 7, 2)]  # tuples with version numbers
+PY_VERSIONS_LOCAL = [(3, 6, 7)]
 
-def is_mac():
-    """Returns true if running on macOS.
+def is_linux():
+    """Returns true if running on Linux.
     """
-    return "posix" == os.name
+    return "Linux" == os.uname()[0]
 
 class Tests(unittest.TestCase):
     """Run tests for installation of Python."""
         
-    def test_version(self):
-        version = sys.version_info[:3]
-        expected_versions = VERSIONS_LOCAL if is_mac() else VERSIONS_DSG
-        self.assertTrue(version in expected_versions)
+    def test_python_version(self):
+        py_version = sys.version_info[:3]
+        expected_py_versions = PY_VERSIONS_DSG if is_linux() else PY_VERSIONS_LOCAL
+        self.assertTrue(py_version in expected_py_versions)
 
 if '__main__' == __name__:
     import unittest
