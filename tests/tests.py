d62a5eb3c64b805f4072ff4ad8d824139c5c5f73
Miguel Morin <32396311+miguelmorin@users.noreply.github.com>
2019-03-29 14:43:20 +0000

    Add Python package testing

--- tests/tests.py
+++ tests/tests.py
@@ -2,23 +2,38 @@ import os
 import sys
 import unittest
 
-import numpy as np
+import pkg_resources
 
-PY_VERSIONS_DSG = [(2, 7, 16), (3, 6, 8), (3, 7, 2)]  # tuples with version numbers
-PY_VERSIONS_LOCAL = [(3, 6, 7)]
+
+PY_VERSIONS_DSG = ["2.7", "3.6", "3.7"]  # tuples with version numbers
+PY_VERSIONS_LOCAL = ["2.7", "3.6"]
 
 def is_linux():
     """Returns true if running on Linux.
     """
     return "Linux" == os.uname()[0]
 
+def get_version():
+    """Gets the current Python version in a string.
+    """
+    return ".".join(sys.version_info[:2])
+
+def get_package()
+
 class Tests(unittest.TestCase):
     """Run tests for installation of Python."""
         
     def test_python_version(self):
-        py_version = sys.version_info[:3]
+        py_version = get_version()
         expected_py_versions = PY_VERSIONS_DSG if is_linux() else PY_VERSIONS_LOCAL
         self.assertTrue(py_version in expected_py_versions)
+
+    def test_packages(self):
+        for p in PACKAGES:
+            try:
+                dist_info = pkg_resources.get_distribution(p)
+            except pkg_resources.DistributionNotFound:
+                self.fail("Package %s not installed" %p)
 
 if '__main__' == __name__:
     import unittest
