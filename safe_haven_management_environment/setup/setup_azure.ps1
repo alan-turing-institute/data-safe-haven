1d2583315be029d2f3db28ca288aded5e620e699
Oscar Giles <ogiles@turing.ac.uk>
2019-05-24 15:57:09 +0100

    Setup azure scipt
    - Generates certificates
    - Creates all required resource groups and does the following in these resource groups:
    
        - RG_SHM_RESOURCES: creates a storage container and uploads files to blob storage and file shares
        - RG_SHM_SECRETS: Creates a keyvault, generates passwords for all 3 VMs and sticks them in the keyvault (you can then access them through the portal or powershell/cli)

--- safe_haven_management_environment/setup/setup_azure.ps1
+++ safe_haven_management_environment/setup/setup_azure.ps1
@@ -10,10 +10,25 @@ New-AzResourceGroup -Name RG_SHM_VNET -Location uksouth
 New-AzResourceGroup -Name RG_SHM_DC -Location uksouth
 New-AzResourceGroup -Name RG_SHM_NPS -Location uksouth
 New-AzResourceGroup -Name RG_SHM_SECRETS -Location uksouth
+New-AzResourceGroup -Name RG_SHM_RESOURCES -Location uksouth
 
+# Create a keyvault and generate passwords
+New-AzKeyVault -Name $vaultName -ResourceGroupName RG_SHM_SECRETS  -Location uksouth
 
-# Create a keyvault
-New-AzKeyVault -Name 'shmvault' -ResourceGroupName RG_SHM_VNET -Location uksouth
+Import-Module ../scripts/local/GeneratePassword.psm1
+
+# VM1
+$secretvalue = ConvertTo-SecureString (New-Password) -AsPlainText -Force
+Set-AzKeyVaultSecret -VaultName $vaultName -Name 'SHMDC1' -SecretValue $secretvalue
+# VM2
+$secretvalue = ConvertTo-SecureString (New-Password) -AsPlainText -Force
+Set-AzKeyVaultSecret -VaultName $vaultName -Name 'SHMDC2' -SecretValue $secretvalue
+# VM3
+$secretvalue = ConvertTo-SecureString (New-Password) -AsPlainText -Force
+Set-AzKeyVaultSecret -VaultName $vaultName -Name 'NPS' -SecretValue $secretvalue
+
+# To retrieve keys
+# (Get-AzKeyVaultSecret -vaultName "Contosokeyvault" -name "ExamplePassword").SecretValueText
 
 # Generate certificates
 $cwd = Get-Location
@@ -21,6 +36,25 @@ Set-Location -Path ../scripts/local/ -PassThru
 sh generate-root-cert.sh
 Set-Location -Path $cwd -PassThru
 
+# Setup resources
+$storageAccount = New-AzStorageAccount -ResourceGroupName RG_SHM_RESOURCES -Name "shmfiles" -Location uksouth -SkuName "Standard_LRS"
+new-AzStoragecontainer -Name "dsc" -Context $storageAccount.Context 
+New-AzStorageShare -Name 'scripts' -Context $storageAccount.Context
+New-AzStorageShare -Name 'sqlserver' -Context $storageAccount.Context
+
+# Create directories in file share
+New-AzStorageDirectory -Context $storageAccount.Context -ShareName "scripts" -Path "dc"
+New-AzStorageDirectory -Context $storageAccount.Context -ShareName "scripts" -Path "nps"
+
+
+# Upload files
+Get-ChildItem -File "../dsc/shmdc1/" -Recurse | Set-AzStorageBlobContent -Container "dsc" -Context $storageAccount.Context
+
+Get-ChildItem -File "../scripts/dc/" -Recurse | Set-AzStorageFileContent -ShareName "scripts" -Path "dc/" -Context $storageAccount.Context 
+Get-ChildItem -File "../scripts/nps/" -Recurse | Set-AzStorageFileContent -ShareName "scripts" -Path "nps/" -Context $storageAccount.Context 
+
+    
+# TO RUN THIS SCRIPT (second is my personal subscription)
 # ./setup_azure.ps1 -SubscriptionId "ff4b0757-0eb8-4e76-a53d-4065421633a6"
 # ./setup_azure.ps1 -SubscriptionId "a570a7a2-8632-4a2f-aa10-d7fe37eca122" -vaultName "shmvault"
 
