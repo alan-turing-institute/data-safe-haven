4a03e4dbd24c857e43b9593d1ff605fc3310baae
James Robinson <james.em.robinson@gmail.com>
2019-06-07 15:12:09 +0100

    Added unpeering script, fixed problem with peering script

---
+++ new_dsg_environment/dsg_deploy_scripts/09_peer_mirrors/Unpeer_Dsg_And_Mirror_Networks.ps1
@@ -0,0 +1,39 @@
+param(
+  [Parameter(Position=0, Mandatory = $true, HelpMessage = "Enter DSG ID (usually a number e.g enter '9' for DSG9)")]
+  [string]$dsgId
+)
+
+Import-Module Az
+Import-Module $PSScriptRoot/../DsgConfig.psm1 -Force
+
+# Get DSG config and store original subscription
+$config = Get-DsgConfig($dsgId)
+$originalSubscription = Get-AzContext
+
+
+# Switch to management subscription
+Set-AzContext -SubscriptionId $config.shm.subscriptionName;
+# Remove peering from mirror VNet
+$mirrorPeeringParams = @{
+  "Name" = "PEER_" + $config.dsg.network.vnet.name
+  "VirtualNetworkName" = $config.dsg.mirrors.vnet.name
+  "ResourceGroupName" = $config.dsg.mirrors.vnet.rg
+}
+Write-Output $mirrorPeeringParams
+Remove-AzVirtualNetworkPeering @mirrorPeeringParams -Force
+
+
+# Switch to DSG subscription
+Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
+# Remove peering from DSG VNet
+$dsgPeeringParams = @{
+  "Name" = "PEER_" + $config.shm.mirrors.vnet.name
+  "VirtualNetworkName" = $config.dsg.network.vnet.name
+  "ResourceGroupName" = $config.dsg.network.vnet.rg
+}
+Write-Output $dsgPeeringParams
+Remove-AzVirtualNetworkPeering @dsgPeeringParams -Force
+
+
+# Switch back to original subscription
+Set-AzContext -Context $originalSubscription;
