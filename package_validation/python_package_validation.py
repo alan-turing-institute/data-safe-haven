b5ca785d434c265a8b4c2bd1621659bc247bbb7e
Diego Arenas <darenasc@gmail.com>
2019-06-09 19:37:48 +0100

    functions updated

--- package_validation/python_package_validation.py
+++ package_validation/python_package_validation.py
@@ -308,6 +308,91 @@ def get_dependencies(name, version):
             dependencies.append((d['name']+' '+d['requirements']))
         return len(dependencies), dependencies
 
+def evaluate_packages_from_anaconda(python_version):
+    anaconda_url = 'https://docs.anaconda.com/anaconda/packages/py{python_version}_linux-64/'.format(python_version=python_version)
+    df = get_initial_python_packages(anaconda_url)
+
+    # Creates new columns in the dataframe
+    df['github_link'] = None            # url of the github repository
+    df['has_version_control'] = None    # Yes/No if has a public repository in github, gitlab, or bitbucket
+    df['pypi_url'] = None               # generated pypi url, this url needs to be tested if exists or not.
+
+    # 
+    pbar = tqdm(range(len(df)))
+    for i in pbar:
+        pbar.set_description('{}'.format(df.name.iloc[i]))
+        df.at[i, 'github_link'] = get_github_url(df.name.iloc[i], df.link.iloc[i])
+        df.at[i, 'has_version_control'] = has_version_control(df.name.iloc[i], df.github_link.iloc[i])
+        df.at[i, 'pypi_url'] = get_pypi_url(df.name.iloc[i])
+
+    # Information from libraries.io
+    df['login'] = None          # login of a main contributor
+    df['email'] = None          # email of a main contributor
+    df['cont_rank'] = None      # ranking of the contributor, starts searching from the lead contributor until finds an email and its rank
+    df['licenses'] = None       # license of the package
+
+    # This takes awhile because libraries.io has a rate limit of 60 calls per minute
+    pbar = tqdm(range(len(df)))
+    for i in pbar:
+        pbar.set_description('{}'.format(df.name.iloc[i]))
+        login, email, cont_rank = get_contributor_email(df.name.iloc[i])
+        _, repo_url, licenses = get_project_info(df.name.iloc[i])
+        df.at[i, 'login'] = login
+        df.at[i, 'email'] = email
+        df.at[i, 'cont_rank'] = cont_rank
+        if repo_url and df.github_link.iloc[i] == None:
+            df.at[i, 'github_link'] = repo_url
+        if licenses:
+            df.at[i, 'licenses'] = licenses
+        time.sleep(1.5) # 1.5s because there is two calls to the API within the loop
+
+    # Creates new columns Information from GitHub
+    df['num_commits'] = None					# Number of commits in the last year
+    df['num_contributors_last_year'] = None		# Number of contributors in the last year
+    df['total_contributors'] = None				# Total number of contributores of the package
+    df['more_than_3_contributors'] = None		# Yes/No if the package has more than 3 contributors
+    df['is_in_pypi'] = None						# Yes/No if the package is in PYPI
+    df['is_in_conda'] = None					# Yes/No if the package is in Anaconda
+
+    # Gets all the packages in Python 3.7 in a list, to verify if the package is or not in the anaconda distribution.
+    python_anaconda_37 = get_conda_packages(anaconda_url)
+
+    pbar = tqdm(range(len(df)))
+    for i in pbar:
+        pbar.set_description('{}'.format(df.name.iloc[i]))
+        if df.github_link.iloc[i]:
+            df.at[i, 'num_commits'] = get_num_commits(df.github_link.iloc[i])
+            df.at[i, 'num_contributors_last_year'] = get_contributors(df.github_link.iloc[i], 365)
+            df.at[i, 'total_contributors'] = get_total_contributors(df.github_link.iloc[i])
+            df.at[i, 'more_than_3_contributors'] = has_more_than_n_contributors(df.num_contributors_last_year.iloc[i])
+        df.at[i, 'is_in_pypi'] = is_in_pypi(df.name.iloc[i])
+        df.at[i, 'is_in_conda'] = is_in_conda(python_anaconda_37, df.name.iloc[i])
+
+    # Creating the evaluation criteria. Yes/No if passes all the criteria.
+    df['evaluation_criteria'] = False
+
+    for i in range(len(df)):
+        df.at[i, 'evaluation_criteria'] = (df.has_version_control.iloc[i]=='Yes'
+            and (False if df.num_commits.iloc[i] == None else df.num_commits.iloc[i]>3)
+            and df.more_than_3_contributors.iloc[i]=='Yes'
+            and (df.is_in_pypi.iloc[i]=='Yes' or df.is_in_conda.iloc[i]=='Yes')
+            and df.licenses.iloc[i] != None
+            and df.email.iloc[i] != None)
+
+    # Adding dependencies of the packages
+    df['number_of_dependencies'] = None		# Number of dependencies of the package
+    df['dependencies'] = None				# Dependencies and versions of the package
+
+    pbar = tqdm(range(len(df)))
+    for i in pbar:
+        pbar.set_description('{}'.format(df.name.iloc[i]))
+        num_deps, deps = get_dependencies(df.name.iloc[i], df.version.iloc[i])
+        df.at[i, 'number_of_dependencies'] = num_deps
+        df.at[i, 'dependencies'] = deps
+        time.sleep(1)
+
+    return df
+
 def evaluate_package(name, python_version = '3.7'):
     """
     Returns a dataframe with 1 row evaluating the package.
@@ -360,7 +445,7 @@ def evaluate_package(name, python_version = '3.7'):
     df['is_in_conda'] = None					# Yes/No if the package is in Anaconda
 
     # Gets all the packages in Python 3.7 in a list, to verify if the package is or not in the anaconda distribution.
-    python_anaconda_37 = pv.get_conda_packages('https://docs.anaconda.com/anaconda/packages/py3.7_linux-64/')
+    python_anaconda_37 = get_conda_packages(anaconda_url)
 
     pbar = tqdm(range(len(df)))
     for i in pbar:
