3445fb99ea029819686c214e73185b05852a5f08
Martin O'Reilly <developer@mtspace.net>
2019-06-21 00:48:41 +0100

    Replace hard coded db admin role and user

--- new_dsg_environment/dsg_deploy_scripts/07_deploy_compute_vms/remote_scripts/create_postgres_dbadmin.sh
+++ new_dsg_environment/dsg_deploy_scripts/07_deploy_compute_vms/remote_scripts/create_postgres_dbadmin.sh
@@ -8,12 +8,12 @@
 # Check if admin role exists and create if it does not
 (sudo -i -u postgres psql -tc "SELECT 1 FROM pg_roles WHERE rolname='${DBADMINROLE}'" | grep -q 1 \
     && echo "Role '${DBADMINROLE}' already exists, skipping creation.") \
-|| sudo -i -u postgres psql -c "CREATE ROLE admin WITH CREATEDB CREATEROLE;"
+|| sudo -i -u postgres psql -c "CREATE ROLE ${DBADMINROLE} WITH CREATEDB CREATEROLE;"
 # Check if dbadmin user exists and (a) create if it does not or (ii) update its password if it does
 (sudo -i -u postgres psql -tc "SELECT 1 FROM pg_user WHERE usename='${DBADMINUSER}'" | grep -q 1 \
     && echo "User '${DBADMINUSER}' already exists." \
     && sudo -i -u postgres psql -c "ALTER USER ${DBADMINUSER} WITH PASSWORD '${DBADMINPWD}';" \
     && echo "Password for user '${DBADMINUSER}' reset to provided value.") \
-|| sudo -i -u postgres psql -c "CREATE USER dbadmin IN ROLE admin INHERIT PASSWORD '${DBADMINPWD}';"
+|| sudo -i -u postgres psql -c "CREATE USER ${DBADMINUSER} IN ROLE admin INHERIT PASSWORD '${DBADMINPWD}';"
 # Show current user table
 sudo -i -u postgres psql -c "SELECT * FROM pg_user"
