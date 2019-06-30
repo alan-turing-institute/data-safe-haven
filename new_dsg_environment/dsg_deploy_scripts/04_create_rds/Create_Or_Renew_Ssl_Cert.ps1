d3cbe182d060bf2f4951af660bad941ee54b9026
Martin O'Reilly <developer@mtspace.net>
2019-07-01 00:25:43 +0100

    Add top-level wrapper script to create/renew SSL cert

---
+++ new_dsg_environment/dsg_deploy_scripts/04_create_rds/Create_Or_Renew_Ssl_Cert.ps1
@@ -0,0 +1,25 @@
+param(
+  [Parameter(Position=0, Mandatory = $true, HelpMessage = "Enter DSG ID (usually a number e.g enter '9' for DSG9)")]
+  [string]$dsgId,
+  [Parameter(Position=1, Mandatory = $false, HelpMessage = "Enter working directory (defaults to a temp direcotry)")]
+  [string]$workingDirectory = $null,
+  [Parameter(Position=2, Mandatory = $false, HelpMessage = "Flag to do a test 'dry run' against the Let's Encrypt staging servers (also uses a different local certbot folder)")]
+  [bool]$dryRun = $false
+)
+
+if([String]::IsNullOrEmpty($workingDirectory)) {
+    $workingDirectory = [system.io.path]::GetTempPath()
+}
+
+$helperScriptsDir = (Join-Path $PSScriptRoot "Create_Or_Renew_Ssl_Cert_Helper_Scripts" "local_scripts")
+
+# Create CSR on RDS Gateway and download to local filesystem
+$getCsrCmd = (Join-Path $helperScriptsDir "Get_Csr_From_Rds_Gateway.ps1")
+$result = Invoke-Expression -Command "$getCsrCmd -dsgId $dsgId -workingDirectory $workingDirectory"
+
+# Extract path to saved CSR from result message
+$csrPath = $result[-1]
+
+# Use CSR to get signed SSL certificate from Let's Encrypt
+$signCertCmd = (Join-Path $helperScriptsDir "Get_LetsEncrypt_Cert_From_Csr.ps1")
+$result = Invoke-Expression -Command "$signCertCmd -dsgId $dsgId -csrPath $csrPath -dryRun `$dryRun"
