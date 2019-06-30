d43791d1dbc710d86e7464b858a97823d8d40496
Martin O'Reilly <developer@mtspace.net>
2019-06-30 21:10:15 +0100

    Add script to get Cert from Let's Encrypt using local CSR (requires manual chnages to DNS)

---
+++ new_dsg_environment/dsg_deploy_scripts/04_create_rds/local_helper_scripts/LetsEncrypt_Request_Cert_From_Csr.ps1
@@ -0,0 +1,64 @@
+param(
+  [Parameter(Position=0, Mandatory = $true, HelpMessage = "Enter path to certbot root directory")]
+  [string]$certbotDir,
+  [Parameter(Position=1, Mandatory = $true, HelpMessage = "Enter DSG ID (usually a number e.g enter '9' for DSG9)")]
+  [string]$dsgId,
+  [Parameter(Position=2, Mandatory = $true, HelpMessage = "Enter path to CSR file generated on DSG RDS Gateway VM")]
+  [string]$csrPath,
+  [Parameter(Position=3, Mandatory = $false, HelpMessage = "Flag to do a 'dry run' (i.e. don't actually contact Let's Encrypt servers)")]
+  [bool]$dryRun = $false,
+  [Parameter(Position=4, Mandatory = $false, HelpMessage = "Flag to do a test run against the Let's Encrypt test servers")]
+  [bool]$testRun = $false
+
+)
+
+if(-not (Test-Path -Path $csrPath)) { throw [System.IO.FileNotFoundException] "$csrPath not found."}
+
+Import-Module Az
+Import-Module $PSScriptRoot/../../DsgConfig.psm1 -Force
+
+# Get DSG config
+$config = Get-DsgConfig($dsgId);
+
+# Temporarily switch to DSG subscription
+$prevContext = Get-AzContext
+$_ = Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
+
+$certbotConfigDir = (Join-Path $certbotDir "config")
+$certbotLogDir = (Join-Path $certbotDir "log")
+$certbotWorkingDir = Split-Path -Parent -Path $csrPath
+$csrExt = (Split-Path -Leaf -Path $csrPath).Split(".")[-1]
+if($csrExt) {
+  # Take all of filename before to extenstion
+  $csrStem = (Split-Path -Leaf -Path $csrPath).Split(".$csrExt")[0]
+} else {
+  # No extension to strip so take whole filename
+  $csrStem = (Split-Path -Leaf -Path $csrPath)[0]
+}
+$certPath = (Join-Path $certbotWorkingDir "$($csrStem)_cert.pem")
+$fullChainPath = (Join-Path $certbotWorkingDir "$($csrStem)_full_chain.pem")
+$chainPath = (Join-Path $certbotWorkingDir "$($csrStem)_chain.pem")
+
+# Set standard certbot parameters (these are the same for all certs)
+$certbotCmd = "certbot certonly --config-dir '$certbotConfigDir' --logs-dir '$certbotLogDir'"
+
+# Set cert specific input parameters
+$certbotCmd += " -d $($config.dsg.rds.gateway.fqdn) --csr '$csrPath' --cert-name '$csrStem'"
+# Set cert-specific outoput parameters
+$certbotCmd += " --work-dir '$certbotWorkingDir' --cert-path '$certPath' --fullchain-path '$fullChainPath' --chain-path '$chainPath'"
+
+# Set certbot authentication options
+$certbotCmd += " --preferred-challenges 'dns' --manual"
+
+if($dryRun) {
+  $certbotCmd += " --dry-run"
+}
+if($testRun) {
+  $certbotCmd += " --test-cert"
+}
+
+Write-Output $certbotCmd
+bash -c "$certbotCmd"
+
+# Switch back to previous subscription
+$_ = Set-AzContext -Context $prevContext;
