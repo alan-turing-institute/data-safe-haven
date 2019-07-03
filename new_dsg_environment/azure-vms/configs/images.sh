d10e0586c4c3f763dad113044bb2a383e7a2e113
James Robinson <james.em.robinson@gmail.com>
2019-07-03 18:46:12 +0100

    Updated image building

--- new_dsg_environment/azure-vms/configs/images.sh
+++ new_dsg_environment/azure-vms/configs/images.sh
@@ -4,3 +4,44 @@ RESOURCEGROUP_IMAGES="RG_SH_IMAGE_STORAGE"
 RESOURCEGROUP_NETWORK="RG_SH_NETWORKING"
 SUBSCRIPTION="Safe Haven VM Images"
 LOCATION="westeurope" # have to build in West Europe in order to use Shared Image Gallery
+
+# Other useful constants
+GALLERYNAME="SAFE_HAVEN_COMPUTE_IMAGES" # must be unique within this subscription
+
+# Ensure required features for shared image galleries are enabled for this subscription
+ensure_image_galleries_enabled() {
+    source ${BASH_SOURCE%/*}/text.sh
+    FEATURE="GalleryPreview"
+    NAMESPACE="Microsoft.Compute"
+    RESOURCE="galleries/images/versions"
+
+    # Get current feature state and metadata
+    FEATURE_STATE="$(az feature show --namespace $NAMESPACE --name $FEATURE --query 'properties.state' | xargs)"
+    RESOURCE_STATE=$(az provider show --namespace $NAMESPACE --query "resourceTypes[?resourceType=='$RESOURCE'].resourceType" -o tsv)
+
+    # Print out current status
+    echo -e "${BOLD}Ensuring that this subscription has the ${BLUE}$FEATURE${END}${BOLD} feature and ${BLUE}$RESOURCE${END}${BOLD} resource from ${BLUE}$NAMESPACE${END}${BOLD} enabled (this may take some time).${END}"
+    echo -e "${BOLD}Current ${BLUE}$FEATURE${END}${BOLD} feature state is ${BLUE}$FEATURE_STATE${END}${BOLD}.${END}"
+    if [ "$RESOURCE_STATE" = "$RESOURCE" ]; then
+        echo -e "${BOLD}Resource ${BLUE}$RESOURCE${END}${BOLD} is present.${END}"
+    else
+        echo -e "${BOLD}Resource ${BLUE}$RESOURCE${END}${BOLD} is ${RED}not${END} ${BOLD}present.${END}"
+    fi
+    # Loop until features are present
+    while [ "$FEATURE_STATE" != "Registered"  -o  "$RESOURCE_STATE" != "$RESOURCE" ]; do
+        if [ "$FEATURE_STATE" = "NotRegistered" ]; then
+            # Register feature
+            echo -e "${BOLD}Registering ${BLUE}$FEATURE${END} ${BOLD}for ${BLUE}$SUBSCRIPTION${END}"
+            az feature register --namespace Microsoft.Compute --name "$FEATURE" --subscription "$SUBSCRIPTION"
+            az provider register --namespace Microsoft.Compute
+        elif [ "$FEATURE_STATE" = "Pending" -o "$FEATURE_STATE" = "Registering" -o "$RESOURCE_STATE" != "$RESOURCE" ]; then
+            echo -ne "."
+            sleep 30
+        else
+            echo -e "${RED}$FEATURE state or $RESOURCE resource could not be found. Try updating Azure CLI.${END}"
+            exit 1
+        fi
+        FEATURE_STATE="$(az feature show --namespace $NAMESPACE --name $FEATURE --query 'properties.state' | xargs)"
+        RESOURCE_STATE=$(az provider show --namespace $NAMESPACE --query "resourceTypes[?resourceType=='$RESOURCE'].resourceType" -o tsv)
+    done
+}
--- new_dsg_environment/azure-vms/images_build_azure_compute_vm.sh
+++ new_dsg_environment/azure-vms/images_build_azure_compute_vm.sh
@@ -9,17 +9,17 @@ VMSIZE="Standard_E16s_v3"
 
 # Other constants
 ADMIN_USERNAME="atiadmin"
-MACHINENAME="ComputeVM"
-NSGNAME="NSG_IMAGE_BUILD"
-VNETNAME="VNET_IMAGE_BUILD"
-SUBNETNAME="SUBNET_IMAGE_BUILD"
+MACHINE_NAME="ComputeVM"
+NSG_NAME="NSG_IMAGE_BUILD"
+VNET_NAME="VNET_IMAGE_BUILD"
+SUBNET_NAME="SUBNET_IMAGE_BUILD"
 IP_RANGE="10.48.0.0/16" # ensure that this avoids clashes with other deployments
 
 # Document usage for this script
 print_usage_and_exit() {
     echo "usage: $0 [-h] -s subscription [-i source_image] [-r resource_group] [-z vm_size]"
     echo "  -h                           display help"
-    echo "  -s subscription              specify subscription for storing the VM images. (defaults to '${SUBSCRIPTION}')"
+    echo "  -s subscription              specify subscription for building the VM. (defaults to '${SUBSCRIPTION}')"
     echo "  -i source_image              specify source image: either 'Ubuntu' (default) or 'UbuntuTorch' (as 'Ubuntu' but with Torch included)"
     echo "  -r resource_group            specify resource group - will be created if it does not already exist (defaults to '${RESOURCEGROUP_BUILD}')"
     echo "  -z vm_size                   size of the VM to use for build (defaults to '${VMSIZE}')"
@@ -78,60 +78,37 @@ if [ $(az group exists --name $RESOURCEGROUP_NETWORK) != "true" ]; then
 fi
 
 # Ensure required features for shared image galleries are enabled for this subscription
-FEATURE="GalleryPreview"
-NAMESPACE="Microsoft.Compute"
-FEATURE_STATE="$(az feature show --namespace $NAMESPACE --name $FEATURE --query 'properties.state' | xargs)"
-RESOURCE="galleries/images/versions"
-RESOURCE_METADATA_QUERY="resourceTypes[?resourceType=='$RESOURCE']"
-RESOURCE_METADATA="$(az provider show --namespace $NAMESPACE --query $RESOURCE_METADATA_QUERY)"
-# Print out current status
-echo -e "${BOLD}Ensuring namespace ${BLUE}$NAMESPACE${END} ${BOLD}contains ${BLUE}$FEATURE${END} ${BOLD}feature and ${BLUE}$RESOURCE${END} ${BOLD}(this may take some time).${END}"
-echo -e "${BOLD}Current ${BLUE}$FEATURE${END} ${BOLD}feature state is ${BLUE}$FEATURE_STATE.${END}"
-if [ "$RESOURCE_METADATA" = "[]" ]; then
-    echo -e "${BOLD}Resource ${BLUE}$RESOURCE${END} ${BOLD}is ${RED}not${END} ${BOLD}present.${END}"
-else
-    echo -e "${BOLD}Resource ${BLUE}$RESOURCE${END} ${BOLD}is present.${END}"
-fi
-# Loop until features are present
-while [ "$FEATURE_STATE" != "Registered"  -o  "$RESOURCE_METADATA" = "[]" ]; do
-    if [ "$FEATURE_STATE" = "NotRegistered" ]; then
-        # Register feature
-        echo -e "${BOLD}Registering ${BLUE}$FEATURE${END} ${BOLD}for ${BLUE}$SUBSCRIPTION${END}"
-        az feature register --namespace Microsoft.Compute --name "$FEATURE" --subscription "$SUBSCRIPTION"
-        az provider register --namespace Microsoft.Compute
-    elif [ "$FEATURE_STATE" = "Pending"  -o  "$FEATURE_STATE" = "Registering" -o "$RESOURCE_METADATA" = "[]" ]; then
-        echo -ne "."
-        sleep 30
-    else
-        echo -e "${RED}$FEATURE state or $RESOURCE resource could not be found. Try updating Azure CLI.${END}"
-        exit 1
-    fi
-    FEATURE_STATE="$(az feature show --namespace $NAMESPACE --name $FEATURE --query 'properties.state' | xargs)"
-    RESOURCE_METADATA="$(az provider show --namespace $NAMESPACE --query $RESOURCE_METADATA_QUERY)"
-done
+ensure_image_galleries_enabled
 
-# Create a VNet and subnet for the deployment
-if [ "$(az network vnet list --resource-group $RESOURCEGROUP_NETWORK | grep $VNETNAME 2> /dev/null)" = "" ]; then
-    echo -e "${BOLD}Creating VNet for image building: ${BLUE}$VNETNAME${END}"
-    az network vnet create --resource-group $RESOURCEGROUP_NETWORK --name $VNETNAME --address-prefixes $IP_RANGE
+# Create a VNet for the deployment
+# --------------------------------
+if [ "$(az network vnet list --resource-group $RESOURCEGROUP_NETWORK | grep $VNET_NAME 2> /dev/null)" = "" ]; then
+    echo -e "${BOLD}Creating VNet for image building: ${BLUE}$VNET_NAME${END}"
+    az network vnet create --resource-group $RESOURCEGROUP_NETWORK --name $VNET_NAME --address-prefixes $IP_RANGE
 fi
-if [ "$(az network vnet subnet list --resource-group $RESOURCEGROUP_NETWORK --vnet-name $VNETNAME | grep $SUBNETNAME 2> /dev/null)" = "" ]; then
-    echo -e "${BOLD}Creating subnet for image building: ${BLUE}$SUBNETNAME${END}"
-    az network vnet subnet create --name $SUBNETNAME --resource-group $RESOURCEGROUP_NETWORK --vnet-name $VNETNAME --address-prefixes $IP_RANGE
+VNET_ID=$(az network vnet show --resource-group $RESOURCEGROUP_NETWORK --name $VNET_NAME --query "id" -o tsv)
+
+# Create a subnet for the deployment
+# ----------------------------------
+if [ "$(az network vnet subnet list --resource-group $RESOURCEGROUP_NETWORK --vnet-name $VNET_NAME | grep $SUBNET_NAME 2> /dev/null)" = "" ]; then
+    echo -e "${BOLD}Creating subnet for image building: ${BLUE}$SUBNET_NAME${END}"
+    az network vnet subnet create --name $SUBNET_NAME --resource-group $RESOURCEGROUP_NETWORK --vnet-name $VNET_NAME --address-prefixes $IP_RANGE
 fi
+SUBNET_ID=$(az network vnet subnet show --resource-group $RESOURCEGROUP_NETWORK --vnet $VNET_NAME --name $SUBNET_NAME --query "id" -o tsv)
 
 # Add an NSG group to deny inbound connections except Turing-based SSH
-if [ "$(az network nsg show --resource-group $RESOURCEGROUP_NETWORK --name $NSGNAME 2> /dev/null)" = "" ]; then
-    echo -e "${BOLD}Creating NSG for image build: ${BLUE}$NSGNAME${END}"
-    az network nsg create --resource-group $RESOURCEGROUP_NETWORK --name $NSGNAME
+# --------------------------------------------------------------------
+if [ "$(az network nsg show --resource-group $RESOURCEGROUP_NETWORK --name $NSG_NAME 2> /dev/null)" = "" ]; then
+    echo -e "${BOLD}Creating NSG for image build: ${BLUE}$NSG_NAME${END}"
+    az network nsg create --resource-group $RESOURCEGROUP_NETWORK --name $NSG_NAME
     az network nsg rule create \
         --description "Allow port 22 for management over ssh" \
         --destination-address-prefixes "*" \
         --destination-port-ranges 22 \
         --direction Inbound \
         --name TuringSSH \
-        --nsg-name $NSGNAME \
-        --priority 100 \
+        --nsg-name $NSG_NAME \
+        --priority 1000 \
         --protocol TCP \
         --resource-group $RESOURCEGROUP_NETWORK \
         --source-address-prefixes 193.60.220.240 193.60.220.253 \
@@ -143,13 +120,14 @@ if [ "$(az network nsg show --resource-group $RESOURCEGROUP_NETWORK --name $NSGN
         --destination-port-ranges "*" \
         --direction Inbound \
         --name DenyAll \
-        --nsg-name $NSGNAME \
+        --nsg-name $NSG_NAME \
         --priority 3000 \
         --protocol "*" \
         --resource-group $RESOURCEGROUP_NETWORK \
         --source-address-prefixes "*" \
         --source-port-ranges "*"
 fi
+NSG_ID=$(az network nsg show --resource-group $RESOURCEGROUP_NETWORK --name $NSG_NAME --query "id" -o tsv)
 
 # Select source image - either Ubuntu 18.04 or Ubuntu 18.04 plus Torch
 # If anything else is requested then print usage message and exit.
@@ -159,9 +137,9 @@ if [ "$SOURCEIMAGE" = "Ubuntu" -o "$SOURCEIMAGE" = "UbuntuTorch" ]; then
         echo -e "${BOLD}Enabling ${BLUE}Torch${END}${BOLD} compilation${END}"
         # Make a temporary config file with the Torch lines uncommented
         sed "s/#IF_TORCH_ENABLED //" cloud-init-buildimage-ubuntu.yaml > $TMP_CLOUD_CONFIG_YAML
-        MACHINENAME="${MACHINENAME}-UbuntuTorch1804Base"
+        MACHINE_NAME="${MACHINE_NAME}-UbuntuTorch1804Base"
     else
-        MACHINENAME="${MACHINENAME}-Ubuntu1804Base"
+        MACHINE_NAME="${MACHINE_NAME}-Ubuntu1804Base"
         cp cloud-init-buildimage-ubuntu.yaml $TMP_CLOUD_CONFIG_YAML
     fi
     SOURCEIMAGE="Canonical:UbuntuServer:18.04-LTS:latest"
@@ -173,7 +151,7 @@ fi
 
 # Append timestamp to allow unique naming
 TIMESTAMP="$(date '+%Y%m%d%H%M')"
-BASENAME="Generalized${MACHINENAME}-${TIMESTAMP}"
+BASENAME="Candidate${MACHINE_NAME}-${TIMESTAMP}"
 
 # Build python package lists
 cd package_lists && source generate_python_package_specs.sh > ../python_package_specs.yaml && cd ..
@@ -196,13 +174,13 @@ az vm create \
   --generate-ssh-keys \
   --image $SOURCEIMAGE \
   --name $BASENAME \
-  --nsg $NSGNAME \
+  --nsg $NSG_ID \
   --os-disk-name "${BASENAME}OSDISK" \
   --os-disk-size-gb $DISKSIZEGB \
   --resource-group $RESOURCEGROUP_BUILD \
   --size $VMSIZE \
-  --subnet $SUBNETNAME \
-  --vnet-name $VNETNAME
+  --subnet $SUBNET_ID \
+  --vnet-name $VNET_ID
 
 # --generate-ssh-keys will use ~/.ssh/id_rsa if available and otherwise generate a new key
 # the key will be removed from the build machine at the end of VM creation
