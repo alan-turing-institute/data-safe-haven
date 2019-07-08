fac417c68e97d1718fe37ebf003c1d42ddc1e30c
Oscar Giles <ogiles@turing.ac.uk>
2019-07-08 19:39:10 +0100

    stash

--- safe_haven_management_environment/scripts/dc/SHM_DC/map_drive.ps1
+++ safe_haven_management_environment/scripts/dc/SHM_DC/map_drive.ps1
@@ -1,22 +1,27 @@
-# Don't make parameters mandatory as if there is any issue binding them, the script will prompt for them
-# and remote execution will stall waiting for the non-present user to enter the missing parameter on the
-# command line. This take up to 90 minutes to timeout, though you can try running resetState.cmd in 
-# C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandWindows\1.1.0 on the remote VM to cancel a stalled
-# job, but this does not seem to have an immediate effect
-# Fror details, see https://docs.microsoft.com/en-gb/azure/virtual-machines/windows/run-command
 param(
-  $configJson
+  [string]$accountName,
+  [string]$sasToken
 )
 
-# The cmdkey utility is a command-line (rather than PowerShell) tool. We use Invoke-Expression to allow us to 
-# consume the appropriate values from the storage account variables. The value given to the add parameter of the
-# cmdkey utility is the host address for the storage account, <storage-account>.file.core.windows.net for Azure 
-# Public Regions. $storageAccount.Context.FileEndpoint is used because non-Public Azure regions, such as sovereign 
-# clouds or Azure Stack deployments, will have different hosts for Azure file shares (and other storage resources).
-Invoke-Expression -Command ("cmdkey /add:dsgtestbartifacts.file.core.windows.net /user:Azure\dsgtestbartifacts /pass:OFy1QJKpPOLVV13RfvlxRelV3wkEg2tH3LQM7CGzEfpLuIyiqSVeunppWt22OSA3AIXHu0PwnxwiwnAZjCd4/A==
-net use Z: \\dsgtestbartifacts.file.core.windows.net\scripts /persistent:Yes")
 
-Expand-Archive Z:/dc/SHM_DC.zip -DestinationPath c:\Scripts\
+# Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=853017" -OutFile "temp/SQLServer2017-SSEI-Expr.exe"
 
-Write-Host (Get-ChildItem -Path c:\Scripts\)
+
+# azcopy copy
+
+# $StorageContext = (New-AzureStorageContext $accountName -SasToken $sasToken)
+
+# Get-AzureStorageBlobContent -Container 'scripts' -Blob 'SHM_DC.zip' -Destination 'C:/Scripts/SHM_DC.zip' -Context $storageAccountContext -Force
+
+# # The cmdkey utility is a command-line (rather than PowerShell) tool. We use Invoke-Expression to allow us to 
+# # consume the appropriate values from the storage account variables. The value given to the add parameter of the
+# # cmdkey utility is the host address for the storage account, <storage-account>.file.core.windows.net for Azure 
+# # Public Regions. $storageAccount.Context.FileEndpoint is used because non-Public Azure regions, such as sovereign 
+# # clouds or Azure Stack deployments, will have different hosts for Azure file shares (and other storage resources).
+# Invoke-Expression -Command ("cmdkey /add:dsgtestbartifacts.file.core.windows.net /user:Azure\dsgtestbartifacts /pass:OFy1QJKpPOLVV13RfvlxRelV3wkEg2tH3LQM7CGzEfpLuIyiqSVeunppWt22OSA3AIXHu0PwnxwiwnAZjCd4/A==
+# net use Z: \\dsgtestbartifacts.file.core.windows.net\scripts /persistent:Yes")
+
+# Expand-Archive C:/Scripts/SHM_DC.zip -DestinationPath C:\Scripts\
+
+# Write-Host (Get-ChildItem -Path c:\Scripts\)
 
--- safe_haven_management_environment/setup/configure_dc.ps1
+++ safe_haven_management_environment/setup/configure_dc.ps1
@@ -10,17 +10,22 @@ Import-Module $PSScriptRoot/../../new_dsg_environment/dsg_deploy_scripts/Generat
 
 # Get DSG config
 $config = Get-ShmFullConfig($shmId)
+# For some reason, passing a JSON string as the -Parameter value for Invoke-AzVMRunCommand
+# results in the double quotes in the JSON string being stripped in transit
+# Escaping these with a single backslash retains the double quotes but the transferred
+# string is truncated. Escaping these with backticks still results in the double quotes
+# being stripped in transit, but we can then replace the backticks with double quotes 
+# at the other end to recover a valid JSON string.
+$configJson = ($config | ConvertTo-Json -depth 10 -Compress).Replace("`"","```"")
 
 # Temporarily switch to DSG subscription
 $prevContext = Get-AzContext
 Set-AzContext -SubscriptionId $config.subscriptionName;
 
-
 # Run remote script
 $scriptPath1 = Join-Path $PSScriptRoot ".." "scripts" "dc" "SHM_DC" "Set_OS_Language.ps1"
 $scriptPath2 = Join-Path $PSScriptRoot ".." "scripts" "dc" "SHM_DC" "map_drive.ps1"
 $scriptPath3 = Join-Path $PSScriptRoot ".." "scripts" "dc" "SHM_DC" "Active_Directory_Configuration.ps1"
-
 
 # Fetch ADSync user password (or create if not present)
 $ADSyncPassword = (Get-AzKeyVaultSecret -vaultName $config.keyVault.name -name $config.keyVault.secretNames.adsync).SecretValueText;
@@ -32,7 +37,6 @@ if ($null -eq $ADSyncPassword ) {
   $ADSyncPassword  = (Get-AzKeyVaultSecret -VaultName $config.keyVault.name -Name $config.keyVault.secretNames.dc ).SecretValueText
 }
 
-
 # Execute remote script 1
 $result1= Invoke-AzVMRunCommand -ResourceGroupName $config.dc.rg -Name SHMDC1 `
     -CommandId 'RunPowerShellScript' -ScriptPath $scriptPath1;
@@ -40,33 +44,39 @@ $result1= Invoke-AzVMRunCommand -ResourceGroupName $config.dc.rg -Name SHMDC1 `
 Write-Output $result1.Value;
 
 # Map drive to SHMDC1
-$result2= Invoke-AzVMRunCommand -ResourceGroupName $config.dc.rg -Name SHMDC1 `
-    -CommandId 'RunPowerShellScript' -ScriptPath $scriptPath2;
+$currentSubscription = $config.subscriptionName;
+$artifactSasToken = (New-AccountSasToken -subscriptionName $config.subscriptionName -resourceGroup $config.storage.artifacts.rg `
+  -accountName $config.storage.artifacts.accountName -service Blob,File -resourceType Service,Container,Object `
+  -permission "rl" -prevSubscription $currentSubscription);
+
+$params = @{
+  accountName = $config.storage.artifacts.accountName
+  sas_token = $artifactSasToken
+}
+
+
+$result2= Invoke-AzVMRunCommand -ResourceGroupName $config.dc.rg `
+            -Name SHMDC1 `
+            -CommandId 'RunPowerShellScript'`
+            -ScriptPath $scriptPath2;
 
 Write-Output $result2.Value;
 
-# For some reason, passing a JSON string as the -Parameter value for Invoke-AzVMRunCommand
-# results in the double quotes in the JSON string being stripped in transit
-# Escaping these with a single backslash retains the double quotes but the transferred
-# string is truncated. Escaping these with backticks still results in the double quotes
-# being stripped in transit, but we can then replace the backticks with double quotes 
-# at the other end to recover a valid JSON string.
-$configJson = ($config | ConvertTo-Json -depth 10 -Compress).Replace("`"","```"")
-$oubackuppath= "C:\Scripts\GPOs"
+# $oubackuppath= "C:\Scripts\GPOs"
 
-$params = @{
-  configJson = $configJson
-  adsyncpassword = $ADSyncPassword
-  oubackuppath = $oubackuppath
-}
+# $params = @{
+#   configJson = $configJson
+#   adsyncpassword = $ADSyncPassword
+#   oubackuppath = $oubackuppath
+# }
 
-$result3 = Invoke-AzVMRunCommand -ResourceGroupName $config.dc.rg `
-                -Name SHMDC1 `
-                -CommandId 'RunPowerShellScript'`
-                -ScriptPath $scriptPath3 `
-                -Parameter $params;
+# $result3 = Invoke-AzVMRunCommand -ResourceGroupName $config.dc.rg `
+#                 -Name SHMDC1 `
+#                 -CommandId 'RunPowerShellScript'`
+#                 -ScriptPath $scriptPath3 `
+#                 -Parameter $params;
 
-Write-Output $result3.Value;
-# # # Switch back to previous subscription
-Set-AzContext -Context $prevContext;
+# Write-Output $result3.Value;
+# # # # Switch back to previous subscription
+# Set-AzContext -Context $prevContext;
 
