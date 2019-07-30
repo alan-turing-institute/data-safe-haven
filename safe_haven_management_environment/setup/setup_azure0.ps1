b7b4aa230aef90d47cc0a87bed37a6792a92f193
Martin O'Reilly <developer@mtspace.net>
2019-07-30 11:20:28 +0100

    Fix resource group for KeyVault

--- safe_haven_management_environment/setup/setup_azure0.ps1
+++ safe_haven_management_environment/setup/setup_azure0.ps1
@@ -17,10 +17,10 @@ Set-AzContext -SubscriptionId $config.subscriptionName;
 
 # Create Resource Groups
 New-AzResourceGroup -Name $config.keyVault.rg  -Location $config.location
-New-AzResourceGroup -Name $config.storage.artifacts.rg  -Location $config.location
+New-AzResourceGroup -Name $config.keyVault.rg  -Location $config.location
 
 # Create a keyvault and generate passwords
-New-AzKeyVault -Name $config.keyVault.name  -ResourceGroupName $config.storage.artifacts.rg -Location $config.location
+New-AzKeyVault -Name $config.keyVault.name  -ResourceGroupName $config.keyVault.rg -Location $config.location
 
 Write-Host "Before running the next step, make sure to add a policy to the KeyVault '$($config.keyVault.name)' in the '$($config.keyVault.rg)' resource group that allows gives the administrator security group for this Safe Haven instance rights to manage Keys, Secrets and Certificates."
         
