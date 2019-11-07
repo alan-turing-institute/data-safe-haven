e275417be678732a71c78218ee02eff67929e340
James Robinson <james.em.robinson@gmail.com>
2019-11-07 15:48:41 +0000

    Fix help message

--- safe_haven_management_environment/setup/setup_azure2.ps1
+++ safe_haven_management_environment/setup/setup_azure2.ps1
@@ -1,5 +1,5 @@
 param(
-  [Parameter(Position=0, Mandatory = $true, HelpMessage = "Enter DSG ID (usually a number e.g enter '9' for DSG9)")]
+  [Parameter(Position=0, Mandatory = $true, HelpMessage = "Enter SHM ID (usually a string e.g enter 'testa' for Turing Development Safe Haven A)")]
   [string]$shmId
 )
 
@@ -30,7 +30,7 @@ New-AzResourceGroupDeployment -resourcegroupname $config.nps.rg`
         -VM_Size $vmSize `
         -Virtual_Network_Name $config.network.vnet.name `
         -Virtual_Network_Subnet $config.network.subnets.identity.name `
-        -NPS_VM_Name $config.nps.vmName ` 
+        -NPS_VM_Name $config.nps.vmName `
         -NPS_Host_Name $config.nps.hostname `
         -NPS_IP_Address $config.nps.ip `
         -OU_Path $config.domain.serviceServerOuPath `
