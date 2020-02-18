31d90a8540ed95e46cbc3817a1f508d6eaac4f90
Tom Doel <tom.doel@codechoreography.co.uk>
2020-02-18 12:44:11 +0000

    Update `Reset_Ldap_Password.ps1` in line with config changes

--- secure_research_environment/sre_deploy_scripts/07_deploy_compute_vms/Reset_Ldap_Password.ps1
+++ secure_research_environment/sre_deploy_scripts/07_deploy_compute_vms/Reset_Ldap_Password.ps1
@@ -1,24 +1,24 @@
 param(
     [Parameter(Position = 0,Mandatory = $true,HelpMessage = "Enter SRE ID (a short string) e.g 'sandbox' for the sandbox environment")]
-    [string]$dsgId,
+    [string]$sreId,
     [Parameter(Position = 1,Mandatory = $true,HelpMessage = "Enter last octet of compute VM IP address (e.g. 160)")]
     [string]$ipLastOctet
 )
 
 Import-Module Az
-Import-Module $PSScriptRoot/../DsgConfig.psm1 -Force
+Import-Module $PSScriptRoot/../common_powershell/Configuration.psm1 -Force
 
 # Get SRE config
-$config = Get-SreConfig ($dsgId);
+$config = Get-SreConfig ($sreId);
 $prevContext = Get-AzContext
 
 
 # Find VM with private IP address matching the provided last octet
 # ----------------------------------------------------------------
-$_ = Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
+$_ = Set-AzContext -SubscriptionId $config.sre.subscriptionName;
 Write-Host "Finding VM with last IP octet: $ipLastOctet"
-$vmId = Get-AzNetworkInterface -ResourceGroupName $config.dsg.dsvm.rg | Where-Object { ($_.IpConfigurations.PrivateIpAddress).Split(".") -eq $ipLastOctet } | ForEach-Object { $_.VirtualMachine.Id }
-$vm = Get-AzVM -ResourceGroupName $config.dsg.dsvm.rg | Where-Object { $_.Id -eq $vmId }
+$vmId = Get-AzNetworkInterface -ResourceGroupName $config.sre.dsvm.rg | Where-Object { ($_.IpConfigurations.PrivateIpAddress).Split(".") -eq $ipLastOctet } | ForEach-Object { $_.VirtualMachine.Id }
+$vm = Get-AzVM -ResourceGroupName $config.sre.dsvm.rg | Where-Object { $_.Id -eq $vmId }
 if ($?) {
     Write-Host "Found VM: $($vm.Name)"
 } else {
@@ -28,13 +28,13 @@ if ($?) {
 
 # Get LDAP secret from the KeyVault
 # ---------------------------------
-$_ = Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
-Write-Host "Checking LDAP secret in KeyVault: $($config.dsg.keyVault.name)"
-$kvLdapPassword = (Get-AzKeyVaultSecret -VaultName $config.dsg.keyVault.Name -Name $config.dsg.users.ldap.dsvm.passwordSecretName).SecretValueText;
+$_ = Set-AzContext -SubscriptionId $config.sre.subscriptionName;
+Write-Host "Checking LDAP secret in KeyVault: $($config.sre.keyVault.name)"
+$kvLdapPassword = (Get-AzKeyVaultSecret -VaultName $config.sre.keyVault.Name -Name $config.sre.keyVault.secretNames.dsvmLdapPassword).SecretValueText;
 if ($kvLdapPassword -ne $null) {
     Write-Host "Found LDAP secret in the KeyVault"
 } else {
-    throw "Could not load LDAP secret from KeyVault '$($config.dsg.keyVault.name)'"
+    throw "Could not load LDAP secret from KeyVault '$($config.sre.keyVault.name)'"
 }
 
 
@@ -43,7 +43,7 @@ if ($kvLdapPassword -ne $null) {
 $_ = Set-AzContext -SubscriptionId $config.shm.subscriptionName;
 $scriptPath = Join-Path $PSScriptRoot "remote_scripts" "ResetLdapPasswordOnAD.ps1"
 $params = @{
-    samAccountName = "`"$($config.dsg.users.ldap.dsvm.samAccountName)`""
+    samAccountName = "`"$($config.sre.users.ldap.dsvm.samAccountName)`""
     ldapPassword = "`"$kvLdapPassword`""
 }
 Write-Host "Setting LDAP secret in local AD on: $($config.shm.dc.vmName)"
@@ -60,12 +60,12 @@ if ($success) {
 # Set LDAP secret on the VM
 # -------------------------
 Write-Host "Setting LDAP secret on compute VM: $($vm.Name)"
-$_ = Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
+$_ = Set-AzContext -SubscriptionId $config.sre.subscriptionName;
 $scriptPath = Join-Path $PSScriptRoot "remote_scripts" "reset_ldap_password.sh"
 $params = @{
     ldapPassword = "`"$kvLdapPassword`""
 }
-$result = Invoke-AzVMRunCommand -ResourceGroupName $config.dsg.dsvm.rg -Name $vm.Name `
+$result = Invoke-AzVMRunCommand -ResourceGroupName $config.sre.dsvm.rg -Name $vm.Name `
      -CommandId 'RunShellScript' -ScriptPath $scriptPath -Parameter $params
 $success = $?
 Write-Output $result.Value
