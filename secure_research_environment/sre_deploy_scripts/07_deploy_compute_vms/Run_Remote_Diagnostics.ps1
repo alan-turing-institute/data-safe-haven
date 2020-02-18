823ff8bf83bbad1588d7837d97989f0be1d53d9a
Tom Doel <tom.doel@codechoreography.co.uk>
2020-02-18 12:29:47 +0000

    Updated remote diagnostics script with config changes dsg->sre

--- secure_research_environment/sre_deploy_scripts/07_deploy_compute_vms/Run_Remote_Diagnostics.ps1
+++ secure_research_environment/sre_deploy_scripts/07_deploy_compute_vms/Run_Remote_Diagnostics.ps1
@@ -13,31 +13,31 @@ Import-Module $PSScriptRoot/../../../common_powershell/Configuration.psm1 -Force
 $config = Get-SreConfig ($sreId);
 $originalContext = Get-AzContext
 
-$_ = Set-AzContext -SubscriptionId $config.dsg.subscriptionName;
+$_ = Set-AzContext -SubscriptionId $config.sre.subscriptionName;
 
 
 # Find VM with private IP address matching the provided last octect
 ## Turn provided last octect into full IP address in the data subnet
-$vmIpAddress = ($config.dsg.network.subnets.data.prefix + "." + $ipLastOctet)
+$vmIpAddress = ($config.sre.network.subnets.data.prefix + "." + $ipLastOctet)
 Write-Host " - Finding VM with IP $vmIpAddress"
 ## Get all compute VMs
-$computeVms = Get-AzVM -ResourceGroupName $config.dsg.dsvm.rg
+$computeVms = Get-AzVM -ResourceGroupName $config.sre.dsvm.rg
 ## Get the NICs attached to all the compute VMs
-$computeVmNicIds = ($computeVms | ForEach-Object { (Get-AzVM -ResourceGroupName $config.dsg.dsvm.rg -Name $_.Name).NetworkProfile.NetworkInterfaces.Id })
-$computeVmNics = ($computeVmNicIds | ForEach-Object { Get-AzNetworkInterface -ResourceGroupName $config.dsg.dsvm.rg -Name $_.Split("/")[-1] })
+$computeVmNicIds = ($computeVms | ForEach-Object { (Get-AzVM -ResourceGroupName $config.sre.dsvm.rg -Name $_.Name).NetworkProfile.NetworkInterfaces.Id })
+$computeVmNics = ($computeVmNicIds | ForEach-Object { Get-AzNetworkInterface -ResourceGroupName $config.sre.dsvm.rg -Name $_.Split("/")[-1] })
 ## Filter the NICs to the one matching the desired IP address and get the name of the VM it is attached to
 $computeVmName = ($computeVmNics | Where-Object { $_.IpConfigurations.PrivateIpAddress -match $vmIpAddress })[0].VirtualMachine.Id.Split("/")[-1]
 
 # Run remote scripts
 $diagnostic_scripts = @("check_ldap_connection.sh","restart_name_resolution_service.sh","rerun_realm_join.sh","restart_sssd_service.sh")
 $testHost = $config.shm.dc.fqdn
-$ldapUser = $config.dsg.users.ldap.dsvm.samAccountName
+$ldapUser = $config.sre.users.ldap.dsvm.samAccountName
 $domainLower = $config.shm.domain.fqdn
 $servicePath = $config.shm.domain.serviceOuPath
 
 $params = @{
     TEST_HOST = $config.shm.dc.fqdn
-    LDAP_USER = $config.dsg.users.ldap.dsvm.samAccountName
+    LDAP_USER = $config.sre.users.ldap.dsvm.samAccountName
     DOMAIN_LOWER = $config.shm.domain.fqdn
     SERVICE_PATH = "'" + $config.shm.domain.serviceOuPath + "'"
 }
@@ -46,7 +46,7 @@ Write-Host " - Running diagnostic scripts on VM $computeVmName"
 
 foreach ($diagnostic_script in $diagnostic_scripts) {
     $scriptPath = Join-Path $PSScriptRoot "remote_scripts" $diagnostic_script
-    $result = Invoke-AzVMRunCommand -ResourceGroupName $config.dsg.dsvm.rg -Name "$computeVmName" `
+    $result = Invoke-AzVMRunCommand -ResourceGroupName $config.sre.dsvm.rg -Name "$computeVmName" `
          -CommandId 'RunShellScript' -ScriptPath $scriptPath `
          -Parameter $params
     Write-Output $result.Value;
