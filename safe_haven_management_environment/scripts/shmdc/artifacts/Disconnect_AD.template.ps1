b440088370d802cc8ae87410ee8eee36c70f0068
Tom Doel <tom.doel@codechoreography.co.uk>
2020-02-18 14:45:10 +0000

    Escape Cmdlet in Disconnect_AD template

--- safe_haven_management_environment/scripts/shmdc/artifacts/Disconnect_AD.template.ps1
+++ safe_haven_management_environment/scripts/shmdc/artifacts/Disconnect_AD.template.ps1
@@ -4,7 +4,7 @@ Write-Host "Please use username admin@$tmplShmFqdn and the $tmplAadPasswordName 
 Connect-MsolService
 Write-Host "Disabling directory synchronisation..."
 Set-MsolDirSyncEnabled -EnableDirSync `$False -Force
-Write-Host "Is directory synchronisation currently enabled? $((Get-MSOLCompanyInformation).DirectorySynchronizationEnabled)"
+Write-Host "Is directory synchronisation currently enabled? `$((Get-MSOLCompanyInformation).DirectorySynchronizationEnabled)"
 Write-Host "Removing any connected applications..."
 Get-MsolServicePrincipal | Select-Object DisplayName
 Get-MsolServicePrincipal | Remove-MsolServicePrincipal
