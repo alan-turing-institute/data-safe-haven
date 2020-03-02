1a985487680ceff15bd08925f9132903876a9309
Tom Doel <tom.doel@codechoreography.co.uk>
2020-03-02 16:22:43 +0000

    Disconnect_AD script will install required MSOnline module if needed

--- safe_haven_management_environment/scripts/shmdc/artifacts/Disconnect_AD.template.ps1
+++ safe_haven_management_environment/scripts/shmdc/artifacts/Disconnect_AD.template.ps1
@@ -1,5 +1,11 @@
 # NB. This solves the issue of orphaned AAD users when the local AD is deleted
 # https://support.microsoft.com/en-gb/help/2619062/you-can-t-manage-or-remove-objects-that-were-synchronized-through-the
+
+# Ensure that MSOnline is installed for current user
+if (-not (Get-Module -ListAvailable -Name MSOnline)) {
+    Install-Module -Name MSOnline -Scope CurrentUser -Force
+}
+
 Write-Host "Please use username admin@$tmplShmFqdn and the $tmplAadPasswordName from $tmplKeyVaultName."
 Connect-MsolService
 Write-Host "Disabling directory synchronisation..."
