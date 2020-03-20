8ce96371359882343b4b3c278673c5455140945b
James Robinson <james.em.robinson@gmail.com>
2020-03-20 14:33:53 +0000

    Removed drive mapping from GPOs

--- deployment/safe_haven_management_environment/remote/create_dc/scripts/Active_Directory_Configuration.ps1
+++ deployment/safe_haven_management_environment/remote/create_dc/scripts/Active_Directory_Configuration.ps1
@@ -152,7 +152,6 @@ Write-Host "Importing GPOs..."
 foreach ($backupTargetPair in (("0AF343A0-248D-4CA5-B19E-5FA46DAE9F9C", "All servers - Local Administrators"),
                                ("EE9EF278-1F3F-461C-9F7A-97F2B82C04B4", "All Servers - Windows Update"),
                                ("742211F9-1482-4D06-A8DE-BA66101933EB", "All Servers - Windows Services"),
-                               ("D320C1D9-52EF-47D8-80A7-1E73A457CDAD", "Research Users - Mapped Drives"),
                                ("B0A14FC3-292E-4A23-B280-9CC172D92FD5", "Session Servers - Remote Desktop Control"))) {
     $backup,$target = $backupTargetPair
     Import-GPO -BackupId "$backup" -TargetName "$target" -Path $oubackuppath -CreateIfNeeded
@@ -179,7 +178,6 @@ foreach ($gpoOuNamePair in (("All servers - Local Administrators", "Safe Haven S
                             ("All Servers - Windows Update", "Secure Research Environment Data Servers"),
                             ("All Servers - Windows Update", "Secure Research Environment RDS Session Servers"),
                             ("All Servers - Windows Update", "Secure Research Environment Service Servers"),
-                            ("Research Users - Mapped Drives", "Secure Research Environment RDS Session Servers"),
                             ("Session Servers - Remote Desktop Control", "Secure Research Environment RDS Session Servers"))) {
     $gpoName,$ouName = $gpoOuNamePair
     $gpo = Get-GPO -Name "$gpoName"
