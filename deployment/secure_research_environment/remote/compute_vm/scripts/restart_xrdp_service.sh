1d93cff2e27a2fe87c40e95fe410acf814b5c5c7
James Robinson <james.em.robinson@gmail.com>
2020-03-27 11:14:23 +0000

    Updated XRDP script

--- deployment/secure_research_environment/remote/compute_vm/scripts/restart_xrdp_service.sh
+++ deployment/secure_research_environment/remote/compute_vm/scripts/restart_xrdp_service.sh
@@ -8,31 +8,38 @@ BLUE="\033[0;36m"
 END="\033[0m"
 
 echo -e "${BLUE}Checking xrdp status${END}"
-STATUS_CMD="sudo systemctl status xrdp; sudo systemctl status xrdp-sesman"
+XRDP_CMD="systemctl status xrdp"
+XRDP_SESMAN_CMD="systemctl status xrdp-sesman"
+
 
 echo "Testing xrdp status..."
-STATUS=$(${STATUS_CMD})
-if [ "$(echo $STATUS | grep 'Active: failed')" != "" ]; then
-    echo -e "${RED}xrdp service has failed. Restarting...${END}"
+XRDP_STATUS=$(sudo ${XRDP_CMD} | grep 'Active: failed')
+XRDP_SESMAN_STATUS=$(sudo ${XRDP_SESMAN_CMD} | grep 'Active: failed')
+if [[ ("$XRDP_STATUS" != "") || ("$XRDP_SESMAN_STATUS" != "") ]]; then
+    echo -e "${RED}xrdp services have failed. Restarting...${END}"
     sudo systemctl restart xrdp
     sudo systemctl restart xrdp-sesman
 else
-    echo -e "${BLUE}xrdp service is working. No need to restart.${END}"
+    echo -e "${BLUE}xrdp services are working. No need to restart.${END}"
     echo "XRDP STATUS RESULT:"
-    echo "$STATUS"
+    sudo ${XRDP_CMD}
+    sudo ${XRDP_SESMAN_CMD}
     exit 0
 fi
 
 echo "Retesting xrdp status..."
-STATUS=$(${STATUS_CMD})
-if [ "$(echo $STATUS | grep 'Active: failed')" != "" ]; then
-    echo -e "${RED}xrdp service not working after restart.${END}"
+XRDP_STATUS=$(sudo ${XRDP_CMD} | grep 'Active: failed')
+XRDP_SESMAN_STATUS=$(sudo ${XRDP_SESMAN_CMD} | grep 'Active: failed')
+if [[ ("$XRDP_STATUS" != "") || ("$XRDP_SESMAN_STATUS" != "") ]]; then
+    echo -e "${RED}xrdp services are not working after restart.${END}"
     echo "XRDP STATUS RESULT:"
-    echo "$STATUS"
+    sudo ${XRDP_CMD}
+    sudo ${XRDP_SESMAN_CMD}
     exit 1
 else
-    echo -e "${BLUE}xrdp service is working after restart.${END}"
+    echo -e "${BLUE}xrdp services are working after restart.${END}"
     echo "XRDP STATUS RESULT:"
-    echo "$STATUS"
+    sudo ${XRDP_CMD}
+    sudo ${XRDP_SESMAN_CMD}
     exit 0
 fi
