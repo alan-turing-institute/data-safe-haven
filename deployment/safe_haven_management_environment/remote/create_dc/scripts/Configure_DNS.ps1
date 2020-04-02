ddddbd22cedfa982e99b1056a6d08dcf2a927e13
James Robinson <james.em.robinson@gmail.com>
2020-04-02 17:15:12 +0100

    Add a DNS forwarder to the two SHM DCs

---
+++ deployment/safe_haven_management_environment/remote/create_dc/scripts/Configure_DNS.ps1
@@ -0,0 +1,10 @@
+﻿# Use Microsoft Azure DNS server for resolving external addresses
+# https://docs.microsoft.com/en-us/azure/virtual-network/what-is-ip-address-168-63-129-16
+Write-Host "Forward external DNS requests to Microsoft Azure DNS server..."
+Add-DnsServerForwarder -IPAddress 168.63.129.16 -PassThru
+if ($?) {
+    Write-Host " [o] Successfully created/updated DNS forwarding"
+} else {
+    Write-Host " [x] Failed to create/update DNS forwarding!"
+}
+
