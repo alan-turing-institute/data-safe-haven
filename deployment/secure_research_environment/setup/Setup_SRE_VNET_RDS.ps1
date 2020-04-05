fc4d286006c123feefc7d94d35c5e01a455d905e
James Robinson <james.em.robinson@gmail.com>
2020-04-05 13:02:05 +0100

    Move reboots to the end of the process

--- deployment/secure_research_environment/setup/Setup_SRE_VNET_RDS.ps1
+++ deployment/secure_research_environment/setup/Setup_SRE_VNET_RDS.ps1
@@ -448,6 +448,13 @@ $result = Invoke-RemoteScript -Shell "PowerShell" -ScriptPath $scriptPath -VMNam
 Write-Output $result.Value
 
 
+# Add VMs to correct NSG
+# ----------------------
+Add-VmToNSG -VMName $config.sre.rds.gateway.vmName -NSGName $config.sre.rds.gateway.nsg
+Add-VmToNSG -VMName $config.sre.rds.sessionHost1.vmName -NSGName $config.sre.rds.sessionHost1.nsg
+Add-VmToNSG -VMName $config.sre.rds.sessionHost2.vmName -NSGName $config.sre.rds.sessionHost2.nsg
+
+
 # Reboot all the RDS VMs
 # ----------------------
 foreach ($nameVMNameParamsPair in $vmNamePairs) {
@@ -460,13 +467,6 @@ foreach ($nameVMNameParamsPair in $vmNamePairs) {
         Add-LogMessage -Level Fatal "Rebooting the ${name} failed!"
     }
 }
-
-
-# Add VMs to correct NSG
-# ----------------------
-Add-VmToNSG -VMName $config.sre.rds.gateway.vmName -NSGName $config.sre.rds.gateway.nsg
-Add-VmToNSG -VMName $config.sre.rds.sessionHost1.vmName -NSGName $config.sre.rds.sessionHost1.nsg
-Add-VmToNSG -VMName $config.sre.rds.sessionHost2.vmName -NSGName $config.sre.rds.sessionHost2.nsg
 
 
 # Switch back to original subscription
