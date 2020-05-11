bbb965b43dc0ef461c10019fcccce06ec345d074
Jack Roberts <jroberts@turing.ac.uk>
2020-05-11 12:55:54 +0100

    check_merge_requests docstrings, comments and formatting

--- deployment/secure_research_environment/cloud_init/scripts/check_merge_requests.py
+++ deployment/secure_research_environment/cloud_init/scripts/check_merge_requests.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+Check merge requests on gitlab external, approve them where appropriate,
+and push the approved repos to gitlab internal.
+
+1) Get open merge requests in the approval group on gitlab external.
+2) Check whether any of them meet the approval conditions. By default: status
+is can be merged, not flagged as work in progress, no unresolved discussions,
+at least two upvotes, and no downvotes.
+3) Accept approved merge requests (merged unapproved repo into approval repo).
+4) Push whole approval repo to gitlab internal, creating the repo if it doesn't
+already exist.
+
+This script creates two log files in the same directory that it is run from:
+* check_merge_requests.log : A verbose log of the steps performed in each run
+and any errors encountered.
+* accepted_merge_requests.log : A list of merge requests that have been accepted
+in CSV format with columns merged time, source project, source branch, source
+commit, target project, target branch and target commit.
+"""
+
 import requests
 import subprocess
 from urllib.parse import quote as url_quote
@@ -5,6 +27,8 @@ from pathlib import Path
 import logging
 from logging.handlers import RotatingFileHandler
 
+# Setup logging to console and file. File uses RotatingFileHandler to create
+# logs over a rolling window, 10 files each max 5 MB in size.
 logger = logging.getLogger("merge_requests_logger")
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
@@ -17,37 +41,39 @@ c_handler.setFormatter(formatter)
 logger.addHandler(f_handler)
 logger.addHandler(c_handler)
 
-global HOME
-global GL_INTERNAL_IP
-global GL_INTERNAL_TOKEN
-global GL_INTERNAL_AUTH_HEADER
 
-HOME = str(Path.home())
+def internal_project_exists(repo_name, config):
+    """Determine whether a repo exist in the ingress namespace on
+    GITLAB-INTERNAL.
 
-with open(f"{HOME}/.secrets/gitlab-internal-ip-address", "r") as f:
-    GL_INTERNAL_IP = f.readlines()[0].strip()
+    Parameters
+    ----------
+    repo_name : str
+        The name of a repo (not a URL) to search for in the ingress namespace
+        on GITLAB-INTERNAL.
+    config : dict
+        GITLAB-INTERNAL details and secrets as returned by get_gitlab_config
 
-GL_INTERNAL_URL = "http://" + GL_INTERNAL_IP + "/api/v4"
+    Returns
+    -------
+    tuple
+        (exists, url) tuple where exists: boolean - does repo_name exist on
+        GITLAB-INTERNAL?, and url: str - the ssh url to the repo (when 'exists'
+        is true)
 
-with open(f"{HOME}/.secrets/gitlab-internal-api-token", "r") as f:
-    GL_INTERNAL_TOKEN = f.readlines()[0].strip()
-
-GL_INTERNAL_AUTH_HEADER = {"Authorization": "Bearer " + GL_INTERNAL_TOKEN}
-
-
-def internal_project_exists(repo_name):
-    """Given a string (the name of a repo - not a URL), returns a pair
-    (exists, url):
-    - exists: boolean - does repo_name exist on GITLAB-INTERNAL?
-    - url: str - the ssh url to the repo (when 'exists' is true)
+    Raises
+    ------
+    requests.HTTPError
+        If API request returns an unexpected code (not 404 or 200)
     """
 
     # build url-encoded repo_name
-    repo_path_encoded = url_quote("ingress/" + repo_name, safe='')
+    repo_path_encoded = url_quote("ingress/" + repo_name, safe="")
 
     # Does repo_name exist on GITLAB-INTERNAL?
-    response = requests.get(GL_INTERNAL_URL + '/projects/' + repo_path_encoded,
-                            headers=GL_INTERNAL_AUTH_HEADER)
+    response = requests.get(
+        config["api_url"] + "projects/" + repo_path_encoded, headers=config["headers"]
+    )
 
     if response.status_code == 404:
         return (False, "")
@@ -57,73 +83,152 @@ def internal_project_exists(repo_name):
         # Not using `response.raise_for_status()`, since we also want
         # to raise an exception on unexpected "successful" responses
         # (not 200)
-        raise requests.HTTPError("Unexpected response: " + response.reason
-                                 + ", content: " + response.text)
+        raise requests.HTTPError(
+            "Unexpected response: " + response.reason + ", content: " + response.text
+        )
 
 
-def internal_update_repo(gh_url, repo_name):
-    """Takes a GitHub URL, `gh_url`, which should be the URL to the
-    "APPROVED" repo, clones it and pushes all branches to the repo
-    `repo_name` owned by 'ingress' on GITLAB-INTERNAL, creating it
+def internal_update_repo(git_url, repo_name, config):
+    """Takes a git URL, `git_url`, which should be the URL to the
+    "APPROVED" repo on GITLAB-EXTERNAL, clones it and pushes all branches to
+    the repo `repo_name` owned by 'ingress' on GITLAB-INTERNAL, creating it
     there first if it doesn't exist.
+
+    Parameters
+    ----------
+    git_url : str
+        URL to the "APPROVED" repo on GITLAB-EXTERNAL
+    repo_name : str
+        Name of repo to create on GITLAB-INTERNAL.
+    config : dict
+        GITLAB-INTERNAL details and secrets as returned by get_gitlab_config
     """
-    # clone the repo from gh_url (on GITLAB-EXTERNAL), removing any of
+
+    # clone the repo from git_url (on GITLAB-EXTERNAL), removing any of
     # the same name first (simpler than checking if it exists, has the
     # same remote and pulling)
     subprocess.run(["rm", "-rf", repo_name], check=True)
-    subprocess.run(["git", "clone", gh_url, repo_name], check=True)
+    subprocess.run(["git", "clone", git_url, repo_name], check=True)
 
-    project_exists, gl_internal_repo_url = internal_project_exists(repo_name)
+    project_exists, gl_internal_repo_url = internal_project_exists(repo_name, config)
 
     # create the project if it doesn't exist
     if not project_exists:
         print("Creating: " + repo_name)
-        response = requests.post(GL_INTERNAL_URL + '/projects',
-                                 headers=GL_INTERNAL_AUTH_HEADER,
-                                 data={"name": repo_name,
-                                       "visibility": "public"})
+        response = requests.post(
+            config["api_url"] + "projects",
+            headers=config["headers"],
+            data={"name": repo_name, "visibility": "public"},
+        )
         response.raise_for_status()
-        assert(response.json()["path_with_namespace"] == "ingress/" + repo_name)
+        assert response.json()["path_with_namespace"] == "ingress/" + repo_name
 
         gl_internal_repo_url = response.json()["ssh_url_to_repo"]
 
     # Set the remote
-    subprocess.run(["git", "remote", "add", "gitlab-internal",
-                    gl_internal_repo_url], cwd=repo_name, check=True)
+    subprocess.run(
+        ["git", "remote", "add", "gitlab-internal", gl_internal_repo_url],
+        cwd=repo_name,
+        check=True,
+    )
 
     # Force push current contents of all branches
-    subprocess.run(["git", "push", "--force", "--all",
-                    "gitlab-internal"], cwd=repo_name, check=True)
+    subprocess.run(
+        ["git", "push", "--force", "--all", "gitlab-internal"],
+        cwd=repo_name,
+        check=True,
+    )
 
 
 def get_request(endpoint, headers, params=None):
+    """Wrapper around requests.get that returns a JSON if request was successful
+    or raises a HTTPError otherwise.
+
+    Parameters
+    ----------
+    endpoint : str
+        URL of API endpoint to call
+    headers : dict
+        Request headers
+    params : dict, optional
+        Request parameters
+
+    Returns
+    -------
+    dict
+        JSON of request result
+
+    Raises
+    ------
+    requests.HTTPError
+        If not r.ok, raise HTTPError with details of failure
+    """
     r = requests.get(endpoint, headers=headers, params=params)
     if r.ok:
         return r.json()
     else:
-        raise ValueError(
+        raise requests.HTTPError(
             f"Request failed: URL {endpoint}, CODE {r.status_code}, CONTENT {r.content}"
         )
 
 
 def put_request(endpoint, headers, params=None):
+    """Wrapper around requests.put that returns a JSON if request was successful
+    or raises a HTTPError otherwise.
+
+    Parameters
+    ----------
+    endpoint : str
+        URL of API endpoint to call
+    headers : dict
+        Request headers
+    params : dict, optional
+        Request parameters
+
+    Returns
+    -------
+    dict
+        JSON of request result
+
+    Raises
+    ------
+    requests.HTTPError
+        If not r.ok, raise HTTPError with details of failure
+    """
     r = requests.put(endpoint, headers=headers, params=params)
     if r.ok:
         return r.json()
     else:
-        raise ValueError(
+        raise requests.HTTPError(
             f"Request failed: URL {endpoint}, CODE {r.status_code}, CONTENT {r.content}"
         )
 
 
 def get_gitlab_config(server="external"):
+    """Get gitlab server details and user account secrets
+
+    Parameters
+    ----------
+    server : str, optional
+        Which server to get secrets for either "internal" or, by default "external"
+
+    Returns
+    -------
+    dict
+        Secrets api_url, api_token, ip and headers.
+
+    Raises
+    ------
+    ValueError
+        If server is not a supported value
+    """
     home = str(Path.home())
 
     if server == "external":
         with open(f"{home}/.secrets/gitlab-external-ip-address", "r") as f:
-           ip = f.readlines()[0].strip()
+            ip = f.readlines()[0].strip()
         with open(f"{home}/.secrets/gitlab-external-api-token", "r") as f:
-           token = f.readlines()[0].strip()
+            token = f.readlines()[0].strip()
     elif server == "internal":
         with open(f"{home}/.secrets/gitlab-internal-ip-address", "r") as f:
             ip = f.readlines()[0].strip()
@@ -139,6 +244,25 @@ def get_gitlab_config(server="external"):
 
 
 def get_group_id(group_name, config):
+    """Get the ID of a group on a gitlab server.
+
+    Parameters
+    ----------
+    group_name : str
+        Group name to find.
+    config : dict
+        Gitlab details and secrets as returned by get_gitlab_config
+
+    Returns
+    -------
+    int
+        Group ID for group_name
+
+    Raises
+    ------
+    ValueError
+        If group_name not found in the groups returned from the gitlab server.
+    """
     endpoint = config["api_url"] + "groups"
     response = get_request(endpoint, headers=config["headers"])
     for group in response:
@@ -148,12 +272,39 @@ def get_group_id(group_name, config):
 
 
 def get_project(project_id, config):
+    """Get the details of a project from its ID.
+
+    Parameters
+    ----------
+    project_id : int
+        ID of the project on the gitlab server.
+    config : dict
+        Gitlab details and secrets as returned by get_gitlab_config
+
+    Returns
+    -------
+    dict
+        Project JSON as returned by the gitlab API.
+    """
     endpoint = config["api_url"] + f"projects/{project_id}"
     project = get_request(endpoint, headers=config["headers"])
     return project
 
 
 def get_merge_requests_for_approval(config):
+    """Get the details of all open merge requests into the approval group on
+    a gitlab server.
+
+    Parameters
+    ----------
+    config : dict
+        Gitlab details and secrets as returned by get_gitlab_config
+
+    Returns
+    -------
+    list
+        List of merge requests JSONs as returned by the gitlab API.
+    """
     group = get_group_id("approval", config)
     endpoint = config["api_url"] + f"/groups/{group}/merge_requests"
     response = get_request(
@@ -163,6 +314,22 @@ def get_merge_requests_for_approval(config):
 
 
 def count_unresolved_mr_discussions(mr, config):
+    """Count the number of unresolved discussions a merge request has. Requires
+    calling the discussions API endpoint for the merge request to determine
+    each comment's resolved status.
+
+    Parameters
+    ----------
+    mr : dict
+        A merge request JSON as returned by the gitlab API
+    config : dict
+        Gitlab details and secrets as returned by get_gitlab_config
+
+    Returns
+    -------
+    int
+        Number of unresolved discussions.
+    """
     if mr["user_notes_count"] == 0:
         return 0
     project_id = mr["project_id"]
@@ -183,6 +350,22 @@ def count_unresolved_mr_discussions(mr, config):
 
 
 def accept_merge_request(mr, config):
+    """Accept a merge request
+
+    Parameters
+    ----------
+    mr : dict
+        For the merge request to approve: The merge request JSON as returned by
+        the gitlab API.
+    config : dict
+        Gitlab details and secrets as returned by get_gitlab_config
+
+    Returns
+    -------
+    dict
+        JSON response from gitlab API representing the status of the accepted
+        merge request.
+    """
     project_id = mr["project_id"]
     mr_iid = mr["iid"]
     endpoint = (
@@ -192,17 +375,22 @@ def accept_merge_request(mr, config):
 
 
 def check_merge_requests():
+    """Main function to check merge requests in the approval group on gitlab external,
+    approve them where appropriate, and then push the approved repos to gitlab
+    internal.
+    """
     logger.info(f"STARTING RUN")
 
     try:
-        config = get_gitlab_config(server="external")
+        config_external = get_gitlab_config(server="external")
+        config_internal = get_gitlab_config(server="internal")
     except Exception as e:
         logger.critical(f"Failed to load gitlab secrets: {e}")
         return
 
     logger.info("Getting open merge requests for approval")
     try:
-        merge_requests = get_merge_requests_for_approval(config)
+        merge_requests = get_merge_requests_for_approval(config_external)
     except Exception as e:
         logger.critical(f"Failed to get merge requests: {e}")
         return
@@ -212,19 +400,25 @@ def check_merge_requests():
         logger.info("-" * 20)
         logger.info(f"Merge request {i+1} out of {len(merge_requests)}")
         try:
-            source_project = get_project(mr["source_project_id"], config)
+            # Extract merge request details
+            source_project = get_project(mr["source_project_id"], config_external)
             logger.info(f"Source Project: {source_project['name_with_namespace']}")
             logger.info(f"Source Branch: {mr['source_branch']}")
-            target_project = get_project(mr["project_id"], config)
+            target_project = get_project(mr["project_id"], config_external)
             logger.info(f"Target Project: {target_project['name_with_namespace']}")
             logger.info(f"Target Branch: {mr['target_branch']}")
             logger.info(f"Commit SHA: {mr['sha']}")
             logger.info(f"Created At: {mr['created_at']}")
             status = mr["merge_status"]
-            logger.info(f"Merge Status: {status}")
+            if status != "can_be_merged":
+                # Should never get merge conflicts so if we do something has
+                # gone wrong - log an error
+                logger.error(f"Merge Status: {status}")
+            else:
+                logger.info(f"Merge Status: {status}")
             wip = mr["work_in_progress"]
             logger.info(f"Work in Progress: {wip}")
-            unresolved = count_unresolved_mr_discussions(mr, config)
+            unresolved = count_unresolved_mr_discussions(mr, config_external)
             logger.info(f"Unresolved Discussions: {unresolved}")
             upvotes = mr["upvotes"]
             logger.info(f"Upvotes: {upvotes}")
@@ -242,13 +436,14 @@ def check_merge_requests():
         ):
             logger.info("Merge request has been approved. Proceeding with merge.")
             try:
-                result = accept_merge_request(mr, config)
+                result = accept_merge_request(mr, config_external)
             except Exception as e:
                 logger.error(f"Merge failed! {e}")
                 continue
             if result["state"] == "merged":
                 logger.info(f"Merge successful! Merge SHA {result['merge_commit_sha']}")
                 try:
+                    # Save details of accepted merge request to separate log file
                     with open("accepted_merge_requests.log", "a") as f:
                         f.write(
                             f"{result['merged_at']}, {source_project['name_with_namespace']}, {mr['source_branch']}, {mr['sha']}, {target_project['name_with_namespace']}, {mr['target_branch']}, {result['merge_commit_sha']}\n"
@@ -256,9 +451,11 @@ def check_merge_requests():
                 except Exception as e:
                     logger.error(f"Failed to log accepted merge request: {e}")
                 try:
+                    logger.info("Pushing project to gitlab internal.")
                     internal_update_repo(
                         target_project["ssh_url_to_repo"],
-                        target_project["name"]
+                        target_project["name"],
+                        config_internal,
                     )
                 except Exception as e:
                     logger.error(f"Failed to push to internal: {e}")
