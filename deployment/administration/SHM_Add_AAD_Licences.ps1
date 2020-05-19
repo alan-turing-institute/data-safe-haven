6687b2c9883ab7a984c143743b926b519ba29587
Martin O'Reilly <developer@mtspace.net>
2020-05-19 19:19:00 +0100

    Take SHM ID rather than Tenant ID as parameter
    Connect to Azure AD using SHM domain from config

--- deployment/administration/SHM_Add_AAD_Licences.ps1
+++ deployment/administration/SHM_Add_AAD_Licences.ps1
@@ -1,15 +1,21 @@
 param(
-  [Parameter(Mandatory = $true,HelpMessage = "Enter tenant ID for the relevant Azure Active Directory")]
-  [string]$tenantId,
+  [Parameter(Mandatory = $true, HelpMessage = "Enter SHM ID")]
+  [string]$shmId,
   [Parameter(Mandatory = $false,HelpMessage = "Sku for the licence you want to assign")]
   [string]$licenceSku = "AAD_PREMIUM"
 )
 
 Import-Module AzureAD.Standard.Preview
+Import-Module $PSScriptRoot/../common/Configuration.psm1 -Force
 Import-Module $PSScriptRoot/../common/Logging.psm1 -Force
 
+$config = Get-ShmFullConfig $shmId
+$shmDomain = $config.domain.fqdn
+
 # Connect to the Azure AD
-$_ = Connect-AzureAD -TenantId $tenantId
+# We connect within the script to ensure we are connected to the right Azure AD for the SHM
+Add-LogMessage -Level Info "Connecting to Azure AD for '$shmDomain'..."
+$_ = Connect-AzureAD -TenantId $shmDomain
 
 
 # Get the appropriate licence
@@ -24,3 +30,9 @@ Add-LogMessage -Level Info "Preparing to add licence '$licenceSku' ($($Licence.S
 $unlicensedUsers = Get-AzureAdUser | Where-Object { -Not $_.AssignedLicenses } | Where-Object { $_.OnPremisesSecurityIdentifier }
 Add-LogMessage -Level Info "Assigning licences to $($unlicensedUsers.Count) unlicenced users"
 $unlicensedUsers | Set-AzureADUserLicense -AssignedLicenses $LicencesToAssign
+
+# Disconnect from AzureAD
+# We disconnect from the SHM Azure AD to ensure the user does not assume they are still 
+# connected to a different Azure AD they may have been connected to prior to running this
+# script and therefore perform subsequent operations against the wrong Azure AD
+Add-LogMessage -Level Info "Disconnecting from AzureAD for '$shmDomain"
