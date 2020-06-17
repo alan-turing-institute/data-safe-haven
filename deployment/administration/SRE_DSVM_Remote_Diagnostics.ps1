48e904eb16053c59cff436f27a4b5a19a9a12b05
James Robinson <james.em.robinson@gmail.com>
2020-06-17 13:53:09 +0100

    Switched sreId to configId for administration scripts

--- deployment/administration/SRE_DSVM_Remote_Diagnostics.ps1
+++ deployment/administration/SRE_DSVM_Remote_Diagnostics.ps1
@@ -1,6 +1,6 @@
 param(
-    [Parameter(Position = 0,Mandatory = $true,HelpMessage = "Enter SRE ID (a short string) e.g 'sandbox' for the sandbox environment")]
-    [string]$sreId,
+    [Parameter(Position = 0, Mandatory = $true, HelpMessage = "Enter SRE config ID. This will be the concatenation of <SHM ID> and <SRE ID> (eg. 'testasandbox' for SRE 'sandbox' in SHM 'testa')")]
+    [string]$configId,
     [Parameter(Position = 1,Mandatory = $true,HelpMessage = "Enter last octet of compute VM IP address (e.g. 160)")]
     [string]$ipLastOctet
 )
@@ -13,7 +13,7 @@ Import-Module $PSScriptRoot/../common/Logging.psm1 -Force
 
 # Get config and original context before changing subscription
 # ------------------------------------------------------------
-$config = Get-SreConfig $sreId
+$config = Get-SreConfig $configId
 $originalContext = Get-AzContext
 $_ = Set-AzContext -SubscriptionId $config.sre.subscriptionName
 
