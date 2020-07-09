97c031c9ffbdef410db5a9e3e30700eca7aae77e
Martin O'Reilly <developer@mtspace.net>
2020-07-10 00:00:28 +0100

    Restore SHM log workspace configuration

--- deployment/safe_haven_management_environment/setup/Setup_SHM_Logging.ps1
+++ deployment/safe_haven_management_environment/setup/Setup_SHM_Logging.ps1
@@ -36,6 +36,137 @@ foreach ($resourceGroup in $shmResourceGroups) {
 Add-LogMessage -Level Info "Finished ensuring Log Analytics agent is installed on all SHM VMs."
 
 
+# Ensure required Windows event logs are collected
+# ------------------------------------------------
+Add-LogMessage -Level Info "Ensuring required Windows event logs are being collected...'"
+$eventLogNames = @(
+    "Active Directory Web Services"
+    "Directory Service",
+    "DFS Replication",
+    "DNS Server",
+    "Microsoft-Windows-Security-Netlogon/Operational",
+    "Microsoft-Windows-Winlogon/Operational",
+    "System"
+)
+
+foreach ($eventLogName in $eventLogNames) {
+    $sourceName = "windows-event-$eventLogName".
+        Replace("%","percent").Replace("/","-per-").Replace(" ","-").ToLower()
+    $source = Get-AzOperationalInsightsDataSource `
+        -ResourceGroupName $config.logging.rg `
+        -WorkspaceName $config.logging.workspaceName `
+        -Name $sourceName
+    if($source) {  
+        Add-LogMessage -Level InfoSuccess "Logging already active for '$eventLogName'."  
+    } else {
+        $null = New-AzOperationalInsightsWindowsEventDataSource `
+            -ResourceGroupName $config.logging.rg `
+            -WorkspaceName $config.logging.workspaceName `
+            -Name $sourceName `
+            -EventLogName $eventLogName `
+            -CollectErrors `
+            -CollectWarnings `
+            -CollectInformation
+        if($?) {
+            Add-LogMessage -Level Success "Logging activated for '$eventLogName'."
+        } else {
+            Add-LogMessage -Level Fatal "Failed to activate logging for '$eventLogName'!"
+        }
+    }
+}
+
+
+# Ensure require Windows performance counters are collected
+# ---------------------------------------------------------
+Add-LogMessage -Level Info "Ensuring required Windows performance counters are being collected...'"
+$counters = @(
+    @{setName = "LogicalDisk"; counterName = "Avg. Disk sec/Read"},
+    @{setName = "LogicalDisk"; counterName = "Avg. Disk sec/Write"},
+    @{setName = "LogicalDisk"; counterName = "Current Disk Queue Length"},
+    @{setName = "LogicalDisk"; counterName = "Disk Reads/sec"},
+    @{setName = "LogicalDisk"; counterName = "Disk Transfers/sec"},
+    @{setName = "LogicalDisk"; counterName = "Disk Writes/sec"},
+    @{setName = "LogicalDisk"; counterName = "Free Megabytes"},
+    @{setName = "Memory"; counterName = "Available MBytes"},
+    @{setName = "Memory"; counterName = "% Committed Bytes In Use"},
+    @{setName = "LogicalDisk"; counterName = "% Free Space"},
+    @{setName = "Processor"; counterName = "% Processor Time"},
+    @{setName = "System"; counterName = "Processor Queue Length"}
+)
+foreach ($counter in $counters) {
+    $sourceName = "windows-counter-$($counter.setName)-$($counter.counterName)".
+        Replace("%","percent").Replace("/","-per-").Replace(" ","-").ToLower()
+    $source = Get-AzOperationalInsightsDataSource `
+        -ResourceGroupName $config.logging.rg `
+        -WorkspaceName $config.logging.workspaceName `
+        -Name $sourceName
+    if($source) {
+        Add-LogMessage -Level InfoSuccess "Logging already active for '$($counter.setName)/$($counter.counterName)'"
+    } else {
+        $null = New-AzOperationalInsightsWindowsPerformanceCounterDataSource `
+            -ResourceGroupName $config.logging.rg `
+            -WorkspaceName $config.logging.workspaceName `
+            -ObjectName $counter.setName `
+            -InstanceName "*" `
+            -CounterName $counter.counterName `
+            -IntervalSeconds 60 `
+            -Name $sourceName
+        if($?) {
+            Add-LogMessage -Level Success "Logging activated for '$($counter.setName)/$($counter.counterName)'."
+        } else {
+            Add-LogMessage -Level Fatal "Failed to activate logging for '$($counter.setName)/$($counter.counterName)'!"
+        }
+    }
+}
+
+
+# Activate required Intelligence Packs
+# ------------------------------------
+Add-LogMessage -Level Info "Ensuring required Log Analytics Intelligence Packs are enabled...'"
+$packNames = @(
+    "AntiMalware",
+    "ADReplication",
+    "AzureNetworking",
+    "AzureNSGAnalytics",
+    "Backup",
+    "CapacityPerformance",
+    "ChangeTracking",
+    "DHCPActivity",
+    "DnsAnalytics",
+    "InternalWindowsEvent",
+    "NetFlow",
+    "NetworkMonitoring",
+    "Security",
+    "SecurityCenterFree",
+    "SecurityCenterNetworkTraffic",
+    "SecurityInsights",
+    "ServiceMap",
+    "Updates",
+    "WindowsDefenderATP",
+    "WindowsEventForwarding",
+    "WindowsFirewall",
+    "WinLog",
+    "VMInsights"
+)
+foreach ($packName in $packNames) {
+    $pack = Get-AzOperationalInsightsIntelligencePack `
+        -WorkspaceName $config.logging.workspaceName `
+        -ResourceGroupName $config.logging.rg | Where-Object { $_.Name -eq $packName }
+    if($pack.Enabled) {
+        Add-LogMessage -Level InfoSuccess "'$packName' Intelligence Pack already enabled."
+    } else {
+        $pack = Set-AzOperationalInsightsIntelligencePack `
+            -WorkspaceName $config.logging.workspaceName `
+            -ResourceGroupName $config.logging.rg `
+            -IntelligencePackName $packName -Enabled $true
+        if($?) {
+            Add-LogMessage -Level Success "'$packName' Intelligence Pack enabled."
+        } else {
+            Add-LogMessage -Level Fatal "Failed to enable '$packName' Intelligence Pack!"
+        }
+    }
+
+
 # Switch back to original subscription
 # ------------------------------------
 $null = Set-AzContext -Context $originalContext
