10dc15c9c2d75a86ec98702cebea025e86dff2f8
James Robinson <james.em.robinson@gmail.com>
2020-07-10 18:53:07 +0100

    Added new script that ensures all expected drives are mounted on the DSVM

---
+++ deployment/secure_research_environment/remote/compute_vm/scripts/check_drive_mounts.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# This script is designed to be deployed to an Azure Linux VM via
+# the Powershell Invoke-AzVMRunCommand, which sets all variables
+# passed in its -Parameter argument as environment variables
+
+RED="\033[0;31m"
+BLUE="\033[0;36m"
+END="\033[0m"
+
+MOUNT_POINTS=("/data" "/home" "/scratch" "/shared" "/output")
+echo -e "${BLUE}Checking drives are mounted...${END}"
+for MOUNT_POINT in ${MOUNT_POINTS[@]}; do
+    if [ "$(mount | grep $MOUNT_POINT)" ]; then
+        echo -e "${BLUE} [o] ${MOUNT_POINT} is mounted...${END}"
+        df -h  | grep $MOUNT_POINT
+    else
+        echo -e "${RED}Attempting to mount ${MOUNT_POINT}...${END}"
+        mount $MOUNT_POINT
+    fi
+done
+sleep 30
+
+echo -e "${BLUE}Rechecking drives are mounted...${END}"
+for MOUNT_POINT in ${MOUNT_POINTS[@]}; do
+    if [ "$(mount | grep $MOUNT_POINT)" ]; then
+        echo -e "${BLUE} [o] ${MOUNT_POINT} is mounted...${END}"
+        df -h  | grep $MOUNT_POINT
+    else
+        echo -e "${RED} [x] ${MOUNT_POINT} is not currently mounted...${END}"
+    fi
+done
