0368bf99b23da12d75179e6018f8df9bfbecfeda
James Robinson <james.em.robinson@gmail.com>
2020-07-10 22:51:28 +0100

    Fix shell linting

--- deployment/secure_research_environment/remote/compute_vm/scripts/check_drive_mounts.sh
+++ deployment/secure_research_environment/remote/compute_vm/scripts/check_drive_mounts.sh
@@ -9,7 +9,7 @@ END="\033[0m"
 
 MOUNT_POINTS=("/data" "/home" "/scratch" "/shared" "/output")
 echo -e "${BLUE}Checking drives are mounted...${END}"
-for MOUNT_POINT in ${MOUNT_POINTS[@]}; do
+for MOUNT_POINT in "${MOUNT_POINTS[@]}"; do
     if [ "$(mount | grep $MOUNT_POINT)" ]; then
         echo -e "${BLUE} [o] ${MOUNT_POINT} is mounted...${END}"
     else
@@ -20,7 +20,7 @@ done
 sleep 30
 
 echo -e "${BLUE}Rechecking drives are mounted...${END}"
-for MOUNT_POINT in ${MOUNT_POINTS[@]}; do
+for MOUNT_POINT in "${MOUNT_POINTS[@]}"; do
     if [ "$(mount | grep $MOUNT_POINT)" ]; then
         echo -e "${BLUE} [o] ${MOUNT_POINT} is mounted...${END}"
         df -h  | grep $MOUNT_POINT
