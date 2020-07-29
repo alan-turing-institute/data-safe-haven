0f4f332c9af47b3fdde6faf5ca122dedbfea3907
Martin O'Reilly <developer@mtspace.net>
2020-07-29 02:33:48 +0100

    Disable weak TLS cipher suites
    Disable all TLS cipher suites installed on a Windows Server 2019 VM deployed on Azure
    on 09 July 2020 and *not* listed in the table of preferred secure cipher suites at
    https://www.acunetix.com/blog/articles/tls-ssl-cipher-hardening/
    
    We exclude known weak suites, rather than restrict ourselved to known strong suites,
    as additional stronger suites may be introduced over time (e.g. with the rollout of general
    availability support for TLS 3.0).

--- deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
+++ deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
@@ -38,3 +38,53 @@ Disable-Protocol -Protocol "SSL 2.0"
 Disable-Protocol -Protocol "SSL 3.0"
 Disable-Protocol -Protocol "TLS 1.0"
 Disable-Protocol -Protocol "TLS 1.1"
+
+# Disable weak cipher suites
+Write-Output "Ensuring weak TLS cipher suites are disabled..."
+# The following list of weak cipher suites are the ones listed as supported
+# on a Windows Server 2019 VM deployed on Azure on 09 July 2020 and *not*
+# listed in the table of preferred secure cipher suites at
+# https://www.acunetix.com/blog/articles/tls-ssl-cipher-hardening/
+# NOTE: We exclude known weak suites, rather than restrict ourselved to
+# known strong suites, as additional stronger suites may be introduced 
+# over time (e.g. with the rollout of general availability support for
+# TLS 3.0).
+$weakCipherSuites = @(
+    "TLS_AES_256_GCM_SHA384",
+    "TLS_AES_128_GCM_SHA256",
+    "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
+    "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
+    "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
+    "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
+    "TLS_RSA_WITH_NULL_SHA256",
+    "TLS_RSA_WITH_NULL_SHA",
+    "TLS_PSK_WITH_AES_256_GCM_SHA384",
+    "TLS_PSK_WITH_AES_128_GCM_SHA256",
+    "TLS_PSK_WITH_AES_256_CBC_SHA384",
+    "TLS_PSK_WITH_AES_128_CBC_SHA256",
+    "TLS_PSK_WITH_NULL_SHA384",
+    "TLS_PSK_WITH_NULL_SHA256",
+    "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
+    "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
+    "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+    "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+    "TLS_RSA_WITH_AES_256_GCM_SHA384",
+    "TLS_RSA_WITH_AES_128_GCM_SHA256",
+    "TLS_RSA_WITH_AES_256_CBC_SHA256",
+    "TLS_RSA_WITH_AES_128_CBC_SHA256",
+    "TLS_RSA_WITH_AES_256_CBC_SHA",
+    "TLS_RSA_WITH_AES_128_CBC_SHA",
+    "TLS_RSA_WITH_3DES_EDE_CBC_SHA"
+)
+foreach ($cipherSuite in $WeakCipherSuites) {
+    if(Get-TlsCipherSuite -Name $cipherSuite) {
+        Disable-TlsCipherSuite -Name $cipherSuite
+        if ($?) {
+            Write-Output " [o] Disabled '$cipherSuite' suite."
+        } else {
+            Write-Output " [x] Failed to ensure '$cipherSuite' suite is disabled."
+        }
+    } else {
+        Write-Output " [o] '$cipherSuite' suite already disabled."
+    }
+}
