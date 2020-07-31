e34a0ab35002819ee2d21f830d73a6f3c35bf378
James Robinson <james.em.robinson@gmail.com>
2020-07-31 17:33:15 +0200

    Moved BATS install to cloud-init file otherwise there is no internet-accessibility

--- deployment/secure_research_environment/remote/compute_vm/scripts/upload_smoke_tests.sh
+++ deployment/secure_research_environment/remote/compute_vm/scripts/upload_smoke_tests.sh
@@ -15,12 +15,5 @@ chmod -R 644 smoke_tests/
 chmod ugo+x smoke_tests/ smoke_tests/tests/ smoke_tests/package_lists/
 chmod ugo+rx smoke_tests/tests/*.{jl,py,sh,R}
 
-# Install bats
-git clone https://github.com/bats-core/bats-core /opt/bats/bats-core
-git clone https://github.com/bats-core/bats-support /opt/bats/bats-support
-git clone https://github.com/bats-core/bats-assert /opt/bats/bats-assert
-git clone https://github.com/bats-core/bats-file /opt/bats/bats-file
-/opt/bats/bats-core/install.sh /usr/local
-
 # Show final outputs
 ls -alh /opt/verification/smoke_tests/*
