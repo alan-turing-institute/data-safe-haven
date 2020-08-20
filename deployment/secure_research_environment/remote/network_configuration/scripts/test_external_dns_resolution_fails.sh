53c4b03f704c91b251bea1dc7f565301da9b09f8
Martin O'Reilly <developer@mtspace.net>
2020-08-20 01:40:55 +0100

    Add tests for external DNS esolution via Azue Platform DNS
    Also tidy up output of remote test script and add test outcome summary at end of test output.

--- deployment/secure_research_environment/remote/network_configuration/scripts/test_external_dns_resolution_fails.sh
+++ deployment/secure_research_environment/remote/network_configuration/scripts/test_external_dns_resolution_fails.sh
@@ -3,25 +3,41 @@
 
 confirm_external_dns_lookup_fails () {
     FQDN=$1
+    DNS_SERVER=$2
+    if [ -z "$DNS_SERVER" ];then    
+        DNS_SERVER_MSG="default DNS servers" 
+    else  
+        DNS_SERVER_MSG="$DNS_SERVER"
+    fi
     sudo systemd-resolve --flush-caches;
-    NS_RESULT=$(nslookup $FQDN)
+    NS_RESULT=$(nslookup $FQDN $DNS_SERVER)
     if [ $? -eq 0 ]; then
-        echo " [x] DNS lookup for '$FQDN' unexpectedly succeeded."
-        echo "$NS_RESULT"
+        echo -e " [x] DNS lookup for '$FQDN' unexpectedly succeeded via $DNS_SERVER_MSG."
+        echo -e "$NS_RESULT\n"
+        return 1
     else
-        echo " [o] DNS lookup for '$FQDN' failed as expected."
+        echo -e " [o] DNS lookup for '$FQDN' failed as expected via $DNS_SERVER_MSG."
+        return 0
     fi
 }
 
 confirm_internal_dns_lookup_succeeds () {
     FQDN=$1
+    DNS_SERVER=$2
+    if [ -z "$DNS_SERVER" ];then    
+        DNS_SERVER_MSG="default DNS servers" 
+    else  
+        DNS_SERVER_MSG="$DNS_SERVER"
+    fi
     sudo systemd-resolve --flush-caches;
-    NS_RESULT=$(nslookup $FQDN)
+    NS_RESULT=$(nslookup $FQDN $DNS_SERVER)
     if [ $? -eq 0 ]; then
-        echo " [o] DNS lookup for '$FQDN' succeeded as expected."
+        echo -e " [o] DNS lookup for '$FQDN' succeeded as expected via $DNS_SERVER_MSG."
+        return 0
     else
-        echo " [o] DNS lookup for '$FQDN' unexpectedly failed."
-        echo "$NS_RESULT"
+        echo -e " [o] DNS lookup for '$FQDN' unexpectedly failed via $DNS_SERVER_MSG."
+        echo -e "$NS_RESULT\n"
+        return 1
     fi
 }
 
@@ -29,17 +45,32 @@ echo " Testing DNS resolution lockdown"
 echo " -------------------------------"
 echo " $HOSTNAME> $(date +"%Y-%m-%dT%H:%M:%S")"
 
-echo "Testing DNS lookup for internal FQDNs..."
-confirm_internal_dns_lookup_succeeds "$SHM_DOMAIN_FQDN"
-confirm_internal_dns_lookup_succeeds "$SHM_DC1_FQDN"
-confirm_internal_dns_lookup_succeeds "$SHM_DC2_FQDN"
+FAILED_TESTS=0
+echo -e "\nTesting DNS lookup for internal FQDNs via default DNS servers..."
+confirm_internal_dns_lookup_succeeds "$SHM_DOMAIN_FQDN" || FAILED_TESTS=$(($FAILED_TESTS+1))
+confirm_internal_dns_lookup_succeeds "$SHM_DC1_FQDN" || FAILED_TESTS=$(($FAILED_TESTS+1))
+confirm_internal_dns_lookup_succeeds "$SHM_DC2_FQDN" || FAILED_TESTS=$(($FAILED_TESTS+1))
 
-echo "Testing DNS lookup for non-existent external domains'..."
-confirm_external_dns_lookup_fails "fail.example.com"
+echo -e "\nTesting DNS lookup for non-existent external domains via default DNS servers..."
+confirm_external_dns_lookup_fails "fail.example.com" || FAILED_TESTS=$(($FAILED_TESTS+1))
 
-echo "Testing DNS lookup for resolvable external domains..."
-confirm_external_dns_lookup_fails "example.com"
-confirm_external_dns_lookup_fails "doi.org"
-confirm_external_dns_lookup_fails "bbc.co.uk"
-confirm_external_dns_lookup_fails "google.com"
-confirm_external_dns_lookup_fails "facebook.com"
+echo -e "\nTesting DNS lookup for resolvable external domains via default DNS servers..."
+confirm_external_dns_lookup_fails "example.com" || FAILED_TESTS=$(($FAILED_TESTS+1))
+confirm_external_dns_lookup_fails "doi.org" || FAILED_TESTS=$(($FAILED_TESTS+1))
+confirm_external_dns_lookup_fails "google.com" || FAILED_TESTS=$(($FAILED_TESTS+1))
+confirm_external_dns_lookup_fails "facebook.com" || FAILED_TESTS=$(($FAILED_TESTS+1))
+
+echo -e "\nTesting DNS lookup for non-existent external domains via Azure Platform DNS servers..."
+confirm_external_dns_lookup_fails "fail.example.com" "168.63.129.16" || FAILED_TESTS=$(($FAILED_TESTS+1))
+
+echo -e "\nTesting DNS lookup for resolvable external domains via Azure Platform  DNS servers..."
+confirm_external_dns_lookup_fails "example.com" "168.63.129.16" || FAILED_TESTS=$(($FAILED_TESTS+1))
+confirm_external_dns_lookup_fails "doi.org" "168.63.129.16" || FAILED_TESTS=$(($FAILED_TESTS+1))
+confirm_external_dns_lookup_fails "google.com" "168.63.129.16" || FAILED_TESTS=$(($FAILED_TESTS+1))
+confirm_external_dns_lookup_fails "facebook.com" "168.63.129.16" || FAILED_TESTS=$(($FAILED_TESTS+1))
+
+if [ $FAILED_TESTS -eq 0 ]; then
+    echo -e "\n *** SUCCESS: All tests passed! ***"
+else
+    echo -e "\n *** ERROR: $FAILED_TESTS test(s) failed! ***"
+fi
