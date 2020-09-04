07d0984599b88048a3922d44c890514a57814cb7
James Robinson <james.em.robinson@gmail.com>
2020-09-04 12:48:26 +0100

    Updated guacamole version

--- deployment/secure_research_environment/setup/Setup_SRE_Guacamole_Servers.ps1
+++ deployment/secure_research_environment/setup/Setup_SRE_Guacamole_Servers.ps1
@@ -27,15 +27,15 @@ $vmAdminUsername = Resolve-KeyVaultSecret -VaultName $config.sre.keyVault.Name -
 $vmAdminPassword = Resolve-KeyVaultSecret -VaultName $config.sre.keyVault.Name -SecretName $config.sre.keyVault.secretNames.guacamoleAdminPassword
 
 # Get/set Duo secrets
-$_ = Set-AzContext -SubscriptionId $config.shm.subscriptionName
+$null = Set-AzContext -SubscriptionId $config.shm.subscriptionName
 $duoIntegrationKey = Resolve-KeyVaultSecret -VaultName $config.shm.keyVault.Name -SecretName $config.shm.keyVault.secretNames.duoIntegrationKey -DefaultValue $duoIntegration
 $duoSecretKey = Resolve-KeyVaultSecret -VaultName $config.shm.keyVault.Name -SecretName $config.shm.keyVault.secretNames.duoSecretKey -DefaultValue $duoSecret
 $duoApiHostname = Resolve-KeyVaultSecret -VaultName $config.shm.keyVault.Name -SecretName $config.shm.keyVault.secretNames.duoApiHostname -DefaultValue $duoApiHost
-$_ = Set-AzContext -SubscriptionId $config.sre.subscriptionName
+$null = Set-AzContext -SubscriptionId $config.sre.subscriptionName
 
 # Create RDS resource group if it does not exist
 # ----------------------------------------------
-$_ = Deploy-ResourceGroup -Name $config.sre.guacamole.rg -Location $config.sre.location
+$null = Deploy-ResourceGroup -Name $config.sre.guacamole.rg -Location $config.sre.location
 
 # Construct the cloud-init yaml file for the target subscription
 # --------------------------------------------------------------
@@ -71,17 +71,17 @@ $ldapSearchPassword = Resolve-KeyVaultSecret -VaultName $config.sre.keyVault.Nam
 
 
 $cloudInitTemplate = Join-Path $PSScriptRoot ".." "cloud_init" "cloud-init-guacamole.template.yaml" | Get-Item | Get-Content -Raw
-$cloudInitYaml = $cloudInitTemplate.Replace('<ldap-user-base-dn>', $config.shm.domain.userOuPath).
-                                    Replace('<ldap-hostname>', "$(($config.shm.dc.hostname).ToUpper()).$(($config.shm.domain.fqdn).ToLower())").
-                                    Replace('<ldap-port>', 389).
-                                    Replace('<ldap-group-base-dn>', $config.shm.domain.securityOuPath).
-                                    Replace('<ldap-search-bind-dn>', "CN=" + $config.sre.users.ldap.dsvm.Name + "," + $config.shm.domain.serviceOuPath).
-                                    Replace('<ldap-search-bind-password>', $ldapSearchPassword).
-                                    Replace('<ldap-group-researchers>', $config.sre.domain.securityGroups.researchUsers.Name).
-                                    Replace('<postgres-password>', $guacamoleDbPassword).
-                                    Replace('<duo-api-hostname>', $duoApiHost).
-                                    Replace('<duo-integration-key>', $duoIntegrationKey).
-                                    Replace('<duo-secret-key>', $duoSecretKey).
+$cloudInitYaml = $cloudInitTemplate.Replace("<ldap-user-base-dn>", $config.shm.domain.userOuPath).
+                                    Replace("<ldap-hostname>", "$(($config.shm.dc.hostname).ToUpper()).$(($config.shm.domain.fqdn).ToLower())").
+                                    Replace("<ldap-port>", 389).
+                                    Replace("<ldap-group-base-dn>", $config.shm.domain.securityOuPath).
+                                    Replace("<ldap-search-bind-dn>", "CN=" + $config.sre.users.ldap.dsvm.Name + "," + $config.shm.domain.serviceOuPath).
+                                    Replace("<ldap-search-bind-password>", $ldapSearchPassword).
+                                    Replace("<ldap-group-researchers>", $config.sre.domain.securityGroups.researchUsers.Name).
+                                    Replace("<postgres-password>", $guacamoleDbPassword).
+                                    Replace("<duo-api-hostname>", $duoApiHost).
+                                    Replace("<duo-integration-key>", $duoIntegrationKey).
+                                    Replace("<duo-secret-key>", $duoSecretKey).
                                     Replace("<shm-dc-ip-address>", $config.shm.dc.ip).
                                     Replace("<ldap-users-base-dn>", $config.shm.domain.userOuPath).
                                     Replace("<ldap-user-filter>", "(&(objectClass=user)(memberOf=CN=$($config.sre.domain.securityGroups.researchUsers.Name),$($config.shm.domain.securityOuPath)))").
@@ -118,7 +118,7 @@ $diskType = "Standard_LRS"
 # -------------------------------------
 $vmNic = Deploy-VirtualMachineNIC -Name "$vmName-NIC" -ResourceGroupName $config.sre.guacamole.rg -Subnet $subnet -PrivateIpAddress $config.sre.guacamole.ip -Location $config.sre.location
 $publicIP = New-AzPublicIpAddress -Name "$vmName-PIP" -ResourceGroupName $config.sre.guacamole.rg -AllocationMethod Static -IdleTimeoutInMinutes 4 -Location $config.sre.location -Force
-$_ = $vmNic | Set-AzNetworkInterfaceIpConfig -Name $vmNic.ipConfigurations[0].Name -SubnetId $subnet.Id -PublicIpAddressId $publicIP.Id | Set-AzNetworkInterface
+$null = $vmNic | Set-AzNetworkInterfaceIpConfig -Name $vmNic.ipConfigurations[0].Name -SubnetId $subnet.Id -PublicIpAddressId $publicIP.Id | Set-AzNetworkInterface
 
 
 # Deploy the VM
@@ -136,7 +136,7 @@ $params = @{
     ResourceGroupName = $config.sre.guacamole.rg
     Size = $vmSize
 }
-$_ = Deploy-UbuntuVirtualMachine @params
+$null = Deploy-UbuntuVirtualMachine @params
 
 # Poll VM to see whether it has finished running
 Add-LogMessage -Level Info "Waiting for cloud-init provisioning to finish (this will take 5+ minutes)..."
@@ -167,7 +167,7 @@ if ($?) {
 
 
 # # Add DNS records to SRE DNS Zone
-# $_ = Set-AzContext -SubscriptionId $config.shm.dns.subscriptionName
+# $null = Set-AzContext -SubscriptionId $config.shm.dns.subscriptionName
 # $baseDnsRecordname = "@"
 # $gatewayDnsRecordname = "$($config.sre.rds.gateway.hostname)".ToLower()
 # $dnsResourceGroup = $config.shm.dns.rg
