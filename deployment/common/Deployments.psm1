ac16808616d6c98ce8b8e998e20c8fd48e43e036
James Robinson <james.em.robinson@gmail.com>
2020-10-08 13:40:40 +0100

    Iterate over all regions when purging key vault as it might be in another region than the one you are deploying to

--- deployment/common/Deployments.psm1
+++ deployment/common/Deployments.psm1
@@ -402,13 +402,19 @@ function Deploy-KeyVault {
     $keyVault = Get-AzKeyVault -VaultName $Name -ResourceGroupName $ResourceGroupName -ErrorVariable notExists -ErrorAction SilentlyContinue
     if ($null -eq $keyVault) {
         # Purge any existing soft-deleted key vault
-        if (Get-AzKeyVault -VaultName $Name -Location $Location -InRemovedState) {
-            Add-LogMessage -Level Info "Purging a soft-deleted key vault '$Name'"
-            Remove-AzKeyVault -VaultName $Name -Location $Location -InRemovedState -Force | Out-Null
-            if ($?) {
-                Add-LogMessage -Level Success "Purged key vault '$Name'"
-            } else {
-                Add-LogMessage -Level Fatal "Failed to purge key vault '$Name'!"
+        foreach ($existingLocation in (Get-AzLocation | ForEach-Object { $_.Location })) {
+            try {
+                if (Get-AzKeyVault -VaultName $Name -Location $existingLocation -InRemovedState -ErrorAction Stop) {
+                    Add-LogMessage -Level Info "Purging a soft-deleted key vault '$Name' in $existingLocation"
+                    Remove-AzKeyVault -VaultName $Name -Location $existingLocation -InRemovedState -Force | Out-Null
+                    if ($?) {
+                        Add-LogMessage -Level Success "Purged key vault '$Name'"
+                    } else {
+                        Add-LogMessage -Level Fatal "Failed to purge key vault '$Name'!"
+                    }
+                }
+            } catch [Microsoft.Rest.Azure.CloudException] {
+                continue  # Running Get-AzKeyVault on a location which does not support soft-deleted key vaults causes an error which we catch here
             }
         }
         # Create a new key vault
