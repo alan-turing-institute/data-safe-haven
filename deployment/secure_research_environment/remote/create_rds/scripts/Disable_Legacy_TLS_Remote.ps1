1a723c36e92d7948e2ebc5656f53cf7e44c7dcd0
James Robinson <james.em.robinson@gmail.com>
2020-10-12 14:24:26 +0100

    Allow all 'secure' and 'recommended' ciphers

--- deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
+++ deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
@@ -61,55 +61,48 @@ Set-Protocol -Protocol "TLS 1.1" -Action Disable
 Set-Protocol -Protocol "TLS 1.2" -Action Enable
 
 
-# Disable 'weak' ciphers using no encryption or weak encryption (RC4 or 3DES)
-# ---------------------------------------------------------------------------
-$weakCipherSuites = @(Get-TlsCipherSuite -Name "WITH_NULL" | ForEach-Object { $_.Name }) `
-                  + @(Get-TlsCipherSuite -Name "WITH_RC4" | ForEach-Object { $_.Name }) `
-                  + @(Get-TlsCipherSuite -Name "WITH_3DES" | ForEach-Object { $_.Name })
+# Get all 'recommended' and 'secure' ciphers from ciphersuite.info
+# Note that we need 'secure' since none of the 'recommended' ciphers are currently supported by Microsoft Remote Desktop
+# ----------------------------------------------------------------------------------------------------------------------
+$response = Invoke-RestMethod -Uri https://ciphersuite.info/api/cs/security/recommended
+$recommended = $response.ciphersuites | ForEach-Object { Get-Member -InputObject $_ -MemberType NoteProperty } | Select-Object -Property Name | ForEach-Object { $_.Name }
+$response = Invoke-RestMethod -Uri https://ciphersuite.info/api/cs/security/secure
+$secure = $response.ciphersuites | ForEach-Object { Get-Member -InputObject $_ -MemberType NoteProperty } | Select-Object -Property Name | ForEach-Object { $_.Name }
+$allowedCiphers = @($recommended) + @($secure)
 
 
-# Disable the following ciphers which are 'secure' but not 'recommended'
-# ----------------------------------------------------------------------
-# SHA1 is a deprecated hash function
-$weakCipherSuites += @(Get-TlsCipherSuite -Name "SHA" | ForEach-Object { $_.Name } | Where-Object { $_ -like "*SHA" })
-# PSK (pre-shared key) is a less secure key exchange method than DHE (Diffie-Hellman ephemeral)
-$weakCipherSuites += @(Get-TlsCipherSuite -Name "TLS_PSK" | ForEach-Object { $_.Name })
-# Plain RSA is potentially subject to the ROBOT attack (https://robotattack.org/)
-$weakCipherSuites += @(Get-TlsCipherSuite -Name "TLS_RSA" | ForEach-Object { $_.Name })
-# Plain AES without key exchange/authentication is still secure but disfavoured
-$weakCipherSuites += @(Get-TlsCipherSuite -Name "TLS_AES" | ForEach-Object { $_.Name })
-
-# Disable requested cipher suites
-# -------------------------------
-Write-Output "Disabling weak TLS cipher suites..."
-foreach ($cipherSuite in ($weakCipherSuites | Sort-Object | Get-Unique)) {
-    if (Get-TlsCipherSuite -Name $cipherSuite) {
+# Disable all ciphers that are not in the allowed list
+# ----------------------------------------------------
+foreach ($cipherSuite in $(Get-TlsCipherSuite | ForEach-Object { $_.Name })) {
+    if ($cipherSuite -notin $allowedCiphers) {
         Disable-TlsCipherSuite -Name $cipherSuite
         if ($?) {
             Write-Output " [o] Disabled '$cipherSuite' suite."
         } else {
             Write-Output " [x] Failed to ensure '$cipherSuite' suite is disabled."
         }
-    } else {
-        Write-Output " [o] '$cipherSuite' suite already disabled."
     }
 }
 
+
+# Enable all ciphers that are in the allowed list
+# -----------------------------------------------
+foreach ($cipherSuite in $allowedCiphers) {
+    if ($cipherSuite -notin $(Get-TlsCipherSuite | ForEach-Object { $_.Name })) {
+        Enable-TlsCipherSuite -Name $cipherSuite
+        if ($?) {
+            Write-Output " [o] Enabled '$cipherSuite' suite."
+        } else {
+            Write-Output " [x] Failed to ensure '$cipherSuite' suite is enabled."
+        }
+    }
+}
+
+
 # List all cipher suites that are still allowed
-# At the time of writing this is the following:
-# ... TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 (recommended)
-# ... TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 (recommended)
-# ... TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384 (recommended)
-# ... TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256 (recommended)
-# ... TLS_DHE_RSA_WITH_AES_256_GCM_SHA384 (recommended)
-# ... TLS_DHE_RSA_WITH_AES_128_GCM_SHA256 (recommended)
-# ... TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256 (secure)
-# ... TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384 (secure)
-# ... TLS_DHE_DSS_WITH_AES_128_CBC_SHA256 (secure)
-# ... TLS_DHE_DSS_WITH_AES_256_CBC_SHA256 (secure)
-# ... TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256 (secure)
-# ... TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384 (secure)
-# Note that GCM is preferred over CBC but some browsers do not support it yet
-# ---------------------------------------------------------------------------
+# ---------------------------------------------
 Write-Output "Allowed cipher suites are:"
-[System.Object[]](Get-TlsCipherSuite) | ForEach-Object { Write-Host "... $($_.Name)" }
+(Get-TlsCipherSuite) | ForEach-Object { Write-Host "... $($_.Name)" }
+
+
+
