e6ad948483b2bba4d17d170a7d21d99ec1e95393
James Robinson <james.em.robinson@gmail.com>
2020-10-12 13:21:54 +0100

    Switch Toggle to Action

--- deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
+++ deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
@@ -1,23 +1,23 @@
 # Throwing an exception in a remote script means we have to wait 90 minutes for the remote call to time out
 # Accordingly we don't validate any parameters or make them mandatory
-# In reality, all three parameters are required and both 'Role' and 'Toggle' will only accept certain values
+# In reality, all three parameters are required and both 'Role' and 'Action' will only accept certain values
 function Set-ProtocolForRole {
     param(
         [Parameter(HelpMessage = "Name of protocol")]
         $Protocol,
         [Parameter(HelpMessage = "Name of protocol [must be 'Server' or 'Client']")]
         $Role,
-        [Parameter(HelpMessage = "Set whether we are enabling or disabling [must be 'Enabled' or 'Disabled']")]
-        $Toggle
+        [Parameter(HelpMessage = "Set whether we are enabling or disabling [must be 'Enable' or 'Disable']")]
+        $Action
     )
-    if ($Toggle -eq "Enabled") {
+    if ($Action -eq "Enable") {
         $EnabledValue = 1
         $DisabledByDefaultValue = 0
-    } elseif ($Toggle -eq "Disabled") {
+    } elseif ($Action -eq "Disable") {
         $EnabledValue = 0
         $DisabledByDefaultValue = 1
     } else {
-        Write-Output " [x] Could not interpret '$Toggle'. Please use either 'Enabled' or 'Disabled'."
+        Write-Output " [x] Could not interpret '$Action'. Please use either 'Enable' or 'Disable'."
         return
     }
 
@@ -28,9 +28,9 @@ function Set-ProtocolForRole {
     # Check status
     $status = Get-Item "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\$Protocol\$Role"
     if (($status.GetValue("Enabled") -eq $EnabledValue) -and ($status.GetValue("DisabledByDefault") -eq $DisabledByDefaultValue)) {
-        Write-Output " [o] '$Protocol' protocol is $Toggle for '$Role' role."
+        Write-Output " [o] '$Protocol' protocol is '${Action}d' for '$Role' role."
     } else {
-        Write-Output " [x] Failed to ensure '$Protocol' protocol is $Toggle for '$Role' role."
+        Write-Output " [x] Failed to ensure '$Protocol' protocol is '${Action}d' for '$Role' role."
         Write-Output $status
     }
 }
@@ -39,26 +39,26 @@ function Set-Protocol {
     param(
         [Parameter(HelpMessage = "Name of protocol")]
         $Protocol,
-        [Parameter(HelpMessage = "Set whether we are enabling or disabling [must be 'Enabled' or 'Disabled']")]
-        $Toggle
+        [Parameter(HelpMessage = "Set whether we are enabling or disabling [must be 'Enable' or 'Disable']")]
+        $Action
     )
-    Write-Output "Ensuring '$Protocol' is ${Toggle}..."
-    Set-ProtocolForRole -Protocol $Protocol -Role "Client" -Toggle $Toggle
-    Set-ProtocolForRole -Protocol $Protocol -Role "Server" -Toggle $Toggle
+    Write-Output "Ensuring '$Protocol' is ${Action}..."
+    Set-ProtocolForRole -Protocol $Protocol -Role "Client" -Action $Action
+    Set-ProtocolForRole -Protocol $Protocol -Role "Server" -Action $Action
 }
 
 
 # Disable all legacy protocols
 # ----------------------------
-Set-Protocol -Protocol "SSL 2.0" -Toggle Disabled
-Set-Protocol -Protocol "SSL 3.0" -Toggle Disabled
-Set-Protocol -Protocol "TLS 1.0" -Toggle Disabled
-Set-Protocol -Protocol "TLS 1.1" -Toggle Disabled
+Set-Protocol -Protocol "SSL 2.0" -Action Disable
+Set-Protocol -Protocol "SSL 3.0" -Action Disable
+Set-Protocol -Protocol "TLS 1.0" -Action Disable
+Set-Protocol -Protocol "TLS 1.1" -Action Disable
 
 
 # Explicitly enable TLS 1.2
 # -------------------------
-Set-Protocol -Protocol "TLS 1.2" -Toggle Enabled
+Set-Protocol -Protocol "TLS 1.2" -Action Enable
 
 
 # Disable 'weak' ciphers using no encryption or weak encryption (RC4 or 3DES)
