0ca4f0479cf482c7bff60e67484bdc9b9bb0c48a
James Robinson <james.em.robinson@gmail.com>
2020-10-13 13:20:45 +0100

    Removed Get-IpAddressFromRangeAndOffset as Get-NextAvailableIpInRange can do the same thing

--- deployment/common/Networking.psm1
+++ deployment/common/Networking.psm1
@@ -106,36 +106,6 @@ function Convert-OctetsToIpAddress {
 Export-ModuleMember -Function Convert-OctetsToIpAddress
 
 
-# Get full IP address from CIDR IP range and offset
-# -------------------------------------------------
-function Get-IpAddressFromRangeAndOffset {
-    param(
-        [Parameter(Mandatory = $true, HelpMessage = "Name of VM to run on")]
-        [string]$IpRangeCidr,
-        [Parameter(Mandatory = $true, HelpMessage = "Offset to apply before returning an IP address")]
-        [int]$Offset
-    )
-        # Split the input CIDR into the base address and the CIDR mask
-        $baseIpAddress, $cidrSuffix = $IpRangeCidr.Split("/")
-        $baseIpOctets = $baseIpAddress.Split(".")
-        [uint32]$ipNumber = $($baseIpOctets[0] -shl 24) + $($baseIpOctets[1] -shl 16) + $($baseIpOctets[2] -shl 8) + $baseIpOctets[3]
-
-        # Construct the CIDR mask as a series of '1's followed by enough '0's to fill out a 32-bit integer
-        [uint32]$cidrMask = 0
-        @(1) * $cidrSuffix + @(0) * (32 - $cidrSuffix) | ForEach-Object { $cidrMask = ($cidrMask -shl 1) + $_ }
-
-        # Get the starting and ending IP addresses
-        $ipStart = $ipNumber -band $cidrMask
-        $ipEnd = $ipNumber -bor (-bnot $cidrMask)
-
-        # Return the IP Address at the requeste offser
-        $ipAddresses = $ipStart..$ipEnd | ForEach-Object { [System.Net.IPAddress]::parse($_).IPAddressToString }
-
-        return $ipAddresses | Select-Object -First (1 + $Offset) | Select-Object -Last 1
-}
-Export-ModuleMember -Function Get-IpAddressFromRangeAndOffset
-
-
 # Get next available IP address in range
 # --------------------------------------
 function Get-NextAvailableIpInRange {
--- deployment/secure_research_environment/setup/Block_External_DNS_Queries.ps1
+++ deployment/secure_research_environment/setup/Block_External_DNS_Queries.ps1
@@ -52,7 +52,7 @@ if (-not $dsvmIpLastOctet) {
     Add-LogMessage -Level Warning "Test DSVM not specified by providing last octet of its IP address. Attempting to test on DSVM with last octet of '$defaultDsvmIpLastOctet'."
     $dsvmIpLastOctet = $defaultDsvmIpLastOctet
 }
-$vmIpAddress = Get-IpAddressFromRangeAndOffset -IpRangeCidr $config.sre.network.vnet.subnets.data.cidr -Offset $dsvmIpLastOctet
+$vmIpAddress = Get-NextAvailableIpInRange -IpRangeCidr $config.sre.network.vnet.subnets.data.cidr -Offset $dsvmIpLastOctet
 $existingNic = Get-AzNetworkInterface | Where-Object { $_.IpConfigurations.PrivateIpAddress -eq $vmIpAddress }
 if (-not $existingNic) {
     Add-LogMessage -Level Fatal "No network card found with IP address '$vmIpAddress'. Cannot run test to confirm external DNS resolution is blocked."
