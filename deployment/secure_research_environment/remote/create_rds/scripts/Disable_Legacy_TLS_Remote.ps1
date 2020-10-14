409a68ae50482d61d73d13cef27f024fdfdd0489
James Robinson <james.em.robinson@gmail.com>
2020-10-14 14:59:33 +0100

    Be more explicit about listing allowed/disallowed ciphers

--- deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
+++ deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
@@ -67,27 +67,33 @@ $recommended = $response.ciphersuites | ForEach-Object { $_.PSObject.Properties.
 # Desktop webclient. We choose the three RSA + AES + CBC + SHA256 algorithms.
 # ------------------------------------------------------------------------------
 $response = Invoke-RestMethod -Uri https://ciphersuite.info/api/cs/security/secure -ErrorAction Stop
-$secure = $response.ciphersuites | ForEach-Object { $_.PSObject.Properties.Value.gnutls_name = $_.PSObject.Properties.Name; $_.PSObject.Properties.Value } ` # we overwrite gnutls_name in the Values hashtable with the `Name` property for later use
+# Note that we overwrite gnutls_name in the Values hashtable with the `Name` property for later use
+$secure = $response.ciphersuites | ForEach-Object { $_.PSObject.Properties.Value.gnutls_name = $_.PSObject.Properties.Name; $_.PSObject.Properties.Value } `
                                  | Where-Object { $_.kex_algorithm -ne "DHE" } `
                                  | Where-Object { $_.auth_algorithm -eq "RSA" } `
                                  | Where-Object { $_.enc_algorithm -like "AES * CBC" } `
                                  | Where-Object { $_.hash_algorithm -eq "SHA256" } `
                                  | ForEach-Object { $_.gnutls_name }
 
+# Construct allowed/disallowed lists
+# ----------------------------------
 $allowedCiphers = @($secure) + @($recommended)
+$disallowedCiphers = Get-TlsCipherSuite | ForEach-Object { $_.Name } | Where-Object { -not $allowedCiphers.Contains($_) }
 
 
 # Disable all ciphers that are not in the allowed list
 # ----------------------------------------------------
 Write-Output "Disabling any disallowed ciphersuites..."
-foreach ($unsupportedCipher in $(Get-TlsCipherSuite | ForEach-Object { $_.Name } | Where-Object { -not $allowedCiphers.Contains($_) })) {
-    # Note that running this command on eg. TLS_DHE_RSA_WITH_AES_128_CCM will also disable TLS_DHE_RSA_WITH_AES_128_CCM_8
-    if (Get-TlsCipherSuite -Name $unsupportedCipher) {
-        Disable-TlsCipherSuite -Name $unsupportedCipher
+foreach ($disallowedCipher in $disallowedCiphers) {
+    # Note that running Disable-TlsCipherSuite on eg. TLS_DHE_RSA_WITH_AES_128_CCM will also disable TLS_DHE_RSA_WITH_AES_128_CCM_8
+    # We therefore check whether the cipher still exists before disabling it.
+    # By disabling before enabling, we ensure that this will not remove any ciphers that we want to keep.
+    if (Get-TlsCipherSuite -Name $disallowedCipher) {
+        Disable-TlsCipherSuite -Name $disallowedCipher
         if ($?) {
-            Write-Output " [o] Disabled '$unsupportedCipher' suite."
+            Write-Output " [o] Disabled '$disallowedCipher' suite."
         } else {
-            Write-Output " [x] Failed to ensure '$unsupportedCipher' suite is disabled."
+            Write-Output " [x] Failed to ensure '$disallowedCipher' suite is disabled."
         }
     }
 }
@@ -99,15 +105,16 @@ Write-Output "Enabling all allowed ciphersuites..."
 foreach ($allowedCipher in $allowedCiphers) {
     if (-not $(Get-TlsCipherSuite | ForEach-Object { $_.Name }).Contains($allowedCipher)) {
         Enable-TlsCipherSuite -Name $allowedCipher
-        if (-not $?) {
-            Write-Output " [x] Failed to ensure '$allowedCipher' suite is enabled."
-        } else {
-            # If this cipher is not supported by Windows [ie. it has no CipherSuite entry] then immediately disable it
+        if ($?) {
+            # Check whether this cipher is supported by Windows.
+            # If it is not [ie. it has no CipherSuite entry] then immediately disable it.
             if ($((Get-TlsCipherSuite -Name $allowedCipher).CipherSuite) -eq 0) {
                 Disable-TlsCipherSuite -Name $allowedCipher
             } else {
                 Write-Output " [o] Enabled '$allowedCipher' suite."
             }
+        } else {
+            Write-Output " [x] Failed to ensure '$allowedCipher' suite is enabled."
         }
     }
 }
