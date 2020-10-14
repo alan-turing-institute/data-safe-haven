8d4446bea32151effee9030cc858e3548f6c94ef
James Robinson <james.em.robinson@gmail.com>
2020-10-14 13:54:34 +0100

    Recommended ciphersuites plus three weaker fallbacks

--- deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
+++ deployment/secure_research_environment/remote/create_rds/scripts/Disable_Legacy_TLS_Remote.ps1
@@ -56,26 +56,36 @@ Set-Protocol -Protocol "TLS 1.0" -Action Disable
 Set-Protocol -Protocol "TLS 1.1" -Action Disable
 
 
-# Get all 'recommended' and 'secure' ciphers from ciphersuite.info
-# Note that we need 'secure' since none of the 'recommended' ciphers are currently supported by Microsoft Remote Desktop
-# ----------------------------------------------------------------------------------------------------------------------
-$response = Invoke-RestMethod -Uri https://ciphersuite.info/api/cs/security/recommended
-$recommended = $response.ciphersuites | ForEach-Object { Get-Member -InputObject $_ -MemberType NoteProperty } | Select-Object -Property Name | ForEach-Object { $_.Name }
-$response = Invoke-RestMethod -Uri https://ciphersuite.info/api/cs/security/secure
-$secure = $response.ciphersuites | ForEach-Object { Get-Member -InputObject $_ -MemberType NoteProperty } | Select-Object -Property Name | ForEach-Object { $_.Name }
-$allowedCiphers = @($recommended) + @($secure)
+# Get all 'recommended' ciphers from ciphersuite.info
+# ---------------------------------------------------
+$response = Invoke-RestMethod -Uri https://ciphersuite.info/api/cs/security/recommended -ErrorAction Stop
+$recommended = $response.ciphersuites | ForEach-Object { $_.PSObject.Properties.Name }
+
+
+# ... however we also need at least one cipher from the 'secure' list since none
+# of the 'recommended' ciphers are currently supported by the Microsoft Remote
+# Desktop webclient. We choose the three RSA + AES + CBC + SHA256 algorithms.
+# ------------------------------------------------------------------------------
+$response = Invoke-RestMethod -Uri https://ciphersuite.info/api/cs/security/secure -ErrorAction Stop
+$secure = $response.ciphersuites | ForEach-Object { $_.PSObject.Properties.Value.gnutls_name = $_.PSObject.Properties.Name; $_.PSObject.Properties.Value } `
+                                 | Where-Object { $_.kex_algorithm -ne "DHE" } `
+                                 | Where-Object { $_.auth_algorithm -eq "RSA" } `
+                                 | Where-Object { $_.enc_algorithm -like "AES * CBC" } `
+                                 | Where-Object { `$_.hash_algorithm -eq "SHA256" } `
+                                 | ForEach-Object { $_.gnutls_name }
+
+$allowedCiphers = @($secure) + @($recommended)
 
 
 # Disable all ciphers that are not in the allowed list
 # ----------------------------------------------------
-foreach ($cipherSuite in $(Get-TlsCipherSuite | ForEach-Object { $_.Name })) {
-    if ($cipherSuite -notin $allowedCiphers) {
-        Disable-TlsCipherSuite -Name $cipherSuite
-        if ($?) {
-            Write-Output " [o] Disabled '$cipherSuite' suite."
-        } else {
-            Write-Output " [x] Failed to ensure '$cipherSuite' suite is disabled."
-        }
+foreach ($cipherSuite in $(Get-TlsCipherSuite | ForEach-Object { $_.Name } | Where-Object { $_ -notin $allowedCiphers})) {
+    # Note that running this command on eg. TLS_DHE_RSA_WITH_AES_128_CCM will also disable TLS_DHE_RSA_WITH_AES_128_CCM_8
+    if (Get-TlsCipherSuite -Name $unsupportedCipher) { Disable-TlsCipherSuite -Name $unsupportedCipher }
+    if ($?) {
+        Write-Output " [o] Disabled '$cipherSuite' suite."
+    } else {
+        Write-Output " [x] Failed to ensure '$cipherSuite' suite is disabled."
     }
 }
 
@@ -94,10 +104,22 @@ foreach ($cipherSuite in $allowedCiphers) {
 }
 
 
+# Remove any ciphers that are not supported
+# -----------------------------------------
+# Note that the bare `Get-TlsCipherSuite` does not retrieve full details beyond the name so we pipe this to the version with `-Name`
+$unsupported = (Get-TlsCipherSuite) | ForEach-Object { Get-TlsCipherSuite -Name $_.Name } | Where-Object { $_.CipherSuite -eq 0 } | ForEach-Object { $_.Name }
+foreach ($unsupportedCipher in $unsupported) {
+    # Note that running this command on eg. TLS_DHE_RSA_WITH_AES_128_CCM will also disable TLS_DHE_RSA_WITH_AES_128_CCM_8
+    if (Get-TlsCipherSuite -Name $unsupportedCipher) { Disable-TlsCipherSuite -Name $unsupportedCipher }
+    if ($?) {
+        Write-Output " [o] Removed unsupported '$unsupportedCipher' suite."
+    } else {
+        Write-Output " [x] Failed to remove unsupported '$unsupportedCipher' suite."
+    }
+}
+
+
 # List all cipher suites that are still allowed
 # ---------------------------------------------
-Write-Output "Allowed cipher suites are:"
+Write-Output "There are $(((Get-TlsCipherSuite) | Measure-Object).Count) cipher suites:"
 (Get-TlsCipherSuite) | ForEach-Object { Write-Host "... $($_.Name)" }
-
-
-
