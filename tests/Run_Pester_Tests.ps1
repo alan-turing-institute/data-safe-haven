50e5c17ff29fc21044120a01f5ae515760dd01bc
James Robinson <james.em.robinson@gmail.com>
2020-10-21 10:35:50 +0100

    Switched to using configuration syntax

--- tests/Run_Pester_Tests.ps1
+++ tests/Run_Pester_Tests.ps1
@@ -1,6 +1,14 @@
 Import-Module Pester -ErrorAction Stop
 
 
+# Set Pester configuration
+# ------------------------
+$configuration = [PesterConfiguration]::Default
+$configuration.Output.Verbosity = "Detailed"
+$configuration.Run.Exit = $true
+$configuration.Run.Path = (Join-Path $PSScriptRoot "pester")
+
+
 # Run Pester tests
 # ----------------
-Invoke-Pester $(Join-Path $PSScriptRoot "pester") -Output Detailed -EnableExit
+Invoke-Pester -Configuration $configuration
