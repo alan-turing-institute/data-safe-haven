f5e35e38e63e0670b13d955a6f39b50938b85ae6
Jim Madge <jmmadge@gmail.com>
2020-10-21 14:57:32 +0100

    Remove peerings if they already exist

--- deployment/safe_haven_management_environment/setup/Setup_SHM_Nexus.ps1
+++ deployment/safe_haven_management_environment/setup/Setup_SHM_Nexus.ps1
@@ -59,6 +59,12 @@ if ($?) {
 # Peer repository vnet to SHM vnet
 # --------------------------------
 Add-LogMessage -Level Info "Peering repository vnet to shm vnet"
+if (Get-AzVirtualNetworkPeering -Name "PEER_$($config.network.vnet.name)" -VirtualNetworkName $config.network.repositoryVnet.name -ResourceGroupName $config.network.vnet.rg) {
+    Remove-AzVirtualNetworkPeering -Name "PEER_$($config.network.vnet.name)" -VirtualNetworkName $config.network.repositoryVnet.name -ResourceGroupName $config.network.vnet.rg -Force
+}
+if (Get-AzVirtualNetworkPeering -Name "PEER_$($config.network.repositoryVnet.name)" -VirtualNetworkName $config.network.vnet.name -ResourceGroupName $config.network.vnet.rg) {
+    Remove-AzVirtualNetworkPeering -Name "PEER_$($config.network.repositoryVnet.name)" -VirtualNetworkName $config.network.vnet.name -ResourceGroupName $config.network.vnet.rg -Force
+}
 New-VnetPeering -Vnet1Name $config.network.repositoryVnet.name -Vnet1ResourceGroup $config.network.vnet.rg -Vnet1SubscriptionName $config.subscriptionName -Vnet2Name $config.network.vnet.name -Vnet2ResourceGroup $config.network.vnet.rg -Vnet2SubscriptionName $config.subscriptionName
 
 
