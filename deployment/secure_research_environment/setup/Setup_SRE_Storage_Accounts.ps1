0b35112d6cd2de98a055cc35867a06055687a543
James Robinson <james.em.robinson@gmail.com>
2020-10-29 15:41:22 +0000

    Updated SRE storage account setup to take advantage of the simpler functions

--- deployment/secure_research_environment/setup/Setup_SRE_Storage_Accounts.ps1
+++ deployment/secure_research_environment/setup/Setup_SRE_Storage_Accounts.ps1
@@ -4,9 +4,9 @@ param(
 )
 
 Import-Module Az
+Import-Module $PSScriptRoot/../../common/AzureStorage.psm1 -Force
 Import-Module $PSScriptRoot/../../common/Configuration.psm1 -Force
 Import-Module $PSScriptRoot/../../common/Deployments.psm1 -Force
-Import-Module $PSScriptRoot/../../common/GenerateSasToken.psm1 -Force
 Import-Module $PSScriptRoot/../../common/Logging.psm1 -Force
 Import-Module $PSScriptRoot/../../common/Security.psm1 -Force
 
@@ -15,65 +15,56 @@ Import-Module $PSScriptRoot/../../common/Security.psm1 -Force
 # ------------------------------------------------------------
 $config = Get-SreConfig $configId
 $originalContext = Get-AzContext
-$null = Set-AzContext -SubscriptionId $config.shm.subscriptionName
-
-
-foreach ($accessPolicy in $config.sre.storage.accessPolicies) {
-    write-host ($accessPolicy | Out-String)
-    write-host "Key: $($accessPolicy.Key)"
-    write-host "Value: $($accessPolicy.Value)"
-}
-exit 1
-
-
-# Ensure that a container exists in the correct SHM storage account for this SRE
-# ------------------------------------------------------------------------------
-$null = Deploy-ResourceGroup -Name $config.sre.storage.datastorage.rg -Location $config.shm.location
-$storageAccount = Deploy-StorageAccount -Name $config.sre.storage.datastorage.accountName -ResourceGroupName $config.sre.storage.datastorage.rg -Kind $config.sre.storage.datastorage.storageType -Location $config.shm.location -SkuName "Standard_RAGRS"
-if (-not $storageAccount.PrimaryEndpoints.Blob) {
-    Add-LogMessage -Level Fatal "Storage account '$($config.sre.storage.datastorage.accountName)' does not support blob storage!"
-}
-$containerName = $config.sre.storage.datastorage.containers.ingress.name
-$null = Deploy-StorageContainer -Name $containerName -StorageAccount $storageAccount
-
-
-# Create a new SAS token (and policy if required) then store it in the SRE keyvault
-# ---------------------------------------------------------------------------------
-$sasPolicy = Deploy-SasAccessPolicy -Name $config.sre.accessPolicies.researcher.nameSuffix `
-                                    -Permission $config.sre.accessPolicies.researcher.permissions `
-                                    -StorageAccount $storageAccount `
-                                    -ContainerName $containerName `
-                                    -ValidityYears 1
-$newSAStoken = New-AzStorageContainerSASToken -Name $containerName -Policy $sasPolicy -Context $storageAccount.Context
 $null = Set-AzContext -SubscriptionId $config.sre.subscriptionName
-$null = Resolve-KeyVaultSecret -VaultName $config.sre.keyVault.name -SecretName $config.sre.accessPolicies.researcher.sasSecretName -DefaultValue $newSAStoken
 
 
-# # Disable private endpoint network policies on the data subnet
-# # ------------------------------------------------------------
-# $dataSubnetName = $config.sre.network.vnet.subnets.data.name
-# $virtualNetwork = Get-AzVirtualNetwork -ResourceGroupName $config.sre.network.vnet.rg -Name $config.sre.network.vnet.name
-# ($virtualNetwork | Select-Object -ExpandProperty Subnets | Where-Object  {$_.Name -eq $dataSubnetName }).PrivateEndpointNetworkPolicies = "Disabled"
-# $virtualNetwork | Set-AzVirtualNetwork
-# $dataSubnet = Get-AzSubnet -Name $dataSubnetName -VirtualNetwork $virtualNetwork
+# Ensure that a storage account exists in the SHM for this SRE
+# ------------------------------------------------------------
+$null = Set-AzContext -SubscriptionId $config.shm.subscriptionName
+$null = Deploy-ResourceGroup -Name $config.shm.storage.data.rg -Location $config.shm.location
+$storageAccount = Deploy-StorageAccount -Name $config.sre.storage.data.account.name `
+                                        -AccessTier $config.sre.storage.data.account.accessTier `
+                                        -Kind $config.sre.storage.data.account.storageKind `
+                                        -Location $config.shm.location `
+                                        -ResourceGroupName $config.shm.storage.data.rg `
+                                        -SkuName "Standard_RAGRS"
+if (-not $storageAccount.PrimaryEndpoints.Blob) {
+    Add-LogMessage -Level Fatal "Storage account '$($config.sre.storage.data.accountName)' does not support blob storage!"
+}
+$null = Set-AzContext -SubscriptionId $config.sre.subscriptionName
+
+
+# Ensure that all required containers exist
+# -----------------------------------------
+foreach ($containerName in $config.sre.storage.data.containers.Keys) {
+    # Deploy the container
+    $null = Deploy-StorageContainer -Name $containerName -StorageAccount $storageAccount
+
+    # Ensure that the appropriate SAS policy exists
+    $accessPolicyName = $config.sre.storage.data.containers[$containerName].accessPolicyName
+    $sasPolicy = Deploy-SasAccessPolicy -Name $accessPolicyName `
+                                        -Permission $config.sre.storage.accessPolicies[$accessPolicyName].permissions `
+                                        -StorageAccount $storageAccount `
+                                        -ContainerName $containerName `
+                                        -ValidityYears 1
+
+    # Create a new SAS token then store it in the SRE keyvault
+    $newSAStoken = New-StorageReceptacleSasToken -ContainerName $containerName -Policy $sasPolicy -StorageAccount $storageAccount
+    $null = Resolve-KeyVaultSecret -VaultName $config.sre.keyVault.name -SecretName $config.sre.storage.data.containers[$containerName].sasSecretName -DefaultValue $newSAStoken
+}
 
 
 # Ensure that private endpoint exists
 # -----------------------------------
 $dataSubnet = Get-Subnet -Name $config.sre.network.vnet.subnets.data.name -VirtualNetworkName $config.sre.network.vnet.name -ResourceGroupName $config.sre.network.vnet.rg
-# $privateEndpoint = Deploy-StorageAccountEndpoint -StorageAccount $storageAccount -StorageType $config.sre.storage.datastorage.containers.ingress.storageType -Subnet $dataSubnet -ResourceGroupName $config.sre.network.vnet.rg -Location $config.sre.location
-$privateEndpoint = Deploy-StorageAccountEndpoint -StorageAccount $storageAccount -Subnet $dataSubnet -ResourceGroupName $config.sre.network.vnet.rg -Location $config.sre.location
+$privateEndpoint = Deploy-StorageAccountEndpoint -StorageAccount $storageAccount -StorageType "Default" -Subnet $dataSubnet -ResourceGroupName $config.sre.network.vnet.rg -Location $config.sre.location
 $privateEndpointIp = (Get-AzNetworkInterface -ResourceId $privateEndpoint.NetworkInterfaces.Id).IpConfigurations[0].PrivateIpAddress
 
 
-# Set up DNS zone
-# ---------------
+# Set up a DNS zone on the SHM DC
+# -------------------------------
 $null = Set-AzContext -SubscriptionId $config.shm.subscriptionName
 $privateDnsZoneName = "$($storageAccount.StorageAccountName).blob.core.windows.net".ToLower()
-
-
-
-
 Add-LogMessage -Level Info "Setting up DNS Zone for '$privateDnsZoneName'"
 $params = @{
     Name = $privateDnsZoneName
