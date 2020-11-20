53b574d81d58f7a1d08e00928b5e373e3eb57948
James Robinson <james.em.robinson@gmail.com>
2020-11-20 13:11:52 +0000

    Rationalised join_domain script location

--- deployment/secure_research_environment/setup/Add_DSVM.ps1
+++ deployment/secure_research_environment/setup/Add_DSVM.ps1
@@ -284,9 +284,7 @@ if (-not $cloudInitFilePath) { $cloudInitFilePath = Join-Path $cloudInitBasePath
 $cloudInitTemplate = Get-Content $cloudInitFilePath -Raw
 
 # Insert scripts into the cloud-init template
-$resourcePaths = @()
-$resourcePaths += @("jdk.table.xml", "krb5.conf", "project.default.xml") | ForEach-Object { Join-Path $PSScriptRoot ".." "cloud_init" "resources" $_ }
-$resourcePaths += @("join_domain.sh") | ForEach-Object { Join-Path $PSScriptRoot ".." "cloud_init" "scripts" $_ }
+$resourcePaths = @("jdk.table.xml", "join_domain.sh", "krb5.conf", "project.default.xml") | ForEach-Object { Join-Path $PSScriptRoot ".." "cloud_init" "resources" $_ }
 foreach ($resourcePath in $resourcePaths) {
     $resourceFileName = $resourcePath | Split-Path -Leaf
     $indent = $cloudInitTemplate -split "`n" | Where-Object { $_ -match "<${resourceFileName}>" } | ForEach-Object { $_.Split("<")[0] } | Select-Object -First 1
