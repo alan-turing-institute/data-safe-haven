9d238959a3fa1d1ada6d161f2971525e7f845300
James Robinson <james.em.robinson@gmail.com>
2020-11-30 12:41:30 +0000

    Fixed erroneous failure in Windows configuration

--- deployment/common/remote/Configure_Windows.ps1
+++ deployment/common/remote/Configure_Windows.ps1
@@ -19,10 +19,11 @@ param(
 Write-Output "Setting locale..."
 $GeoId = ([System.Globalization.CultureInfo]::GetCultures("InstalledWin32Cultures") | Where-Object { $_.Name -eq $Locale } | ForEach-Object { [System.Globalization.RegionInfo]$_.Name }).GeoId
 Set-WinHomeLocation -GeoId $GeoId
-Set-WinSystemLocale $Locale
-Set-Culture $Locale
+Set-WinSystemLocale -SystemLocale $Locale
+Set-Culture -CultureInfo $Locale
 Set-WinUserLanguageList -LanguageList $Locale -Force
-if (((Get-WinSystemLocale).Name -eq $Locale) -and ((Get-Culture).Name -eq $Locale) -and ((Get-WinUserLanguageList)[0].LanguageTag -eq $Locale)) {
+if (((Get-Culture).Name -eq $Locale) -and ((Get-WinUserLanguageList)[0].LanguageTag -eq $Locale)) {
+    # Note that Set-WinSystemLocale will not be applied until after a restart
     Write-Output " [o] Setting locale to '$Locale' succeeded"
 } else {
     Write-Output " [x] Setting locale to '$Locale' failed!"
@@ -37,6 +38,7 @@ if ($TimeZone) {
     if ($?) {
         Write-Output " [o] Setting time zone to '$TimeZone' succeeded"
     } else {
+        Write-Output (Get-TimeZone)
         Write-Output " [x] Setting time zone to '$TimeZone' failed!"
     }
 } else {
@@ -49,40 +51,42 @@ if ($TimeZone) {
 # --------------------------------------------------------------------------------------------------------------------------------------------------------
 if ($NTPServer) {
     Write-Output "Setting NTP server..."
+    $success = $true
     # Change DateTime\Servers settings
     # We should end up with exactly two DWORDs: 0th-server and default (pointing to 0th-server)
     # -----------------------------------------------------------------------------------------
     Push-Location
-    $success = $success -and $?
     Set-Location HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers
-    $success = $success -and $?
     Remove-ItemProperty . -Name "*"
-    $success = $success -and $?
     Set-ItemProperty . 0 $NTPServer
-    $success = $success -and $?
     Set-ItemProperty . "(Default)" "0"
-    $success = $success -and $?
-    $success = $success -and (((Get-ItemProperty .).PSObject.Members | Where-Object { $_.MemberType -eq "NoteProperty" -and $_.Name -notlike "PS*" } | Measure-Object | Select-Object Count).Count -eq 2)
     Pop-Location
-    $success = $success -and $?
+    $success = $success -and (
+        (((Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers).PSObject.Members | Where-Object { $_.MemberType -eq "NoteProperty" -and $_.Name -notlike "PS*" } | Measure-Object | Select-Object Count).Count -eq 2) -and
+        ((Get-ItemPropertyValue HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers -Name "0") -eq $NTPServer) -and
+        ((Get-ItemPropertyValue HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers -Name "(Default)") -eq "0")
+    )
 
     # Change Services\W32Time settings
     # --------------------------------
     Set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters Type NTP
-    $success = $success -and $?
     Set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters NtpServer "$NTPServer,0x1"
-    $success = $success -and $?
     Set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Config AnnounceFlags 0xA
-    $success = $success -and $?
     Set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpServer Enabled 1
-    $success = $success -and $?
+    $success = $success -and (
+        ((Get-ItemPropertyValue HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters -Name "Type") -eq "NTP") -and
+        ((Get-ItemPropertyValue HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters -Name "NtpServer") -eq "$NTPServer,0x1") -and
+        ((Get-ItemPropertyValue HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Config -Name "AnnounceFlags") -eq 0xA) -and
+        ((Get-ItemPropertyValue HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpServer -Name "Enabled") -eq 1)
+    )
 
     # Restart the Windows Time service
     # --------------------------------
     Stop-Service W32Time
-    $success = $success -and $?
     Start-Service W32Time
-    $success = $success -and $?
+
+    # Check that settings were applied
+    # --------------------------------
     if ($success) {
         Write-Output " [o] Setting NTP server to '$NTPServer' succeeded"
     } else {
