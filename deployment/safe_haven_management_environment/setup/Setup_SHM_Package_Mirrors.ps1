8ea6dd2c316168ae1825e5257dc720654b7c5591
James Robinson <james.em.robinson@gmail.com>
2021-01-22 15:24:49 +0000

    Store result of SSH key query for later use

--- deployment/safe_haven_management_environment/setup/Setup_SHM_Package_Mirrors.ps1
+++ deployment/safe_haven_management_environment/setup/Setup_SHM_Package_Mirrors.ps1
@@ -187,7 +187,7 @@ function Resolve-CloudInit {
         $vmNameExternal = "$($MirrorType.ToUpper())-EXTERNAL-MIRROR-TIER-$tier"
         $result = Invoke-RemoteScript -VMName $vmNameExternal -ResourceGroupName $config.mirrors.rg -Shell "UnixShell" -Script $script -SuppressOutput
         Add-LogMessage -Level Success "Fetching ssh key from external package mirror succeeded"
-        $externalMirrorPublicKey = $result.Value[0].Message -split "\n" | Select-String "^ssh"
+        $externalMirrorPublicKey = $result.Value[0].Message -Split "`n" | Select-String "^ssh"
         $cloudInitYaml = $cloudInitYaml.Replace("<external-mirror-public-key>", $externalMirrorPublicKey)
     }
 
@@ -318,8 +318,8 @@ function Deploy-PackageMirror {
             #! /bin/bash
             ssh-keyscan 127.0.0.1 2> /dev/null
             "
-            $null = Invoke-RemoteScript -VMName $vmName -ResourceGroupName $config.mirrors.rg -Shell "UnixShell" -Script $script
-            $internalFingerprint = $result.Value[0].Message -split "\n" | Select-String "^127.0.0.1" | ForEach-Object { $_ -replace "127.0.0.1", "$privateIpAddress" }
+            $result = Invoke-RemoteScript -VMName $vmName -ResourceGroupName $config.mirrors.rg -Shell "UnixShell" -Script $script
+            $internalFingerprint = $result.Value[0].Message -Split "`n" | Select-String "^127.0.0.1" | ForEach-Object { $_ -replace "127.0.0.1", "$privateIpAddress" }
 
             # Inform external server about the new internal server
             $externalVmName = $vmName.Replace("INTERNAL", "EXTERNAL")
