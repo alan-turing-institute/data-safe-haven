a03f040450ab3c415be9116a3bb3ee822de7d761
James Robinson <james.em.robinson@gmail.com>
2021-03-16 12:39:46 +0000

    Use more robust method of splitting configId into shmId and sreId

--- tests/pester/ConfigurationFiles.Tests.ps1
+++ tests/pester/ConfigurationFiles.Tests.ps1
@@ -5,7 +5,8 @@ Import-Module $PSScriptRoot/../../deployment/common/Configuration -Force -ErrorA
 # ------------------
 $FileExtensions = @("*.json")
 $ReferenceConfigFilePath = Join-Path -Path (Get-Item $PSScriptRoot).Parent -ChildPath "resources"
-$ConfigFileDetails = @(Get-ChildItem -Path $ReferenceConfigFilePath -Include $FileExtensions -Recurse | ForEach-Object { @{"FilePath" = $_.FullName; "FileName" = $_.Name; "ConfigType" = $_.Name.Split("_")[0]; "ConfigId" = $_.Name.Split("_")[1] } })
+$ShmIds = Get-ChildItem -Path $ReferenceConfigFilePath | ForEach-Object { $_.Name } | Where-Object { $_ -like "shm_*" } | ForEach-Object { $_.Split("_")[1] }
+$ConfigFileDetails = @(Get-ChildItem -Path $ReferenceConfigFilePath -Include $FileExtensions -Recurse | ForEach-Object { @{"FilePath" = $_.FullName; "FileName" = $_.Name; "ConfigType" = $_.Name.Split("_")[0]; "ConfigId" = $_.Name.Split("_")[1]; "ShmIds" = $ShmIds } })
 
 Describe "SHM configuration file check" {
     It "Checks that SHM config '<ConfigId>' expands to give the reference: '<FilePath>'" -TestCases ($ConfigFileDetails | Where-Object { $_.ConfigType -eq "shm" }) {
@@ -26,15 +27,17 @@ Describe "SHM configuration file check" {
 }
 
 Describe "SRE configuration file check" {
+    BeforeAll {
+    }
     It "Checks that SRE config '<ConfigId>' expands to give the reference: '<FilePath>'" -TestCases ($ConfigFileDetails | Where-Object { $_.ConfigType -eq "sre" }) {
-        param ($FileName, $FilePath, $ConfigType, $ConfigId)
+        param ($FileName, $FilePath, $ConfigType, $ConfigId, $ShmIds)
 
         # Load reference config and convert it to a sorted hashtable
         $referenceConfig = Get-Content -Path $FilePath -Raw -ErrorAction Stop | ConvertFrom-Json -AsHashtable | ConvertTo-SortedHashtable
 
-        # Get the shmId from ConfigId, given that all test configs have sreId "sandbox"
-        $sreId = "sandbox"
-        $shmId = $ConfigId.Split($sreId)[0]
+        # Split the ConfigId into shmId and sreId by matching to the list of known shmIds
+        $shmId = $ShmIds | Where-Object { $ConfigId.Split($_)[0] -ne $ConfigId} | Select-Object -First 1
+        $sreId = $ConfigId.Split($shmId)[1]
         # Load test config
         Mock Write-Host {} # we mock Write-Host here as we expect output from the `Get-SreConfig` call
         $testConfig = Get-SreConfig -shmId $shmId -sreId $sreId
