b6e890f52f9ad7476038dfada215b7818716bb2b
James Robinson <james.em.robinson@gmail.com>
2021-04-15 14:01:18 +0100

    Added missing module include

--- deployment/administration/SRE_DSVM_Remote_Diagnostics.ps1
+++ deployment/administration/SRE_DSVM_Remote_Diagnostics.ps1
@@ -11,6 +11,7 @@ Import-Module Az -ErrorAction Stop
 Import-Module $PSScriptRoot/../common/Configuration -Force -ErrorAction Stop
 Import-Module $PSScriptRoot/../common/Deployments -Force -ErrorAction Stop
 Import-Module $PSScriptRoot/../common/Logging -Force -ErrorAction Stop
+Import-Module $PSScriptRoot/../common/Security -Force -ErrorAction Stop
 
 
 # Get config and original context before changing subscription
