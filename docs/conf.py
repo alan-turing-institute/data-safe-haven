28ef842c7cf26385e212ad83f812dff5e276caac
James Robinson <james.em.robinson@gmail.com>
2021-06-04 19:24:23 +0200

    Use NPM to build PDFs

--- docs/conf.py
+++ docs/conf.py
@@ -3,16 +3,16 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Customisation  -----------------------------------------------------------
 from git import Repo
 
-repo = Repo(search_parent_directories=True)
 
+# -- Customisation  -----------------------------------------------------------
+repo = Repo(search_parent_directories=True)
+repo_name = repo.remotes.origin.url.split(".git")[0].split("/")[-1]
 # Find the version name which is a tag or branch name
 tags = [t for t in repo.tags if t.commit == repo.head.commit]
-# version = tags[0].name if tags else repo.head.reference.name.replace("master", "latest")
 version = tags[0].name if tags else "latest"
+versions = ["latest"] + [t.name for t in repo.tags]
 
 # Set sidebar variables
 try:
@@ -22,20 +22,13 @@ except NameError:
 html_context["display_lower_left"] = True
 html_context["version"] = version
 html_context["current_version"] = version
-html_context["versions"] = [(t.name, f"../{t.name}/index.html") for t in repo.tags]
-# html_context['downloads'] = [("PDF", "/latest.pdf")]
+html_context["versions"] = [(v, f"/{repo_name}/{v}/index.html") for v in versions]
+html_context["downloads"] = [
+    ("PDF user guide", f"/{repo_name}/{version}/pdf/user-guide.pdf"),
+    ("PDF SRE deployment", f"/{repo_name}/{version}/pdf/how-to-deploy-sre.pdf"),
+    ("PDF SHM deployment", f"/{repo_name}/{version}/pdf/how-to-deploy-shm.pdf"),
+]
 
-print("html_context", html_context)
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
@@ -74,10 +67,11 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_css_files = [
-    'overrides.css',
+    "overrides.css",
 ]
+
 
 # -- Options for MyST  --------------------------------------------------------
 
