74864ec7542d2aab6346fb8fa93ced23def5da36
James Robinson <james.em.robinson@gmail.com>
2021-06-12 10:51:01 +0200

    :recycle: Moved LogWorkspace deployment from firewall setup to logging setup

--- deployment/safe_haven_management_environment/setup/Setup_SHM_Firewall.ps1
+++ deployment/safe_haven_management_environment/setup/Setup_SHM_Firewall.ps1
@@ -33,7 +33,7 @@ $firewall = Deploy-Firewall -Name $config.firewall.name -ResourceGroupName $conf
 # Enable logging for this firewall
 # --------------------------------
 Add-LogMessage -Level Info "Enable logging for this firewall"
-$workspace = Deploy-LogAnalyticsWorkspace -Name $config.logging.workspaceName -ResourceGroupName $config.logging.rg -Location $config.location
+$workspace = Get-AzOperationalInsightsWorkspace -Name $config.logging.workspaceName -ResourceGroup $config.logging.rg
 $null = Set-AzDiagnosticSetting -ResourceId $firewall.Id -WorkspaceId $workspace.ResourceId -Enabled $true
 if ($?) {
     Add-LogMessage -Level Success "Enabled logging to workspace '$($config.logging.workspaceName)'"
