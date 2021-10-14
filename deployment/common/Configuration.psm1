264b13bf995a4cd887babd625ad272142d5a1b6e
Jim Madge <jmadge@turing.ac.uk>
2021-10-14 14:30:46 +0100

    Update CodiMD version

--- deployment/common/Configuration.psm1
+++ deployment/common/Configuration.psm1
@@ -906,11 +906,11 @@ function Get-SreConfig {
             ip                      = Get-NextAvailableIpInRange -IpRangeCidr $config.sre.network.vnet.subnets.webapps.cidr -Offset 6
             osVersion               = "20.04-LTS"
             codimd                  = [ordered]@{
-                dockerVersion = "2.3.2"
+                dockerVersion = "2.4.1"
             }
             postgres                = [ordered]@{
                 passwordSecretName = "$($config.sre.shortName)-other-codimd-password-postgresdb"
-                dockerVersion      = "13.1"
+                dockerVersion      = "11.6-alpine"
             }
             disks                   = [ordered]@{
                 data = [ordered]@{
