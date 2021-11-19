2e6aa0c78089453c7560d4e475442ae2efbe91c0
James Robinson <james.em.robinson@gmail.com>
2021-11-19 15:25:43 +0000

    :lipstick: Moved downloads section into a separate dropdown menu

--- docs/build/_static/toggle.js
+++ docs/build/_static/toggle.js
@@ -2,3 +2,6 @@
 $(document).on("click", "[data-toggle='rst-versions']", function () {
   $("[data-toggle='rst-versions']").toggleClass("shift-up");
 });
+$(document).on("click", "[data-toggle='rst-downloads']", function () {
+  $("[data-toggle='rst-downloads']").toggleClass("shift-up");
+});
