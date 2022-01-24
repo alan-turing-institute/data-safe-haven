e70db2a4bec86cdb9549a9315ef07e6e10cd533b
James Robinson <james.em.robinson@gmail.com>
2022-01-24 20:05:39 +0100

    :pencil2: Fix typo in Docker Compose path

--- deployment/secure_research_environment/remote/create_guacamole/scripts/install_ssl_certificate.sh
+++ deployment/secure_research_environment/remote/create_guacamole/scripts/install_ssl_certificate.sh
@@ -7,24 +7,24 @@
 #     CERT_THUMBPRINT
 
 # Remove existing certificates
-sudo rm -rf /opt/ssl/conf/live/${USER_FRIENDLY_FQDN}/*
+sudo rm -rf "/opt/ssl/conf/live/${USER_FRIENDLY_FQDN}/*"
 
 # Import the certificates from the VM secret store
-sudo cp /var/lib/waagent/${CERT_THUMBPRINT}.crt /opt/ssl/conf/live/${USER_FRIENDLY_FQDN}/cert.pem
-sudo cp /var/lib/waagent/${CERT_THUMBPRINT}.prv /opt/ssl/conf/live/${USER_FRIENDLY_FQDN}/privkey.pem
+sudo cp "/var/lib/waagent/${CERT_THUMBPRINT}.crt" "/opt/ssl/conf/live/${USER_FRIENDLY_FQDN}/cert.pem"
+sudo cp "/var/lib/waagent/${CERT_THUMBPRINT}.prv" "/opt/ssl/conf/live/${USER_FRIENDLY_FQDN}/privkey.pem"
 
 # Download the Let's Encrypt intermediate certificate
 LETS_ENCRYPT_CERTIFICATE_PATH=/opt/ssl/lets-encrypt-r3.pem
-if [ ! -e $LETS_ENCRYPT_CERTIFICATE_PATH ]; then
+if [ ! -e "$LETS_ENCRYPT_CERTIFICATE_PATH" ]; then
     echo "Downloading Let's Encrypt intermediate certificate..."
-    wget -O $LETS_ENCRYPT_CERTIFICATE_PATH https://letsencrypt.org/certs/lets-encrypt-r3.pem 2>&1
+    wget -O "$LETS_ENCRYPT_CERTIFICATE_PATH" https://letsencrypt.org/certs/lets-encrypt-r3.pem 2>&1
 fi
 
 # Create a certificate chain from the certificate and intermediate certificate
 echo "Creating fullchain certificate..."
-cd /opt/ssl/conf/live/${USER_FRIENDLY_FQDN}/
-cat cert.pem $LETS_ENCRYPT_CERTIFICATE_PATH > fullchain.pem
+cd "/opt/ssl/conf/live/${USER_FRIENDLY_FQDN}/" || exit 1
+cat cert.pem "$LETS_ENCRYPT_CERTIFICATE_PATH" > fullchain.pem
 ls -alh
 
 # Force docker services to reload
-docker-compose -f /opt/guacamole/docker-compose.yml up --force-recreate -d 2>&1
+docker-compose -f /opt/guacamole/docker-compose.yaml up --force-recreate -d 2>&1
