6f86ad8d39548d6d4c771ef5c24b82c8a4c96efc
James Robinson <james.em.robinson@gmail.com>
2022-01-25 11:05:45 +0100

    :art: Add a guacamoledaemon user to minimise use of root

--- deployment/secure_research_environment/cloud_init/resources/guacamole_synchronise_database.sh
+++ deployment/secure_research_environment/cloud_init/resources/guacamole_synchronise_database.sh
@@ -2,5 +2,5 @@
 echo "Running LDAP synchronisation..." | tee >(logger -t SafeHaven)
 pg_ldap_sync -c /opt/pg-ldap-sync/configuration.yaml -vv | logger -t SafeHaven
 echo "Updating database..." | tee >(logger -t SafeHaven)
-docker-compose -f /opt/guacamole/docker-compose.yaml exec -T postgres psql -U guacamole -f /scripts/db_update.sql | logger -t SafeHaven
+su guacamoledaemon -c "docker-compose -f /opt/guacamole/docker-compose.yaml exec -T postgres psql -U guacamole -f /scripts/db_update.sql" | logger -t SafeHaven
 echo "Finished database synchronisation" | tee >(logger -t SafeHaven)
