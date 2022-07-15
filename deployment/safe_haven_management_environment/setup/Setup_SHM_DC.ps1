13e843990d321c0c0a7a06b0862dc8b9d1fe14d5
James Robinson <james.em.robinson@gmail.com>
2022-07-15 23:02:13 +0100

    :truck: Consolidate DC artifacts and desired state

--- deployment/safe_haven_management_environment/setup/Setup_SHM_DC.ps1
+++ deployment/safe_haven_management_environment/setup/Setup_SHM_DC.ps1
@@ -35,10 +35,10 @@ $storageAccount = Deploy-StorageAccount -Name $config.storage.artifacts.accountN
 # Create blob storage containers
 # ------------------------------
 Add-LogMessage -Level Info "Ensuring that blob storage containers exist..."
-$storageContainerDcConfigName = "shm-dc1-artifacts"
-$storageContainerDcDscName = "shm-desired-state"
+$storageContainerArtifactsName = "shm-dc1-artifacts"
+$storageContainerDscName = "shm-desired-state"
 $storageContainerSreRds = "sre-rds-sh-packages"
-foreach ($containerName in ($storageContainerDcDscName, $storageContainerDcConfigName, $storageContainerSreRds)) {
+foreach ($containerName in ($storageContainerDscName, $storageContainerArtifactsName, $storageContainerSreRds)) {
     $null = Deploy-StorageContainer -Name $containerName -StorageAccount $storageAccount
 }
 
@@ -46,27 +46,22 @@ foreach ($containerName in ($storageContainerDcDscName, $storageContainerDcConfi
 # Upload DSC scripts
 # ------------------
 Add-LogMessage -Level Info "[ ] Uploading desired state configuration (DSC) files to storage account '$($storageAccount.StorageAccountName)'..."
-$dc1DscPath = Join-Path $PSScriptRoot ".." "remote" "create_dc" "artifacts" "shm-dc1-desiredstate"
+$dscPath = Join-Path $PSScriptRoot ".." "desired_state_configuration"
 $success = $true
-# foreach ($filename in @("ConfigureActiveDirectoryDC1.ps1", "CreatePrimaryDomainController.ps1", "InstallPowershellModulesDC1.ps1", "UploadArtifactsDC1.ps1")) {
-$null = Publish-AzVMDscConfiguration -ConfigurationPath (Join-Path $dc1DscPath "ConfigurePrimaryDomainController.ps1") `
-                                     -ContainerName $storageContainerDcDscName `
+$null = Publish-AzVMDscConfiguration -ConfigurationPath (Join-Path $dscPath "DC1DesiredState.ps1") `
+                                     -ContainerName $storageContainerDscName `
                                      -Force `
                                      -ResourceGroupName $config.storage.artifacts.rg `
                                      -SkipDependencyDetection `
                                      -StorageAccountName $config.storage.artifacts.accountName
 $success = $success -and $?
-# }
-$dc2DscPath = Join-Path $PSScriptRoot ".." "remote" "create_dc" "artifacts" "shm-dc2-desiredstate"
-# foreach ($filename in @("CreateSecondaryDomainController.ps1", "InstallPowershellModulesDC2.ps1")) {
-$null = Publish-AzVMDscConfiguration -ConfigurationPath (Join-Path $dc2DscPath "ConfigureSecondaryDomainController.ps1") `
-                                        -ContainerName $storageContainerDcDscName `
+$null = Publish-AzVMDscConfiguration -ConfigurationPath (Join-Path $dscPath "DC2DesiredState.ps1") `
+                                        -ContainerName $storageContainerDscName `
                                         -Force `
                                         -ResourceGroupName $config.storage.artifacts.rg `
                                         -SkipDependencyDetection `
                                         -StorageAccountName $config.storage.artifacts.accountName
 $success = $success -and $?
-# }
 if ($success) {
     Add-LogMessage -Level Success "Uploaded desired state configuration (DSC) files"
 } else {
@@ -78,15 +73,15 @@ if ($success) {
 # ---------------------------------------
 Add-LogMessage -Level Info "[ ] Uploading domain controller (DC) configuration files to storage account '$($storageAccount.StorageAccountName)'..."
 $success = $true
-foreach ($filePath in $(Get-ChildItem -File (Join-Path $PSScriptRoot ".." "remote" "create_dc" "artifacts" "shm-dc1-uploads"))) {
+foreach ($filePath in $(Get-ChildItem -File (Join-Path $dscPath "dc1Artifacts"))) {
     if ($($filePath | Split-Path -Leaf) -eq "Disconnect_AD.mustache.ps1") {
         # Expand the AD disconnection template before uploading
         $adScriptLocalFilePath = (New-TemporaryFile).FullName
         Expand-MustacheTemplate -Template $(Get-Content $filePath -Raw) -Parameters $config | Out-File $adScriptLocalFilePath
-        $null = Set-AzStorageBlobContent -Container $storageContainerDcConfigName -Context $storageAccount.Context -Blob "Disconnect_AD.ps1" -File $adScriptLocalFilePath -Force
+        $null = Set-AzStorageBlobContent -Container $storageContainerArtifactsName -Context $storageAccount.Context -Blob "Disconnect_AD.ps1" -File $adScriptLocalFilePath -Force
         $null = Remove-Item $adScriptLocalFilePath
     } else {
-        $null = Set-AzStorageBlobContent -Container $storageContainerDcConfigName -Context $storageAccount.Context -File $filePath -Force
+        $null = Set-AzStorageBlobContent -Container $storageContainerArtifactsName -Context $storageAccount.Context -File $filePath -Force
     }
     $success = $success -and $?
 }
@@ -100,25 +95,18 @@ if ($success) {
 # Upload Windows package installers
 # ---------------------------------
 Add-LogMessage -Level Info "[ ] Uploading Windows package installers to storage account '$($storageAccount.StorageAccountName)'..."
-$success = $true
-# AzureADConnect
-$filename = "AzureADConnect.msi"
-Start-AzStorageBlobCopy -AbsoluteUri "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/$filename" -DestContainer $storageContainerDcConfigName -DestBlob $filename -DestContext $storageAccount.Context -Force
-$success = $success -and $?
-# Chrome
-$filename = "GoogleChromeStandaloneEnterprise64.msi"
-Start-AzStorageBlobCopy -AbsoluteUri "http://dl.google.com/edgedl/chrome/install/$filename" -DestContainer $storageContainerSreRds -DestBlob "GoogleChrome_x64.msi" -DestContext $storageAccount.Context -Force
-$success = $success -and $?
-# PuTTY
-$baseUri = "https://the.earth.li/~sgtatham/putty/latest/w64/"
-$httpContent = Invoke-WebRequest -Uri $baseUri
-$filename = $httpContent.Links | Where-Object { $_.href -like "*installer.msi" } | ForEach-Object { $_.href } | Select-Object -First 1
-$version = ($filename -split "-")[2]
-Start-AzStorageBlobCopy -AbsoluteUri "$($baseUri.Replace('latest', $version))/$filename" -DestContainer $storageContainerSreRds -DestBlob "PuTTY_x64.msi" -DestContext $storageAccount.Context -Force
-$success = $success -and $?
-if ($success) {
+try {
+    # AzureADConnect
+    $null = Set-AzStorageBlobFromUri -FileUri "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi" -StorageContainer $storageContainerArtifactsName -StorageContext $storageAccount.Context
+    # Chrome
+    $null = Set-AzStorageBlobFromUri -FileUri "http://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi" -BlobFilename "GoogleChrome_x64.msi" -StorageContainer $storageContainerArtifactsName -StorageContext $storageAccount.Context
+    # PuTTY
+    $baseUri = "https://the.earth.li/~sgtatham/putty/latest/w64/"
+    $filename = $(Invoke-WebRequest -Uri $baseUri).Links | Where-Object { $_.href -like "*installer.msi" } | ForEach-Object { $_.href } | Select-Object -First 1
+    $version = ($filename -split "-")[2]
+    $null = Set-AzStorageBlobFromUri -FileUri "$($baseUri.Replace('latest', $version))/$filename" -BlobFilename "PuTTY_x64.msi" -StorageContainer $storageContainerSreRds -StorageContext $storageAccount.Context
     Add-LogMessage -Level Success "Uploaded Windows package installers"
-} else {
+} catch {
     Add-LogMessage -Level Fatal "Failed to upload Windows package installers!"
 }
 
@@ -166,7 +154,7 @@ Deploy-ArmTemplate -TemplatePath (Join-Path $PSScriptRoot ".." "arm_templates" "
 $domainAdminCredentials = (New-Object System.Management.Automation.PSCredential ($domainAdminUsername, $(ConvertTo-SecureString $domainAdminPassword -AsPlainText -Force)))
 $safeModeCredentials = (New-Object System.Management.Automation.PSCredential ($domainAdminUsername, $(ConvertTo-SecureString $safemodeAdminPassword -AsPlainText -Force)))
 $commonDscParams = @{
-    ArchiveContainerName      = $storageContainerDcDscName
+    ArchiveContainerName      = $storageContainerDscName
     ArchiveResourceGroupName  = $config.storage.artifacts.rg
     ArchiveStorageAccountName = $config.storage.artifacts.accountName
     VmLocation                = $config.location
@@ -179,14 +167,14 @@ foreach ($user in $userAccounts.Keys) {
 }
 # DC1
 Add-LogMessage -Level Info "Installing desired state prerequisites on DC1..."
-$null = Invoke-RemoteScript -Shell "PowerShell" -ScriptPath (Join-Path $dc1DscPath "BootstrapDSC.ps1") -VMName $config.dc.vmName -ResourceGroupName $config.dc.rg -SuppressOutput
+$null = Invoke-RemoteScript -Shell "PowerShell" -ScriptPath (Join-Path $dscPath "DC1Bootstrap.ps1") -VMName $config.dc.vmName -ResourceGroupName $config.dc.rg -SuppressOutput
 $params = @{
     ActiveDirectoryBasePath       = $config.dc.adDirectory
     AdministratorCredentials      = $domainAdminCredentials
-    ArtifactsBlobNamesB64         = Get-AzStorageBlob -Container $storageContainerDcConfigName -Context $storageAccount.Context | ForEach-Object { $_.Name } | ConvertTo-Json -Depth 99 | ConvertTo-Base64
+    ArtifactsBlobNamesB64         = Get-AzStorageBlob -Container $storageContainerArtifactsName -Context $storageAccount.Context | ForEach-Object { $_.Name } | ConvertTo-Json -Depth 99 | ConvertTo-Base64
     ArtifactsBlobSasTokenB64      = (New-ReadOnlyStorageAccountSasToken -SubscriptionName $config.subscriptionName -ResourceGroup $config.storage.artifacts.rg -AccountName $config.storage.artifacts.accountName) | ConvertTo-Base64
     ArtifactsStorageAccountName   = $config.storage.artifacts.accountName
-    ArtifactsStorageContainerName = $storageContainerDcConfigName
+    ArtifactsStorageContainerName = $storageContainerArtifactsName
     ArtifactsTargetDirectory      = $config.dc.installationDirectory
     DomainDn                      = $config.domain.dn
     DomainFqdn                    = $config.domain.fqdn
@@ -198,14 +186,14 @@ $params = @{
     SafeModeCredentials           = $safeModeCredentials
     UserAccountsB64               = $userAccounts | ConvertTo-Json -Depth 99 | ConvertTo-Base64
 }
-$null = Invoke-AzureVmDesiredState -ArchiveBlobName "ConfigurePrimaryDomainController.ps1.zip" `
+$null = Invoke-AzureVmDesiredState -ArchiveBlobName "DC1DesiredState.ps1.zip" `
                                    -ConfigurationName "ConfigurePrimaryDomainController" `
                                    -ConfigurationParameters $params `
                                    -VmName $config.dc.vmName `
                                    @commonDscParams
 # DC2
 Add-LogMessage -Level Info "Installing desired state prerequisites on DC2..."
-$null = Invoke-RemoteScript -Shell "PowerShell" -ScriptPath (Join-Path $dc2DscPath "BootstrapDSC.ps1") -VMName $config.dcb.vmName -ResourceGroupName $config.dc.rg -SuppressOutput
+$null = Invoke-RemoteScript -Shell "PowerShell" -ScriptPath (Join-Path $dscPath "DC2Bootstrap.ps1") -VMName $config.dcb.vmName -ResourceGroupName $config.dc.rg -SuppressOutput
 $params = @{
     ActiveDirectoryBasePath   = $config.dc.adDirectory
     AdministratorCredentials  = $domainAdminCredentials
@@ -213,7 +201,7 @@ $params = @{
     PrimaryDomainControllerIp = $config.dc.ip
     SafeModeCredentials       = $safeModeCredentials
 }
-$null = Invoke-AzureVmDesiredState -ArchiveBlobName "ConfigureSecondaryDomainController.ps1.zip" `
+$null = Invoke-AzureVmDesiredState -ArchiveBlobName "DC2DesiredState.ps1.zip" `
                                    -ConfigurationName "ConfigureSecondaryDomainController" `
                                    -ConfigurationParameters $params `
                                    -VmName $config.dcb.vmName `
