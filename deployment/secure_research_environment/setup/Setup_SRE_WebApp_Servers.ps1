a089d3316f57df2f56f9ff58a50bd77605aadafa
James Robinson <james.em.robinson@gmail.com>
2022-08-11 15:10:07 +0100

    :truck: Move Update-VMDnsRecords to RemoteCommands

--- deployment/secure_research_environment/setup/Setup_SRE_WebApp_Servers.ps1
+++ deployment/secure_research_environment/setup/Setup_SRE_WebApp_Servers.ps1
@@ -16,6 +16,7 @@ Import-Module $PSScriptRoot/../../common/Configuration -Force -ErrorAction Stop
 Import-Module $PSScriptRoot/../../common/DataStructures -Force -ErrorAction Stop
 Import-Module $PSScriptRoot/../../common/Logging -Force -ErrorAction Stop
 Import-Module $PSScriptRoot/../../common/Networking -Force -ErrorAction Stop
+Import-Module $PSScriptRoot/../../common/RemoteCommands -Force -ErrorAction Stop
 Import-Module $PSScriptRoot/../../common/Security -Force -ErrorAction Stop
 Import-Module $PSScriptRoot/../../common/Templates -Force -ErrorAction Stop
 
