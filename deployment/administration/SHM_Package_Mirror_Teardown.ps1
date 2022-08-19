1bd9bab5b40c63bad441f6919aff03a362216f24
James Robinson <james.em.robinson@gmail.com>
2022-08-20 00:33:59 +0100

    :truck: Move package mirror teardown script and expand it to work for proxies too

---
+++ deployment/administration/SHM_Package_Mirror_Teardown.ps1
@@ -0,0 +1,41 @@
+param(
+    [Parameter(Mandatory = $true, HelpMessage = "Enter SHM ID (e.g. use 'testa' for Turing Development Safe Haven A)")]
+    [string]$shmId,
+    [Parameter(Mandatory = $true, HelpMessage = "Which tier of mirrors should be torn down")]
+    [ValidateSet("2", "3")]
+    [string]$tier
+)
+
+Import-Module Az.Accounts -ErrorAction Stop
+Import-Module Az.Compute -ErrorAction Stop
+Import-Module Az.Resources -ErrorAction Stop
+Import-Module $PSScriptRoot/../common/AzureCompute -Force -ErrorAction Stop
+Import-Module $PSScriptRoot/../common/AzureNetwork -Force -ErrorAction Stop
+Import-Module $PSScriptRoot/../common/Configuration -Force -ErrorAction Stop
+Import-Module $PSScriptRoot/../common/Logging -Force -ErrorAction Stop
+
+
+# Get config and original context before changing subscription
+# ------------------------------------------------------------
+$config = Get-ShmConfig -shmId $shmId
+$originalContext = Get-AzContext
+$null = Set-AzContext -SubscriptionId $config.subscriptionName -ErrorAction Stop
+
+# Check if Resource Group exists
+$null = Get-AzResourceGroup -Name $config.repositories.rg -Location $config.location -ErrorVariable notExists -ErrorAction SilentlyContinue
+if ($notExists) {
+    Add-LogMessage -Level InfoSuccess "Resource group '$($config.repositories.rg)' does not exist"
+} else {
+    # Tear down repository VMs and associated disks/network cards
+    Get-AzVM -ResourceGroupName $config.repositories.rg | Where-Object { $_.Name -like "*-TIER-${tier}" } | ForEach-Object {
+        Remove-VirtualMachine -Name $_.Name -ResourceGroupName $_.ResourceGroupName -Force
+        Remove-ManagedDisk -Name "$($_.Name)-OS-DISK" -ResourceGroupName $config.repositories.rg
+        Remove-ManagedDisk -Name "$($_.Name)-DATA-DISK" -ResourceGroupName $config.repositories.rg
+        Remove-NetworkInterface -Name "$($_.Name)-NIC" -ResourceGroupName $config.repositories.rg
+    }
+}
+
+
+# Switch back to original subscription
+# ------------------------------------
+$null = Set-AzContext -Context $originalContext -ErrorAction Stop
--- docs/roles/system_manager/administrator_guide.md
+++ docs/roles/system_manager/administrator_guide.md
@@ -314,10 +314,10 @@ However, if you no longer have any SREs at a particular tier and you want to sav
 On your **deployment machine**.
 
 - Ensure you have the same version of the Data Safe Haven repository as was used by your deployment team.
-- Open a `Powershell` terminal and navigate to the `deployment/safe_haven_management_environment/setup` directory within the Data Safe Haven repository.
+- Open a `Powershell` terminal and navigate to the `deployment/administration` directory within the Data Safe Haven repository.
 - Ensure you are logged into `Azure` within `Powershell` using the command: `Connect-AzAccount`. This command will give you a URL and a short alphanumeric code. You will need to visit that URL in a web browser and enter the code
   - NB. If your account is a guest in additional Azure tenants, you may need to add the `-Tenant <Tenant ID>` flag, where `<Tenant ID>` is the ID of the Azure tenant you want to deploy into.
-- Tear down the package mirrors by running `./Teardown_SHM_Package_Mirrors.ps1 -shmId <SHM ID> -tier <desired tier>`, where `<SHM ID>` is the {ref}`management environment ID <roles_deployer_shm_id>` specified in the configuration file.
+- Tear down the package mirrors by running `./SHM_Package_Mirror_Teardown.ps1 -shmId <SHM ID> -tier <desired tier>`, where `<SHM ID>` is the {ref}`management environment ID <roles_deployer_shm_id>` specified in the configuration file.
 - This will take **a few minutes** to run.
 
 ## Ingress and Egress
