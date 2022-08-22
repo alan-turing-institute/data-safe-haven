2f113db197068fbbf2698336386a8c0aae4b38e4
James Robinson <james.em.robinson@gmail.com>
2022-08-22 12:03:35 +0100

    :truck: Nest updateServers under shm.monitoring and deploy to the same resource group

--- deployment/safe_haven_management_environment/setup/Setup_SHM_Update_Servers.ps1
+++ deployment/safe_haven_management_environment/setup/Setup_SHM_Update_Servers.ps1
@@ -21,27 +21,27 @@ $null = Set-AzContext -SubscriptionId $config.subscriptionName -ErrorAction Stop
 
 # Create resource group if it does not exist
 # ------------------------------------------
-$null = Deploy-ResourceGroup -Name $config.updateServers.rg -Location $config.location
+$null = Deploy-ResourceGroup -Name $config.monitoring.rg -Location $config.location
 
 
 # Deploy the Linux update server
 # ------------------------------
-$cloudInitYaml = Expand-MustacheTemplate -TemplatePath (Join-Path $PSScriptRoot ".." "cloud_init" "cloud-init-update-server-apt.mustache.yaml") -Parameters $config
+$cloudInitYaml = Expand-MustacheTemplate -TemplatePath (Join-Path $PSScriptRoot ".." "cloud_init" "cloud-init-update-server-linux.mustache.yaml") -Parameters $config
 $bootDiagnosticsAccount = Deploy-StorageAccount -Name $config.storage.bootdiagnostics.accountName -ResourceGroupName $config.storage.bootdiagnostics.rg -Location $config.location
-$vmName = $config.updateServers.linux.vmName
+$vmName = $config.monitoring.updateServers.linux.vmName
 $params = @{
-    AdminPassword          = (Resolve-KeyVaultSecret -VaultName $config.keyVault.name -SecretName $config.updateServers.linux.adminPasswordSecretName -DefaultLength 20)
+    AdminPassword          = (Resolve-KeyVaultSecret -VaultName $config.keyVault.name -SecretName $config.monitoring.updateServers.linux.adminPasswordSecretName -DefaultLength 20)
     AdminUsername          = (Resolve-KeyVaultSecret -VaultName $config.keyVault.name -SecretName $config.keyVault.secretNames.vmAdminUsername -DefaultValue "shm$($config.id)admin".ToLower() -AsPlaintext)
     BootDiagnosticsAccount = $bootDiagnosticsAccount
     CloudInitYaml          = $cloudInitYaml
     ImageSku               = "Ubuntu-latest"
     Location               = $config.location
     Name                   = $vmName
-    OsDiskSizeGb           = $config.updateServers.linux.disks.os.sizeGb
-    OsDiskType             = $config.updateServers.linux.disks.os.type
-    PrivateIpAddress       = $config.updateServers.linux.ip
-    ResourceGroupName      = $config.updateServers.rg
-    Size                   = $config.updateServers.linux.vmSize
+    OsDiskSizeGb           = $config.monitoring.updateServers.linux.disks.os.sizeGb
+    OsDiskType             = $config.monitoring.updateServers.linux.disks.os.type
+    PrivateIpAddress       = $config.monitoring.updateServers.linux.ip
+    ResourceGroupName      = $config.monitoring.rg
+    Size                   = $config.monitoring.updateServers.linux.vmSize
     Subnet                 = (Get-Subnet -Name $config.network.vnet.subnets.updateServers.name -ResourceGroupName $config.network.vnet.rg -VirtualNetworkName $config.network.vnet.name)
 }
 Deploy-LinuxVirtualMachine @params | Start-VM
