06c2dd57a3841e8166689de4b1b1662172ad1658
James Robinson <james.em.robinson@gmail.com>
2022-08-24 19:42:30 +0100

    :truck: Separate deploy-sre and deploy-shm

--- data_safe_haven/cli.py
+++ data_safe_haven/cli.py
@@ -5,14 +5,14 @@ from cleo import Application
 # Local imports
 from data_safe_haven import __version__
 from data_safe_haven.commands import (
-    DeploySRECommand,
+    DeployCommand,
     InitialiseCommand,
     UsersCommand,
     TeardownCommand,
 )
 
 application = Application("dsh", __version__, complete=True)
-application.add(DeploySRECommand())
+application.add(DeployCommand())
 application.add(InitialiseCommand())
 application.add(TeardownCommand())
 application.add(UsersCommand())
--- data_safe_haven/commands/__init__.py
+++ data_safe_haven/commands/__init__.py
@@ -1,11 +1,11 @@
-from .deploy_sre_command import DeploySRECommand as DeploySRECommand
+from .deploy_command import DeployCommand as DeployCommand
 from .initialise_command import InitialiseCommand as InitialiseCommand
 from .teardown_command import TeardownCommand as TeardownCommand
 from .users_command import UsersCommand as UsersCommand
 
 __all__ = [
-    DeploySRECommand,
-    InitialiseCommand,
-    TeardownCommand,
-    UsersCommand,
+    "DeployCommand",
+    "InitialiseCommand",
+    "TeardownCommand",
+    "UsersCommand",
 ]
