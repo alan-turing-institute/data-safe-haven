ab57c2f706fc1d223a81fe6eec1d36d16d0e7d70
Martin O'Reilly <developer@mtspace.net>
2022-09-04 00:12:31 +0100

    Update htmlproofer and try new option for swapping urls when testing locally
    NOTE: doesn't worl

--- docs/build/check_docs.sh
+++ docs/build/check_docs.sh
@@ -5,15 +5,22 @@ usage() {
     echo "usage: $0 [-h] -d directory -n name -p"
     echo "  -h                              display help"
     echo "  -d directory [required]         specify directory for docs to be checked"
+    echo "  -b base_url [required]          base url for site"
     exit 1
 }
 
 # Read command line arguments, overriding defaults where necessary
 docs_directory=""
-while getopts "d:h" option; do
+while getopts "d:b:h" option; do
     case $option in
     d)
         docs_directory=$OPTARG
+        ;;
+    b)
+        base_url=$OPTARG
+        ;;
+    h)
+        usage
         ;;
     \?)
         echo "Invalid option: -$OPTARG" >&2
@@ -31,13 +38,11 @@ if [[ -z "$docs_directory" ]]; then usage; fi
 #   - turing.ac.uk (as it requires a CAPTCHA)
 export LC_CTYPE="en_GB.UTF-8"
 export LC_ALL="en_GB.UTF-8"
+
 htmlproofer $docs_directory \
 --allow-hash-href \
---check-favicon \
---check-html \
---check-img-http \
 --enforce-https \
---file-ignore "*/_static/" \
---http-status-ignore "403,429,503" \
---url-swap "^\/data-safe-haven:/.." \
+--ignore_files "*/_static/" \
+--root-dir "$(readlink -f ${docs_directory})"
+--swap_urls "^\/${base_url}:/.." \
 --url-ignore "/github.com\/alan-turing-institute\/data-safe-haven/,/github.com\/alan-turing-institute\/data-classification-app/,/turing.ac.uk\//"
