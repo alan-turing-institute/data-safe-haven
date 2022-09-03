04f770ee6b9e65f96fc656d3dea6205741e03242
Martin O'Reilly <developer@mtspace.net>
2022-09-03 22:53:48 +0100

    Fix filtering to supported versions after reversing ordering

--- docs/build/repo_info.py
+++ docs/build/repo_info.py
@@ -8,7 +8,7 @@ repo = _git.Repo(search_parent_directories=True)
 repo_name = repo.remotes.origin.url.split(".git")[0].split("/")[-1]
 _all_releases = sorted((t.name for t in repo.tags), reverse=True)
 _earliest_supported_release_idx = _all_releases.index(earliest_supported_release)
-_all_supported_releases = _all_releases[_earliest_supported_release_idx:]
+_all_supported_releases = _all_releases[:_earliest_supported_release_idx]
 supported_versions = [development_branch] + _all_supported_releases
 _latest_supported_release = _all_supported_releases[-1]
 
