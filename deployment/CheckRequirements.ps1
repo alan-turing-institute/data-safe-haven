66001532e77750b94e5ee5d310402edb5b3a5e13
James Robinson <james.em.robinson@gmail.com>
2022-09-22 15:43:05 +0100

    :sparkles: Add -IncludeDev switch and use script to install packages in Travis

--- deployment/CheckRequirements.ps1
+++ deployment/CheckRequirements.ps1
@@ -1,6 +1,8 @@
 param (
   [Parameter(Mandatory = $false, HelpMessage = "If this is set, install any missing modules, otherwise warn.")]
-  [switch]$InstallMissing
+  [switch]$InstallMissing,
+  [Parameter(Mandatory = $false, HelpMessage = "If this is set, install any dev modules, otherwise ignore them.")]
+  [switch]$IncludeDev
 )
 
 Import-Module $PSScriptRoot/common/Logging -Force -ErrorAction Stop
@@ -25,6 +27,10 @@ $ModuleVersionRequired = @{
     "Microsoft.Graph"        = @("ge", "1.5.0")
     "Poshstache"             = @("ge", "0.1.10")
     "Powershell-Yaml"        = @("ge", "0.4.2")
+}
+if ($IncludeDev.IsPresent) {
+    $ModuleVersionRequired["Pester"] = ("ge", "5.1.0")
+    $ModuleVersionRequired["PSScriptAnalyzer"] = ("ge", "1.19.0")
 }
 
 # Powershell version
