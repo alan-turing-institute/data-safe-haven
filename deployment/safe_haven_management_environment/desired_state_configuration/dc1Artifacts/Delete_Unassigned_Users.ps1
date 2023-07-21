d051ac624df73862af3ed8f42d1cc0ef2d484565
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-07-21 14:20:36 +0100

    remove write-out

--- deployment/safe_haven_management_environment/desired_state_configuration/dc1Artifacts/Delete_Unassigned_Users.ps1
+++ deployment/safe_haven_management_environment/desired_state_configuration/dc1Artifacts/Delete_Unassigned_Users.ps1
@@ -1,6 +1,5 @@
 # Extract list of users
 # ---------------------
-Write-Output "Exporting user list..."
 $userOuPath = (Get-ADObject -Filter * | Where-Object { $_.Name -eq "Safe Haven Research Users" }).DistinguishedName
 $users = Get-ADUser -Filter * -SearchBase "$userOuPath" -Properties *
 foreach ($user in $users) {
@@ -10,7 +9,6 @@ foreach ($user in $users) {
 
 # Delete users not found in any group (with exception for named SG e.g. "Sandbox")
 # --------------------------------------------------------------------------------
-Write-Output "Deleting users not in any security group..."
 foreach ($user in $users) {
     if (!($user.GroupName)) {
         $name = $user.SamAccountName
