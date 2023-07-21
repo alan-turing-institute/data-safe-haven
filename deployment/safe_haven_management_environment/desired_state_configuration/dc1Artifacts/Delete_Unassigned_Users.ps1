e8f9cef209666968d20c03f6cf7e5031565972a3
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-07-21 14:22:39 +0100

    perform adsync after

--- deployment/safe_haven_management_environment/desired_state_configuration/dc1Artifacts/Delete_Unassigned_Users.ps1
+++ deployment/safe_haven_management_environment/desired_state_configuration/dc1Artifacts/Delete_Unassigned_Users.ps1
@@ -1,5 +1,4 @@
 # Extract list of users
-# ---------------------
 $userOuPath = (Get-ADObject -Filter * | Where-Object { $_.Name -eq "Safe Haven Research Users" }).DistinguishedName
 $users = Get-ADUser -Filter * -SearchBase "$userOuPath" -Properties *
 foreach ($user in $users) {
@@ -8,10 +7,22 @@ foreach ($user in $users) {
 }
 
 # Delete users not found in any group (with exception for named SG e.g. "Sandbox")
-# --------------------------------------------------------------------------------
 foreach ($user in $users) {
     if (!($user.GroupName)) {
         $name = $user.SamAccountName
         Remove-ADUser -Identity $name
     }
+}
+
+# Force sync with AzureAD. It will still take around 5 minutes for changes to propagate
+Write-Output "Synchronising locally Active Directory with Azure"
+try {
+    Import-Module -Name "C:\Program Files\Microsoft Azure AD Sync\Bin\ADSync" -ErrorAction Stop
+    Start-ADSyncSyncCycle -PolicyType Delta
+}
+catch [System.IO.FileNotFoundException] {
+    Write-Output "Skipping as Azure AD Sync is not installed"
+}
+catch {
+    Write-Output "Unable to run Azure Active Directory synchronisation!"
 }
