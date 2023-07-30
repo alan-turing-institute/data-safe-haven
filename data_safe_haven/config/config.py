28b7715ee3d85211ac25ada201ec3d6c29e87d48
James Robinson <james.em.robinson@gmail.com>
2023-07-30 23:21:45 +0100

    :truck: Factorised out common to_dict function

--- data_safe_haven/config/config.py
+++ data_safe_haven/config/config.py
@@ -1,5 +1,6 @@
 """Configuration file backed by blob storage"""
 import pathlib
+from abc import ABC, abstractmethod
 from collections import defaultdict
 from contextlib import suppress
 from dataclasses import dataclass, field
@@ -29,15 +30,27 @@ from data_safe_haven.utility import LoggingSingleton, SoftwarePackageCategory
 from .backend_settings import BackendSettings
 
 
+class ConfigSection(ABC):
+    def to_dict(self) -> dict[str, Any]:
+        """Dictionary representation of this object."""
+        self.validate()
+        return as_dict(chili.encode(self))
+
+    @abstractmethod
+    def validate(self) -> None:
+        """Validate instance attributes."""
+        pass
+
+
 @dataclass
-class ConfigSectionAzure:
+class ConfigSectionAzure(ConfigSection):
     admin_group_id: str = ""
     location: str = ""
     subscription_id: str = ""
     tenant_id: str = ""
 
     def validate(self) -> None:
-        """Validate input parameters"""
+        """Validate instance attributes."""
         try:
             validate_aad_guid(self.admin_group_id)
         except Exception as exc:
@@ -61,13 +74,9 @@ class ConfigSectionAzure:
             msg = f"Invalid value for 'tenant_id' ({self.tenant_id}).\n{exc}"
             raise ValueError(msg) from exc
 
-    def to_dict(self) -> dict[str, str]:
-        self.validate()
-        return as_dict(chili.encode(self))
-
 
 @dataclass
-class ConfigSectionBackend:
+class ConfigSectionBackend(ConfigSection):
     key_vault_name: str = ""
     managed_identity_name: str = ""
     resource_group_name: str = ""
@@ -75,7 +84,7 @@ class ConfigSectionBackend:
     storage_container_name: str = ""
 
     def validate(self) -> None:
-        """Validate input parameters"""
+        """Validate instance attributes."""
         if not self.key_vault_name:
             msg = f"Invalid value for 'key_vault_name' ({self.key_vault_name})."
             raise ValueError(msg)
@@ -94,31 +103,23 @@ class ConfigSectionBackend:
             msg = f"Invalid value for 'storage_container_name' ({self.storage_container_name})."
             raise ValueError(msg)
 
-    def to_dict(self) -> dict[str, str]:
-        self.validate()
-        return as_dict(chili.encode(self))
-
 
 @dataclass
-class ConfigSectionPulumi:
+class ConfigSectionPulumi(ConfigSection):
     encryption_key_id: str = ""
     encryption_key_name: str = "pulumi-encryption-key"
     stacks: dict[str, str] = field(default_factory=dict)
     storage_container_name: str = "pulumi"
 
     def validate(self) -> None:
-        """Validate input parameters"""
+        """Validate instance attributes."""
         if not isinstance(self.encryption_key_id, str) or not self.encryption_key_id:
             msg = f"Invalid value for 'encryption_key_id' ({self.encryption_key_id})."
             raise ValueError(msg)
 
-    def to_dict(self) -> dict[str, Any]:
-        self.validate()
-        return as_dict(chili.encode(self))
-
 
 @dataclass
-class ConfigSectionSHM:
+class ConfigSectionSHM(ConfigSection):
     aad_tenant_id: str = ""
     admin_email_address: str = ""
     admin_ip_addresses: list[str] = field(default_factory=list)
@@ -127,7 +128,7 @@ class ConfigSectionSHM:
     timezone: str = ""
 
     def validate(self) -> None:
-        """Validate input parameters"""
+        """Validate instance attributes."""
         try:
             validate_aad_guid(self.aad_tenant_id)
         except Exception as exc:
@@ -156,15 +157,11 @@ class ConfigSectionSHM:
             msg = f"Invalid value for 'timezone' ({self.timezone}).\n{exc}"
             raise ValueError(msg) from exc
 
-    def to_dict(self) -> dict[str, Any]:
-        self.validate()
-        return as_dict(chili.encode(self))
-
 
 @dataclass
-class ConfigSectionSRE:
+class ConfigSectionSRE(ConfigSection):
     @dataclass
-    class ConfigSectionRemoteDesktopOpts:
+    class ConfigSubsectionRemoteDesktopOpts:
         allow_copy: bool = False
         allow_paste: bool = False
 
@@ -199,12 +196,8 @@ class ConfigSectionSRE:
                 msg = f"Invalid value for 'allow_paste' ({self.allow_paste})."
                 raise ValueError(msg)
 
-        def to_dict(self) -> dict[str, bool]:
-            self.validate()
-            return as_dict(chili.encode(self))
-
     @dataclass
-    class ConfigSectionResearchDesktopOpts:
+    class ConfigSubsectionResearchDesktopOpts:
         sku: str = ""
 
         def validate(self) -> None:
@@ -215,19 +208,15 @@ class ConfigSectionSRE:
                 msg = f"Invalid value for 'sku' ({self.sku}).\n{exc}"
                 raise ValueError(msg) from exc
 
-        def to_dict(self) -> dict[str, str]:
-            self.validate()
-            return as_dict(chili.encode(self))
-
     data_provider_ip_addresses: list[str] = field(default_factory=list)
     index: int = 0
-    remote_desktop: ConfigSectionRemoteDesktopOpts = field(
-        default_factory=ConfigSectionRemoteDesktopOpts
+    remote_desktop: ConfigSubsectionRemoteDesktopOpts = field(
+        default_factory=ConfigSubsectionRemoteDesktopOpts
     )
     # NB. we cannot use defaultdict here until
     # https://github.com/python/cpython/pull/32056 is included in the Python
     # version we are using
-    research_desktops: dict[str, ConfigSectionResearchDesktopOpts] = field(
+    research_desktops: dict[str, ConfigSubsectionResearchDesktopOpts] = field(
         default_factory=dict
     )
     research_user_ip_addresses: list[str] = field(default_factory=list)
@@ -264,7 +253,7 @@ class ConfigSectionSRE:
                 for idx, vm_sku in enumerate(research_desktops):
                     self.research_desktops[
                         f"workspace-{idx:02d}"
-                    ] = ConfigSectionSRE.ConfigSectionResearchDesktopOpts(sku=vm_sku)
+                    ] = ConfigSectionSRE.ConfigSubsectionResearchDesktopOpts(sku=vm_sku)
         LoggingSingleton().info(
             f"[bold]Research desktops[/] will be [green]{list(self.research_desktops.keys())}[/]."
         )
@@ -288,7 +277,7 @@ class ConfigSectionSRE:
         )
 
     def validate(self) -> None:
-        """Validate input parameters"""
+        """Validate instance attributes."""
         try:
             for ip in self.data_provider_ip_addresses:
                 validate_ip_address(ip)
@@ -305,27 +294,19 @@ class ConfigSectionSRE:
             msg = f"Invalid value for 'research_user_ip_addresses' ({self.research_user_ip_addresses}).\n{exc}"
             raise ValueError(msg) from exc
 
-    def to_dict(self) -> dict[str, Any]:
-        self.validate()
-        return as_dict(chili.encode(self))
-
 
 @dataclass
-class ConfigSectionTags:
+class ConfigSectionTags(ConfigSection):
     deployment: str = ""
     deployed_by: str = "Python"
     project: str = "Data Safe Haven"
     version: str = __version__
 
     def validate(self) -> None:
-        """Validate input parameters"""
+        """Validate instance attributes."""
         if not self.deployment:
             msg = f"Invalid value for 'deployment' ({self.deployment})."
             raise ValueError(msg)
-
-    def to_dict(self) -> dict[str, str]:
-        self.validate()
-        return as_dict(chili.encode(self))
 
 
 class Config:
@@ -456,12 +437,6 @@ class Config:
             self.sres[name].index = highest_index + 1
         return self.sres[name]
 
-    def write_stack(self, name: str, path: pathlib.Path) -> None:
-        """Write a Pulumi stack file from config"""
-        pulumi_cfg = b64decode(self.pulumi.stacks[name])
-        with open(path, "w", encoding="utf-8") as f_stack:
-            f_stack.write(pulumi_cfg)
-
     def upload(self) -> None:
         """Upload config to Azure storage"""
         self.azure_api.upload_blob(
@@ -471,3 +446,9 @@ class Config:
             self.backend_storage_account_name,
             self.backend_storage_container_name,
         )
+
+    def write_stack(self, name: str, path: pathlib.Path) -> None:
+        """Write a Pulumi stack file from config"""
+        pulumi_cfg = b64decode(self.pulumi.stacks[name])
+        with open(path, "w", encoding="utf-8") as f_stack:
+            f_stack.write(pulumi_cfg)
