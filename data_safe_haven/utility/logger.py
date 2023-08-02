a88b046c663e83717a80b92869ecac37d48e2249
James Robinson <james.em.robinson@gmail.com>
2023-08-02 17:21:26 +0100

    :recycle: Explicitly remove handlers from NonLoggingSingleton

--- data_safe_haven/utility/logger.py
+++ data_safe_haven/utility/logger.py
@@ -224,5 +224,11 @@ class LoggingSingleton(logging.Logger, metaclass=Singleton):
 
 
 class NonLoggingSingleton(logging.Logger, metaclass=Singleton):
+    """
+    Non-logging singleton that can be used by anything needing logs to be consumed
+    """
+
     def __init__(self) -> None:
         super().__init__(name="non-logger", level=logging.CRITICAL + 10)
+        while self.handlers:
+            self.removeHandler(self.handlers[0])
