7e638e5beb70b3d237259ed59ac503d71ec83ff4
James Robinson <james.em.robinson@gmail.com>
2023-08-07 17:01:29 +0100

    :sparkles: Add backup policy for blobs

--- data_safe_haven/pulumi/components/sre_backup.py
+++ data_safe_haven/pulumi/components/sre_backup.py
@@ -24,7 +24,7 @@ class SREBackupComponent(ComponentResource):
         opts: ResourceOptions | None = None,
     ) -> None:
         super().__init__("dsh:sre:BackupComponent", name, {}, opts)
-        child_opts = ResourceOptions.merge(ResourceOptions(parent=self), opts)
+        child_opts = ResourceOptions.merge(opts, ResourceOptions(parent=self))
 
         # Deploy resource group
         resource_group = resources.ResourceGroup(
@@ -35,7 +35,7 @@ class SREBackupComponent(ComponentResource):
         )
 
         # Deploy backup vault
-        dataprotection.BackupVault(
+        backup_vault = dataprotection.BackupVault(
             f"{self._name}_backup_vault",
             identity=dataprotection.DppIdentityDetailsArgs(
                 type="SystemAssigned",
@@ -51,4 +51,39 @@ class SREBackupComponent(ComponentResource):
             ),
             resource_group_name=resource_group.name,
             vault_name=f"{stack_name}-bv-backup",
+            opts=child_opts,
+        )
+
+        # Backup policy for blobs
+        dataprotection.BackupPolicy(
+            f"{self._name}_backup_policy_blobs",
+            backup_policy_name="backup-policy-blobs",
+            properties=dataprotection.BackupPolicyArgs(
+                datasource_types=["Microsoft.Storage/storageAccounts/blobServices"],
+                object_type="BackupPolicy",
+                policy_rules=[
+                    dataprotection.AzureRetentionRuleArgs(
+                        is_default=True,
+                        lifecycles=[
+                            dataprotection.SourceLifeCycleArgs(
+                                delete_after=dataprotection.AbsoluteDeleteOptionArgs(
+                                    duration="P30D",  # 30 days
+                                    object_type="AbsoluteDeleteOption",
+                                ),
+                                source_data_store=dataprotection.DataStoreInfoBaseArgs(
+                                    data_store_type=dataprotection.DataStoreTypes.OPERATIONAL_STORE,
+                                    object_type="DataStoreInfoBase",
+                                ),
+                            ),
+                        ],
+                        name="Default",
+                        object_type="AzureRetentionRule",
+                    ),
+                ],
+            ),
+            resource_group_name=resource_group.name,
+            vault_name=backup_vault.name,
+            opts=ResourceOptions.merge(
+                child_opts, ResourceOptions(parent=backup_vault)
+            ),
         )
