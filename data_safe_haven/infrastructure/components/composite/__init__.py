77fbd039a686308e9741e05c34e88d7dba17d97a
James Robinson <james.em.robinson@gmail.com>
2023-08-29 13:45:01 +0100

    :sparkles: Add LocalDnsRecord component

--- data_safe_haven/infrastructure/components/composite/__init__.py
+++ data_safe_haven/infrastructure/components/composite/__init__.py
@@ -1,4 +1,5 @@
 from .automation_dsc_node import AutomationDscNode, AutomationDscNodeProps
+from .local_dns_record import LocalDnsRecordComponent, LocalDnsRecordProps
 from .postgresql_database import PostgresqlDatabaseComponent, PostgresqlDatabaseProps
 from .virtual_machine import LinuxVMComponentProps, VMComponent, WindowsVMComponentProps
 
@@ -6,6 +7,8 @@ __all__ = [
     "AutomationDscNode",
     "AutomationDscNodeProps",
     "LinuxVMComponentProps",
+    "LocalDnsRecordComponent",
+    "LocalDnsRecordProps",
     "PostgresqlDatabaseComponent",
     "PostgresqlDatabaseProps",
     "VMComponent",
