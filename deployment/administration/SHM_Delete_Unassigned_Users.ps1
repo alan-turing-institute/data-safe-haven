b9258dd31e60dd18da0da9baa0a099a1c5e7e5d8
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-08-29 11:52:46 +0100

    add dryrun param to local script

--- deployment/administration/SHM_Delete_Unassigned_Users.ps1
+++ deployment/administration/SHM_Delete_Unassigned_Users.ps1
@@ -1,6 +1,8 @@
 param(
     [Parameter(Mandatory = $true, HelpMessage = "Enter SHM ID (e.g. use 'testa' for Turing Development Safe Haven A)")]
-    [string]$shmId
+    [string]$shmId,
+    [Parameter(Mandatory = $false, HelpMessage = "Shows the users to be deleted without performing deletion")]
+    [bool]$dryRun
 )
 
 Import-Module Az.Accounts -ErrorAction Stop
@@ -16,10 +18,10 @@ $originalContext = Get-AzContext
 # Delete users not currently in a security group
 # ----------------------------------------------
 $null = Set-AzContext -SubscriptionId $config.subscriptionName -ErrorAction Stop
-Add-LogMessage -Level Info "Deleting users not assigned to any security group: $($config.shm.id) from $($config.dc.vmName)..."
+Add-LogMessage -Level Info "EDIT ME: Deleting users not assigned to any security group: $($config.shm.id) from $($config.dc.vmName)..."
 
 $script = "remote/Delete_Unassigned_Users.ps1"
 
-$result = Invoke-RemoteScript -Shell "PowerShell" -ScriptPath $script -VMName $config.dc.vmName -ResourceGroupName $config.dc.rg
+$result = Invoke-RemoteScript -Shell "PowerShell" -ScriptPath $script -VMName $config.dc.vmName -ResourceGroupName $config.dc.rg -Parameter @{"dryRun" = "$dryRun"}
 
 $null = Set-AzContext -Context $originalContext -ErrorAction Stop
