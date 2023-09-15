29983957baaf30f03fa9e43155bf47b113f40bd3
James Robinson <james.em.robinson@gmail.com>
2023-09-15 11:41:25 +0100

    :truck: Rename remote_powershell.py to remote_script.py

--- data_safe_haven/infrastructure/components/dynamic/__init__.py
+++ data_safe_haven/infrastructure/components/dynamic/__init__.py
@@ -2,7 +2,7 @@ from .azuread_application import AzureADApplication, AzureADApplicationProps
 from .blob_container_acl import BlobContainerAcl, BlobContainerAclProps
 from .compiled_dsc import CompiledDsc, CompiledDscProps
 from .file_share_file import FileShareFile, FileShareFileProps
-from .remote_powershell import RemoteScript, RemoteScriptProps
+from .remote_script import RemoteScript, RemoteScriptProps
 from .ssl_certificate import SSLCertificate, SSLCertificateProps
 
 __all__ = [
