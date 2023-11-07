f3acf7248e9563afda2af4f51c1db6d4d2890dc8
Jim Madge <jmadge@turing.ac.uk>
2023-11-07 09:53:53 +0000

    WIP: Use Pydantic for context settings

--- data_safe_haven/config/context_settings.py
+++ data_safe_haven/config/context_settings.py
@@ -4,12 +4,11 @@ from __future__ import (
     annotations,
 )
 
-from dataclasses import dataclass
 from pathlib import Path
-from typing import Any
+from typing import Any, ClassVar
 
 import yaml
-from schema import Schema, SchemaError
+from pydantic import BaseModel, Field, ValidationError
 from yaml.parser import ParserError
 
 from data_safe_haven.exceptions import (
@@ -23,15 +22,14 @@ def default_config_file_path() -> Path:
     return config_dir() / "contexts.yaml"
 
 
-@dataclass
-class Context:
+class Context(BaseModel):
     admin_group_id: str
     location: str
     name: str
     subscription_name: str
 
 
-class ContextSettings:
+class ContextSettings(BaseModel):
     """Load global and local settings from dotfiles with structure like the following
 
     selected: acme_deployment
@@ -44,47 +42,18 @@ class ContextSettings:
         ...
     """
 
-    def __init__(self, settings_dict: dict[Any, Any]) -> None:
-        self.logger = LoggingSingleton()
-
-        context_schema = Schema(
-            {
-                "name": str,
-                "admin_group_id": str,
-                "location": str,
-                "subscription_name": str,
-            }
-        )
-
-        schema = Schema(
-            {
-                "selected": str,
-                "contexts": Schema(
-                    {
-                        str: context_schema,
-                    }
-                ),
-            }
-        )
-
-        try:
-            self._settings: dict[Any, Any] = schema.validate(settings_dict)
-        except SchemaError as exc:
-            msg = f"Invalid context configuration file.\n{exc}"
-            raise DataSafeHavenParameterError(msg) from exc
-
-    @property
-    def settings(self) -> dict[Any, Any]:
-        return self._settings
+    selected_: str = Field(..., alias="selected")
+    contexts: dict[str, Context]
+    logger: ClassVar[LoggingSingleton] = LoggingSingleton()
 
     @property
     def selected(self) -> str:
-        return str(self.settings["selected"])
+        return str(self.selected_)
 
     @selected.setter
     def selected(self, context_name: str) -> None:
         if context_name in self.available:
-            self.settings["selected"] = context_name
+            self.selected_ = context_name
             self.logger.info(f"Switched context to '{context_name}'.")
         else:
             msg = f"Context '{context_name}' is not defined."
@@ -92,11 +61,11 @@ class ContextSettings:
 
     @property
     def context(self) -> Context:
-        return Context(**self.settings["contexts"][self.selected])
+        return self.contexts[self.selected]
 
     @property
     def available(self) -> list[str]:
-        return list(self.settings["contexts"].keys())
+        return list(self.contexts.keys())
 
     def update(
         self,
@@ -106,24 +75,24 @@ class ContextSettings:
         name: str | None = None,
         subscription_name: str | None = None,
     ) -> None:
-        context_dict = self.settings["contexts"][self.selected]
+        context = self.contexts[self.selected]
 
         if admin_group_id:
             self.logger.debug(
                 f"Updating '[green]{admin_group_id}[/]' to '{admin_group_id}'."
             )
-            context_dict["admin_group_id"] = admin_group_id
+            context.admin_group_id = admin_group_id
         if location:
             self.logger.debug(f"Updating '[green]{location}[/]' to '{location}'.")
-            context_dict["location"] = location
+            context.location = location
         if name:
             self.logger.debug(f"Updating '[green]{name}[/]' to '{name}'.")
-            context_dict["name"] = name
+            context.name = name
         if subscription_name:
             self.logger.debug(
                 f"Updating '[green]{subscription_name}[/]' to '{subscription_name}'."
             )
-            context_dict["subscription_name"] = subscription_name
+            context.subscription_name = subscription_name
 
     def add(
         self,
@@ -139,40 +108,53 @@ class ContextSettings:
             msg = f"A context with key '{key}' is already defined."
             raise DataSafeHavenParameterError(msg)
 
-        self.settings["contexts"][key] = {
-            "name": name,
-            "admin_group_id": admin_group_id,
-            "location": location,
-            "subscription_name": subscription_name,
-        }
+        self.contexts[key] = Context(
+            name=name,
+            admin_group_id=admin_group_id,
+            location=location,
+            subscription_name=subscription_name,
+        )
 
     def remove(self, key: str) -> None:
         if key not in self.available:
             msg = f"No context with key '{key}'."
             raise DataSafeHavenParameterError(msg)
-        del self.settings["contexts"][key]
+        del self.contexts[key]
+
+    @classmethod
+    def from_yaml(cls, settings_yaml: str) -> ContextSettings:
+        try:
+            settings_dict = yaml.safe_load(settings_yaml)
+        except ParserError as exc:
+            msg = f"Could not parse context settings as YAML.\n{exc}"
+            raise DataSafeHavenConfigError(msg) from exc
+
+        if not isinstance(settings_dict, dict):
+            msg = "Unable to parse context settings as a dict."
+            raise DataSafeHavenConfigError(msg)
+
+        try:
+            return ContextSettings.model_validate(settings_dict)
+        except ValidationError as exc:
+            cls.logger.error(f"{exc.error_count()} errors found in context settings:")
+            for error in exc.errors():
+                cls.logger.error(f"{error['msg']} at '{'->'.join(error['loc'])}'")
+            msg = f"Could not load context settings.\n{exc}"
+            raise DataSafeHavenConfigError(msg) from exc
 
     @classmethod
     def from_file(cls, config_file_path: Path | None = None) -> ContextSettings:
         if config_file_path is None:
             config_file_path = default_config_file_path()
-        logger = LoggingSingleton()
+        cls.logger.info(
+            f"Reading project settings from '[green]{config_file_path}[/]'."
+        )
         try:
             with open(config_file_path, encoding="utf-8") as f_yaml:
-                settings = yaml.safe_load(f_yaml)
-            if isinstance(settings, dict):
-                logger.info(
-                    f"Reading project settings from '[green]{config_file_path}[/]'."
-                )
-                return cls(settings)
-            else:
-                msg = f"Unable to parse {config_file_path} as a dict."
-                raise DataSafeHavenConfigError(msg)
+                settings_yaml = f_yaml.read()
+            return cls.from_yaml(settings_yaml)
         except FileNotFoundError as exc:
             msg = f"Could not find file {config_file_path}.\n{exc}"
-            raise DataSafeHavenConfigError(msg) from exc
-        except ParserError as exc:
-            msg = f"Could not load settings from {config_file_path}.\n{exc}"
             raise DataSafeHavenConfigError(msg) from exc
 
     def write(self, config_file_path: Path | None = None) -> None:
@@ -183,5 +165,5 @@ class ContextSettings:
         config_file_path.parent.mkdir(parents=True, exist_ok=True)
 
         with open(config_file_path, "w", encoding="utf-8") as f_yaml:
-            yaml.dump(self.settings, f_yaml, indent=2)
+            yaml.dump(self.model_dump(), f_yaml, indent=2)
         self.logger.info(f"Saved context settings to '[green]{config_file_path}[/]'.")
