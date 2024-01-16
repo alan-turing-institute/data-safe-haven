16dc315bfda75ba5935aa1403b65ec5e3208a498
Matt Craddock <5796417+craddm@users.noreply.github.com>
2024-01-16 16:45:06 +0000

    modify python script for updating dbeaver drivers

--- .github/scripts/update_dbeaver_drivers.py
+++ .github/scripts/update_dbeaver_drivers.py
@@ -4,34 +4,63 @@ from lxml import html
 from natsort import natsorted
 import requests
 
-output = {}
-remote_page = requests.get("https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/", allow_redirects=True)
-root = html.fromstring(remote_page.content)
-output["mssql_jdbc"] = natsorted([v for v in root.xpath("//a[contains(text(), 'jre8/')]/@href") if v != "../"])[-1].replace("/", "")
+def get_latest_version(url, search_text):
+    """
+    Get latest version number of a database driver from the Maven repository.
 
-remote_page = requests.get("https://repo1.maven.org/maven2/org/postgresql/pgjdbc-versions/", allow_redirects=True)
-root = html.fromstring(remote_page.content)
-output["pgjdbc"] = natsorted([v for v in root.xpath("//a[contains(text(), '/')]/@href") if v != "../"])[-1].replace("/", "")
+    Fetches the HTML page at the given URL, converts it to an lxml tree, and returns the latest version number of the driver.
 
-remote_page = requests.get("https://repo1.maven.org/maven2/org/postgresql/postgresql/", allow_redirects=True)
-root = html.fromstring(remote_page.content)
-output["postgresql"] = natsorted([v for v in root.xpath("//a[contains(text(), '/')]/@href") if v != "../"])[-1].replace("/", "")
+    Parameters
+    ----------
+    url : str
+        The URL of the Maven repository containing the driver
+    search_text : str
+        Text to search for in the repository, to distinguish the driver from other files
+    
+    Returns
+    -------
+    list
+        The latest available version number of the driver
+    """
 
-remote_page = requests.get("https://repo1.maven.org/maven2/net/postgis/postgis-jdbc/", allow_redirects=True)
-root = html.fromstring(remote_page.content)
-postgis_jdbc_versions = natsorted([v for v in root.xpath("//a[contains(text(), '/')]/@href") if v != "../"])
+    remote_page = requests.get(url, allow_redirects=True)
+    root = html.fromstring(remote_page.content)
+    return natsorted([v for v in root.xpath("//a[contains(text(), '" + search_text + "')]/@href") if v != "../"])[-1].replace("/", "")
 
-remote_page = requests.get("https://repo1.maven.org/maven2/net/postgis/postgis-geometry/", allow_redirects=True)
-root = html.fromstring(remote_page.content)
-postgis_geometry_versions = natsorted([v for v in root.xpath("//a[contains(text(), '/')]/@href") if v != "../"])
+drivers = [
+    {
+        'name': "mssql_jdbc",
+        'url': "https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/",
+        'search_text': "jre8/"
+    },
+    {
+        'name': "pgjdbc",
+        'url': "https://repo1.maven.org/maven2/org/postgresql/pgjdbc-versions/",
+        'search_text': "/"
+    },
+    {
+        'name': "postgresql",
+        'url': "https://repo1.maven.org/maven2/org/postgresql/postgresql/",
+        'search_text': "/"
+    },
+    {
+        'name': "postgis_geometry",
+        'url': "https://repo1.maven.org/maven2/net/postgis/postgis-geometry/",
+        'search_text': "/"
+    },
+    {
+        'name': "postgis_jdbc",
+        'url': "https://repo1.maven.org/maven2/net/postgis/postgis-jdbc/",
+        'search_text': "/"
+    },
+    {
+        'name': "waffle_jna",
+        'url': "https://repo1.maven.org/maven2/com/github/waffle/waffle-jna/",
+        'search_text': "/"
+    }
+]
 
-postgis = natsorted(set(postgis_jdbc_versions).intersection(set(postgis_geometry_versions)))[-1].replace("/", "")
-output["postgis_geometry"] = postgis
-output["postgis_jdbc"] = postgis
-
-remote_page = requests.get("https://repo1.maven.org/maven2/com/github/waffle/waffle-jna/", allow_redirects=True)
-root = html.fromstring(remote_page.content)
-output["waffle_jna"] = natsorted([v for v in root.xpath("//a[contains(text(), '/')]/@href") if v != "../"])[-1].replace("/", "")
+output = {driver['name']: get_latest_version(driver['url'], driver['search_text']) for driver in drivers}
 
 with open("deployment/secure_research_desktop/packages/dbeaver-driver-versions.json", "w") as f_out:
     f_out.writelines(json.dumps(output, indent=4, sort_keys=True))
