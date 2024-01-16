3a915b0a0805837f3de95e719d6e8b501670c513
Matt Craddock <5796417+craddm@users.noreply.github.com>
2024-01-16 16:53:26 +0000

    remove trailing whitespace

--- .github/scripts/update_dbeaver_drivers.py
+++ .github/scripts/update_dbeaver_drivers.py
@@ -10,7 +10,7 @@ def get_latest_version(url, search_text):
     Get latest version number of a database driver from the Maven repository.
 
     Fetches the HTML page at the given URL, then converts it to an lxml tree.
-    Numeric strings are then extracted. 
+    Numeric strings are then extracted.
     Note that mostly numeric strings for some drivers contain non-numeric text,
     as different driver types exist for those drivers, even where the version number is the same.
     The largest (latest) version number of the driver is then returned.
