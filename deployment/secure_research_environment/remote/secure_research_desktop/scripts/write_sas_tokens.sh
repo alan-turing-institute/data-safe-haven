005dfe13a1df52681ee424b057df712c09c30e53
Jim Madge <jmadge@turing.ac.uk>
2024-02-14 11:43:37 +0000

    Restart mount units

--- deployment/secure_research_environment/remote/secure_research_desktop/scripts/write_sas_tokens.sh
+++ deployment/secure_research_environment/remote/secure_research_desktop/scripts/write_sas_tokens.sh
@@ -3,3 +3,7 @@
 sed -i "s|^sasToken|sasToken ${BACKUP}|" /opt/configuration/credentials-backup.secret
 sed -i "s|^sasToken|sasToken ${EGRESS}|" /opt/configuration/credentials-egress.secret
 sed -i "s|^sasToken|sasToken ${INGRESS}|" /opt/configuration/credentials-ingress.secret
+
+systemd restart backup.mount
+systemd restart data.mount
+systemd restart output.mount
