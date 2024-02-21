51426b90ac2b38df8de43adb889bd0994bb4dc04
Jim Madge <jmadge@turing.ac.uk>
2024-02-21 09:50:57 +0000

    Decode base64 in sas tokens script

--- deployment/secure_research_environment/remote/secure_research_desktop/scripts/write_sas_tokens.sh
+++ deployment/secure_research_environment/remote/secure_research_desktop/scripts/write_sas_tokens.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env bash
 
+BACKUP=$(echo "${BACKUP_B64}" | base64 --decode)
+EGRESS=$(echo "${EGRESS_B64}" | base64 --decode)
+INGRESS=$(echo "${INGRESS_B64}" | base64 --decode)
+
 sed -i "s|^sasToken|sasToken ${BACKUP}|" /opt/configuration/credentials-backup.secret
 sed -i "s|^sasToken|sasToken ${EGRESS}|" /opt/configuration/credentials-egress.secret
 sed -i "s|^sasToken|sasToken ${INGRESS}|" /opt/configuration/credentials-ingress.secret
