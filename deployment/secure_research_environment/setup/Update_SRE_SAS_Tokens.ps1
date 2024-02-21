e9ea5389837cbf743f3e4605b1a76c6c61a8fe7e
Jim Madge <jmadge@turing.ac.uk>
2024-02-21 10:03:43 +0000

    Correct cmdlet name

--- deployment/secure_research_environment/setup/Update_SRE_SAS_Tokens.ps1
+++ deployment/secure_research_environment/setup/Update_SRE_SAS_Tokens.ps1
@@ -48,7 +48,7 @@ $VMs = Get-AzVM -ResourceGroupName $config.sre.srd.rg | `
 # Update blobfuse credentials on each SRD
 # ---------------------------------------
 $sasTokensBase64 = @{}
-$sasTokens.Keys | For-EachObject {
+$sasTokens.Keys | ForEach-Object {
     $sasTokensBase64[($_ + '_B64')] = ($sasTokens[$_] | ConvertTo-Base64)
 }
 $scriptPath = Join-Path $PSScriptRoot ".." "remote" "secure_research_desktop" "scripts" "write_sas_tokens.sh"
