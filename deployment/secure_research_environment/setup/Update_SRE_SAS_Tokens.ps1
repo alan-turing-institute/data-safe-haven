62b6f87fa3a4c2053ee8837f996d4550700ed4a2
Jim Madge <jmadge@turing.ac.uk>
2024-02-21 10:00:43 +0000

    Convert SAS tokens to base64 for remote script

--- deployment/secure_research_environment/setup/Update_SRE_SAS_Tokens.ps1
+++ deployment/secure_research_environment/setup/Update_SRE_SAS_Tokens.ps1
@@ -47,6 +47,10 @@ $VMs = Get-AzVM -ResourceGroupName $config.sre.srd.rg | `
 
 # Update blobfuse credentials on each SRD
 # ---------------------------------------
+$sasTokensBase64 = @{}
+$sasTokens.Keys | For-EachObject {
+    $sasTokensBase64[($_ + '_B64')] = ($sasTokens[$_] | ConvertTo-Base64)
+}
 $scriptPath = Join-Path $PSScriptRoot ".." "remote" "secure_research_desktop" "scripts" "write_sas_tokens.sh"
 foreach ($VM in $Vms) {
     $null = Invoke-RemoteScript -VMName $VM.Name -ResourceGroupName $VM.ResourceGroupName -Shell "UnixShell" -ScriptPath $scriptPath -Parameter $sasTokens
