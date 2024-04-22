0a0d8013d91a0854bb77cd933fe9e7990df4287f
Jim Madge <jmadge@turing.ac.uk>
2024-04-22 14:38:48 +0100

    Add ConfigClass

---
+++ data_safe_haven/config/config_class.py
@@ -0,0 +1,65 @@
+from typing import ClassVar
+
+import yaml
+from pydantic import BaseModel, ValidationError
+from yaml import YAMLError
+
+from data_safe_haven.config import Context
+from data_safe_haven.exceptions import (
+    DataSafeHavenConfigError,
+    DataSafeHavenParameterError,
+)
+from data_safe_haven.external import AzureApi
+
+
+class ConfigClass(BaseModel, validate_assignment=True):
+    """Base class for configuration that can be written to Azure storage"""
+
+    config_type: ClassVar[str] = "ConfigClass"
+    filename: ClassVar[str] = "config.yaml"
+
+    def to_yaml(self) -> str:
+        """Write configuration to a YAML formatted string"""
+        return yaml.dump(self.model_dump(mode="json"), indent=2)
+
+    def upload(self, context: Context) -> None:
+        """Upload configuration data to Azure storage"""
+        azure_api = AzureApi(subscription_name=context.subscription_name)
+        azure_api.upload_blob(
+            self.to_yaml(),
+            self.filename,
+            context.resource_group_name,
+            context.storage_account_name,
+            context.storage_container_name,
+        )
+
+    @classmethod
+    def from_yaml(cls, settings_yaml: str) -> BaseModel:
+        """Construct configuration from a YAML string"""
+        try:
+            settings_dict = yaml.safe_load(settings_yaml)
+        except YAMLError as exc:
+            msg = f"Could not parse {cls.config_type} configuration as YAML.\n{exc}"
+            raise DataSafeHavenConfigError(msg) from exc
+
+        if not isinstance(settings_dict, dict):
+            msg = f"Unable to parse {cls.config_type} configuration as a dict."
+            raise DataSafeHavenConfigError(msg)
+
+        try:
+            return cls.model_validate(settings_dict)
+        except ValidationError as exc:
+            msg = f"Could not load {cls.config_type} configuration.\n{exc}"
+            raise DataSafeHavenParameterError(msg) from exc
+
+    @classmethod
+    def from_remote(cls, context: Context) -> BaseModel:
+        """Construct configuration from a YAML file in Azure storage"""
+        azure_api = AzureApi(subscription_name=context.subscription_name)
+        config_yaml = azure_api.download_blob(
+            cls.filename,
+            context.resource_group_name,
+            context.storage_account_name,
+            context.storage_container_name,
+        )
+        return cls.from_yaml(config_yaml)
---
+++ tests/config/test_config_class.py
@@ -0,0 +1,111 @@
+from unittest.mock import patch
+
+from pytest import fixture, raises
+
+from data_safe_haven.config.config_class import ConfigClass
+from data_safe_haven.exceptions import (
+    DataSafeHavenConfigError,
+    DataSafeHavenParameterError,
+)
+from data_safe_haven.external import AzureApi
+
+
+class ExampleConfigClass(ConfigClass):
+    config_type = "Example"
+    filename = "file.yaml"
+    string: str
+    integer: int
+    list_of_integers: list[int]
+
+
+@fixture
+def example_config_class():
+    return ExampleConfigClass(string="hello", integer=5, list_of_integers=[1, 2, 3])
+
+
+@fixture
+def example_config_yaml():
+    return """string: 'abc'
+integer: -3
+list_of_integers: [-1,0,1]
+"""
+
+
+class TestConfigClass:
+    def test_constructor(self, example_config_class):
+        assert isinstance(example_config_class, ExampleConfigClass)
+        assert isinstance(example_config_class, ConfigClass)
+        assert example_config_class.string == "hello"
+
+    def test_to_yaml(self, example_config_class):
+        yaml = example_config_class.to_yaml()
+        assert isinstance(yaml, str)
+        assert "string: hello" in yaml
+        assert "integer: 5" in yaml
+        assert "config_type" not in yaml
+
+    def test_upload(self, example_config_class, context):
+        with patch.object(AzureApi, "upload_blob", return_value=None) as mock_method:
+            example_config_class.upload(context)
+
+        mock_method.assert_called_once_with(
+            example_config_class.to_yaml(),
+            "file.yaml",
+            context.resource_group_name,
+            context.storage_account_name,
+            context.storage_container_name,
+        )
+
+    def test_from_yaml(self, example_config_yaml):
+        example_config_class = ExampleConfigClass.from_yaml(example_config_yaml)
+        assert isinstance(example_config_class, ExampleConfigClass)
+        assert isinstance(example_config_class, ConfigClass)
+        assert example_config_class.string == "abc"
+        assert example_config_class.integer == -3
+        assert example_config_class.list_of_integers == [-1, 0, 1]
+
+    def test_from_yaml_invalid_yaml(self):
+        yaml = """string: 'abc'
+integer: -3
+list_of_integers: [-1,0,1
+"""
+        with raises(
+            DataSafeHavenConfigError,
+            match="Could not parse Example configuration as YAML.",
+        ):
+            ExampleConfigClass.from_yaml(yaml)
+
+    def test_from_yaml_not_dict(self):
+        yaml = """42"""
+        with raises(
+            DataSafeHavenConfigError,
+            match="Unable to parse Example configuration as a dict.",
+        ):
+            ExampleConfigClass.from_yaml(yaml)
+
+    def test_from_yaml_validation_error(self):
+        yaml = """string: 'abc'
+integer: 'not an integer'
+list_of_integers: [-1,0,1]
+"""
+        with raises(
+            DataSafeHavenParameterError,
+            match="Could not load Example configuration.",
+        ):
+            ExampleConfigClass.from_yaml(yaml)
+
+    def test_from_remote(self, context, example_config_yaml):
+        with patch.object(
+            AzureApi, "download_blob", return_value=example_config_yaml
+        ) as mock_method:
+            example_config = ExampleConfigClass.from_remote(context)
+
+        assert isinstance(example_config, ExampleConfigClass)
+        assert example_config.string == "abc"
+
+        mock_method.assert_called_once_with(
+            "file.yaml",
+            context.resource_group_name,
+            context.storage_account_name,
+            context.storage_container_name,
+        )
