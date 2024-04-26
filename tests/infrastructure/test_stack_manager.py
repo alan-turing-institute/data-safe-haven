af8e4e0e8599ec08bc5aae15bd8ae47a47f8d95a
Jim Madge <jmadge@turing.ac.uk>
2024-04-26 10:10:01 +0100

    Add CONFIG_PASSPHRASE env var for tests

--- tests/infrastructure/test_stack_manager.py
+++ tests/infrastructure/test_stack_manager.py
@@ -28,7 +28,9 @@ def mock_install_plugins(monkeypatch):
 @fixture
 def offline_pulumi_account(monkeypatch, mock_azure_cli_confirm):  # noqa: ARG001
     """Overwrite PulumiAccount so that it runs locally"""
-    monkeypatch.setattr(PulumiAccount, "env", {})
+    monkeypatch.setattr(
+        PulumiAccount, "env", {"PULUMI_CONFIG_PASSPHRASE": "passphrase"}
+    )
 
 
 @fixture
@@ -73,7 +75,6 @@ class TestSHMStackManager:
             workspace.secrets_provider
             == shm_stack_manager.context.pulumi_secrets_provider_url
         )
-        assert workspace.env_vars == {}
         config = stack.get_all_config()
         assert config["azure-native:location"].value == "uksouth"
         assert config["data-safe-haven:variable"].value == "5"
