ed497f2bb8ae8c367e429ada04d2fa7005a09cea
Jim Madge <jmadge@turing.ac.uk>
2024-05-15 14:16:28 +0100

    Fix linting

--- tests/commands/test_pulumi.py
+++ tests/commands/test_pulumi.py
@@ -7,11 +7,11 @@ class TestRun:
         runner,
         mock_config_from_remote,  # noqa: ARG002
         mock_pulumi_config_from_remote,  # noqa: ARG002
-        mock_azure_cli_confirm,  # noqa: ARG001
-        mock_install_plugins,  # noqa: ARG001
-        mock_key_vault_key,  # noqa: ARG001
-        offline_pulumi_account,  # noqa: ARG001
-        local_project_settings,  # noqa: ARG001
+        mock_azure_cli_confirm,  # noqa: ARG002
+        mock_install_plugins,  # noqa: ARG002
+        mock_key_vault_key,  # noqa: ARG002
+        offline_pulumi_account,  # noqa: ARG002
+        local_project_settings,  # noqa: ARG002
     ):
         result = runner.invoke(pulumi_command_group, ["shm", "stack ls"])
         assert result.exit_code == 0
@@ -22,11 +22,11 @@ class TestRun:
         runner,
         mock_config_from_remote,  # noqa: ARG002
         mock_pulumi_config_from_remote,  # noqa: ARG002
-        mock_azure_cli_confirm,  # noqa: ARG001
-        mock_install_plugins,  # noqa: ARG001
-        mock_key_vault_key,  # noqa: ARG001
-        offline_pulumi_account,  # noqa: ARG001
-        local_project_settings,  # noqa: ARG001
+        mock_azure_cli_confirm,  # noqa: ARG002
+        mock_install_plugins,  # noqa: ARG002
+        mock_key_vault_key,  # noqa: ARG002
+        offline_pulumi_account,  # noqa: ARG002
+        local_project_settings,  # noqa: ARG002
     ):
         result = runner.invoke(pulumi_command_group, ["shm", "not a pulumi command"])
         assert result.exit_code == 1
