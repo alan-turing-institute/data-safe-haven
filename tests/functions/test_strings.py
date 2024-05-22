3a03e33bbcdce6f37428d52d088beeba5c52483c
Jim Madge <jmadge@turing.ac.uk>
2024-05-22 12:15:24 +0100

    Parametrise next occurrence tests and add dst test

--- tests/functions/test_strings.py
+++ tests/functions/test_strings.py
@@ -7,46 +7,52 @@ from data_safe_haven.exceptions import DataSafeHavenInputError
 from data_safe_haven.functions import next_occurrence, sanitise_sre_name
 
 
-@freeze_time(datetime.datetime(2024, 1, 2, 1, 0, tzinfo=datetime.UTC))
-def test_next_occurrence():
-    next_time = next_occurrence(5, 13, "Australia/Perth")
-    assert next_time == "2024-01-02T21:13:00+00:00"
+class TestNextOccurance:
+    @pytest.mark.parametrize("hour,minute,timezone,expected", [
+        (5, 13, "Australia/Perth", "2024-01-02T21:13:00+00:00"),
+        (0, 13, "Australia/Perth", "2024-01-02T16:13:00+00:00"),
+        (20, 13, "Australia/Perth", "2024-01-02T12:13:00+00:00"),
+        (20, 13, "Europe/London", "2024-01-02T20:13:00+00:00"),
+    ])
+    @freeze_time(datetime.datetime(2024, 1, 2, 1, 0, tzinfo=datetime.UTC))
+    def test_next_occurrence(self, hour, minute, timezone, expected):
+        next_time = next_occurrence(hour, minute, timezone)
+        assert next_time == expected
 
+    @freeze_time(datetime.datetime(2024, 7, 2, 1, 0, tzinfo=datetime.UTC))
+    def test_dst(self):
+        next_time = next_occurrence(13, 5, "Europe/London")
+        assert next_time == "2024-07-02T12:05:00+00:00"
 
-@freeze_time(datetime.datetime(2024, 1, 2, 1, 0, tzinfo=datetime.UTC))
-def test_next_occurrence_timeformat():
-    next_time = next_occurrence(5, 13, "Australia/Perth", time_format="iso_minute")
-    assert next_time == "2024-01-02 21:13"
+    @freeze_time(datetime.datetime(2024, 1, 2, 1, 0, tzinfo=datetime.UTC))
+    def test_timeformat(self):
+        next_time = next_occurrence(5, 13, "Australia/Perth", time_format="iso_minute")
+        assert next_time == "2024-01-02 21:13"
 
+    @freeze_time(datetime.datetime(2024, 1, 2, 23, 0, tzinfo=datetime.UTC))
+    def test_is_tomorrow(self):
+        next_time = next_occurrence(5, 13, "Australia/Perth")
+        assert next_time == "2024-01-03T21:13:00+00:00"
 
-@freeze_time(datetime.datetime(2024, 1, 2, 23, 0, tzinfo=datetime.UTC))
-def test_next_occurrence_is_tomorrow():
-    next_time = next_occurrence(5, 13, "Australia/Perth")
-    assert next_time == "2024-01-03T21:13:00+00:00"
+    def test_invalid_hour(self):
+        with pytest.raises(DataSafeHavenInputError) as exc_info:
+            next_occurrence(99, 13, "Europe/London")
+        assert exc_info.match(r"Time '99:13' was not recognised.")
 
+    def test_invalid_minute(self):
+        with pytest.raises(DataSafeHavenInputError) as exc_info:
+            next_occurrence(5, 99, "Europe/London")
+        assert exc_info.match(r"Time '5:99' was not recognised.")
 
-def test_next_occurrence_invalid_hour():
-    with pytest.raises(DataSafeHavenInputError) as exc_info:
-        next_occurrence(99, 13, "Europe/London")
-    assert exc_info.match(r"Time '99:13' was not recognised.")
+    def test_invalid_timezone(self):
+        with pytest.raises(DataSafeHavenInputError) as exc_info:
+            next_occurrence(5, 13, "Mars/OlympusMons")
+        assert exc_info.match(r"Timezone 'Mars/OlympusMons' was not recognised.")
 
-
-def test_next_occurrence_invalid_minute():
-    with pytest.raises(DataSafeHavenInputError) as exc_info:
-        next_occurrence(5, 99, "Europe/London")
-    assert exc_info.match(r"Time '5:99' was not recognised.")
-
-
-def test_next_occurrence_invalid_timezone():
-    with pytest.raises(DataSafeHavenInputError) as exc_info:
-        next_occurrence(5, 13, "Mars/OlympusMons")
-    assert exc_info.match(r"Timezone 'Mars/OlympusMons' was not recognised.")
-
-
-def test_next_occurrence_invalid_timeformat():
-    with pytest.raises(DataSafeHavenInputError) as exc_info:
-        next_occurrence(5, 13, "Australia/Perth", time_format="invalid")
-    assert exc_info.match(r"Time format 'invalid' was not recognised.")
+    def test_invalid_timeformat(self):
+        with pytest.raises(DataSafeHavenInputError) as exc_info:
+            next_occurrence(5, 13, "Australia/Perth", time_format="invalid")
+        assert exc_info.match(r"Time format 'invalid' was not recognised.")
 
 
 @pytest.mark.parametrize(
