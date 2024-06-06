21d18ce0e0c171ed811cf33177016e9d5a6156c0
James Robinson <james.em.robinson@gmail.com>
2024-06-06 12:08:27 +0100

    :recycle: Convert SREIpRanges and SREDnsIpRanges to dataclasses

--- data_safe_haven/infrastructure/common/ip_ranges.py
+++ data_safe_haven/infrastructure/common/ip_ranges.py
@@ -1,34 +1,34 @@
 """Calculate SRE IP address ranges for a given SRE index"""
 
+from dataclasses import dataclass
+
 from data_safe_haven.external import AzureIPv4Range
 
 
+@dataclass(frozen=True)
 class SREIpRanges:
     """Calculate SRE IP address ranges for a given SRE index"""
 
-    def __init__(self) -> None:
-        """Constructor"""
-        self.vnet = AzureIPv4Range("10.0.0.0", "10.0.255.255")
-        self.application_gateway = self.vnet.next_subnet(256)
-        self.apt_proxy_server = self.vnet.next_subnet(8)
-        self.data_configuration = self.vnet.next_subnet(8)
-        self.data_private = self.vnet.next_subnet(8)
-        self.firewall = self.vnet.next_subnet(64)  # 64 address minimum
-        self.firewall_management = self.vnet.next_subnet(64)  # 64 address minimum
-        self.guacamole_containers = self.vnet.next_subnet(8)
-        self.guacamole_containers_support = self.vnet.next_subnet(8)
-        self.identity_containers = self.vnet.next_subnet(8)
-        self.monitoring = self.vnet.next_subnet(32)
-        self.user_services_containers = self.vnet.next_subnet(8)
-        self.user_services_containers_support = self.vnet.next_subnet(8)
-        self.user_services_databases = self.vnet.next_subnet(8)
-        self.user_services_software_repositories = self.vnet.next_subnet(8)
-        self.workspaces = self.vnet.next_subnet(256)
+    vnet = AzureIPv4Range("10.0.0.0", "10.0.255.255")
+    application_gateway = vnet.next_subnet(256)
+    apt_proxy_server = vnet.next_subnet(8)
+    data_configuration = vnet.next_subnet(8)
+    data_private = vnet.next_subnet(8)
+    firewall = vnet.next_subnet(64)  # 64 address minimum
+    firewall_management = vnet.next_subnet(64)  # 64 address minimum
+    guacamole_containers = vnet.next_subnet(8)
+    guacamole_containers_support = vnet.next_subnet(8)
+    identity_containers = vnet.next_subnet(8)
+    monitoring = vnet.next_subnet(32)
+    user_services_containers = vnet.next_subnet(8)
+    user_services_containers_support = vnet.next_subnet(8)
+    user_services_databases = vnet.next_subnet(8)
+    user_services_software_repositories = vnet.next_subnet(8)
+    workspaces = vnet.next_subnet(256)
 
 
+@dataclass(frozen=True)
 class SREDnsIpRanges:
     """Calculate SRE DNS IP address ranges."""
 
-    def __init__(self) -> None:
-        """Constructor"""
-        self.vnet = AzureIPv4Range("192.168.0.0", "192.168.0.7")
+    vnet = AzureIPv4Range("192.168.0.0", "192.168.0.7")
