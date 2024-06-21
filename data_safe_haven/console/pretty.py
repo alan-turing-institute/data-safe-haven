46c739395cc8624c785d3b3d320ad4801eea1c8e
Jim Madge <jmadge@turing.ac.uk>
2024-06-21 14:44:29 +0100

    Add pretty_print tests

--- data_safe_haven/console/pretty.py
+++ data_safe_haven/console/pretty.py
@@ -1,7 +1,6 @@
 from typing import Any
 
-from rich.console import Console, JustifyMethod, OverflowMethod
-from rich.style import Style
+from rich.console import Console
 
 console = Console()
 
@@ -9,34 +8,8 @@ console = Console()
 def pretty_print(
     *objects: Any,
     sep: str = " ",
-    end: str = "\n",
-    style: str | Style | None = None,
-    justify: JustifyMethod | None = None,
-    overflow: OverflowMethod | None = None,
-    no_wrap: bool | None = None,
-    emoji: bool | None = None,
-    markup: bool | None = None,
-    highlight: bool | None = None,
-    width: int | None = None,
-    height: int | None = None,
-    crop: bool = True,
-    soft_wrap: bool | None = None,
-    new_line_start: bool = False,
 ) -> None:
     console.print(
         *objects,
-        sep,
-        end,
-        style,
-        justify,
-        overflow,
-        no_wrap,
-        emoji,
-        markup,
-        highlight,
-        width,
-        height,
-        crop,
-        soft_wrap,
-        new_line_start,
+        sep=sep,
     )
