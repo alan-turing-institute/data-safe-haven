d6d1dff5fedfed1c7496cbae93fae9d85356aea2
Matt Craddock <5796417+craddm@users.noreply.github.com>
2024-06-25 16:00:34 +0000

    Change tests to check for TypeError

--- tests/config/test_sre_config.py
+++ tests/config/test_sre_config.py
@@ -9,7 +9,7 @@ from data_safe_haven.config.config_sections import (
 from data_safe_haven.config.sre_config import sre_config_name
 from data_safe_haven.context import Context
 from data_safe_haven.exceptions import (
-    DataSafeHavenParameterError,
+    DataSafeHavenTypeError,
 )
 from data_safe_haven.external import AzureApi
 from data_safe_haven.types import SoftwarePackageCategory
@@ -71,7 +71,7 @@ class TestConfig:
 
     def test_template_validation(self) -> None:
         config = SREConfig.template()
-        with pytest.raises(DataSafeHavenParameterError):
+        with pytest.raises(DataSafeHavenTypeError):
             SREConfig.from_yaml(config.to_yaml())
 
     def test_from_yaml(self, sre_config, sre_config_yaml) -> None:
