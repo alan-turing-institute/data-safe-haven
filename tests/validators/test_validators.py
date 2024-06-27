f578b96ad10fc18bd3988c0afe6767512f0b153c
James Robinson <james.em.robinson@gmail.com>
2024-06-27 15:13:53 +0100

    :coffin: Drop unused ConfigName type

--- tests/validators/test_validators.py
+++ tests/validators/test_validators.py
@@ -57,36 +57,6 @@ class TestAzureSubscriptionName:
             validators.azure_subscription_name(subscription_name)
 
 
-class TestValidateConfigName:
-    @pytest.mark.parametrize(
-        "config_name",
-        [
-            "valid-with-hyphens",
-            "valid with spaces",
-            "mIxeD CAse iNpuT",
-            "0123456789",
-        ],
-    )
-    def test_config_name(self, config_name):
-        assert validators.config_name(config_name) == config_name
-
-    @pytest.mark.parametrize(
-        "config_name",
-        [
-            " starts with space",
-            "ends with space ",
-            "-starts with hyphen",
-            "ends with hyphen-",
-        ],
-    )
-    def test_config_name_fail(self, config_name):
-        with pytest.raises(
-            ValueError,
-            match="DSH config names can only contain alphanumeric characters, spaces and hyphens.\nThey must start and end with alphanumeric characters.",
-        ):
-            validators.config_name(config_name)
-
-
 class TestValidateFqdn:
     @pytest.mark.parametrize(
         "fqdn",
