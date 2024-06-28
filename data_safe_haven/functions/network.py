8d4f9a8ae3101e3316c11079b3fd35b4ba55bfa6
James Robinson <james.em.robinson@gmail.com>
2024-06-28 16:40:29 +0100

    :rotating_light: Make 'as_cidr' argument keyword-only

--- data_safe_haven/functions/network.py
+++ data_safe_haven/functions/network.py
@@ -1,8 +1,10 @@
 import requests
 
 
-def current_ip_address(as_cidr: bool = False) -> str:
-    ip_address = requests.get("https://api.ipify.org", timeout=300).content.decode("utf8")
+def current_ip_address(*, as_cidr: bool = False) -> str:
+    ip_address = requests.get("https://api.ipify.org", timeout=300).content.decode(
+        "utf8"
+    )
     if as_cidr:
         ip_address += "/32"
     return ip_address
