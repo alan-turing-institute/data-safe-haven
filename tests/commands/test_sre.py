cb0544ed6a6fc3c57c64be790e1c73623e58457e
James Robinson <james.em.robinson@gmail.com>
2024-07-02 22:39:51 +0100

    :white_check_mark: Add some additional 'dsh sre deploy' tests

--- tests/commands/test_sre.py
+++ tests/commands/test_sre.py
@@ -17,3 +17,29 @@ class TestDeploySRE:
         assert result.exit_code == 1
         assert "mock deploy" in result.stdout
         assert "mock deploy error" in result.stdout
+
+    def test_no_context_file(self, runner_no_context_file):
+        result = runner_no_context_file.invoke(sre_command_group, ["deploy", "sandbox"])
+        assert result.exit_code == 1
+        assert "Could not find file" in result.stdout
+
+    def test_auth_failure(
+        self,
+        runner,
+        mock_azure_authenticator_login_exception,  # noqa: ARG002
+    ):
+        result = runner.invoke(sre_command_group, ["deploy", "sandbox"])
+        assert result.exit_code == 1
+        assert "mock login" in result.stdout
+        assert "mock login error" in result.stdout
+
+    def test_no_shm(
+        self,
+        capfd,
+        runner,
+        mock_shm_config_from_remote_fails,  # noqa: ARG002
+    ):
+        result = runner.invoke(sre_command_group, ["deploy", "sandbox"])
+        out, _ = capfd.readouterr()
+        assert result.exit_code == 1
+        assert "mock from_remote failure" in out
