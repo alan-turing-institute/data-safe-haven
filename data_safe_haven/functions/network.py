a96876df0f49b00dbebc53417f61535b2969dc21
James Robinson <james.em.robinson@gmail.com>
2024-07-02 13:44:27 +0100

    :bug: Allow for list to include CIDRs rather than just IP addresses

--- data_safe_haven/functions/network.py
+++ data_safe_haven/functions/network.py
@@ -6,7 +6,7 @@ import requests
 from data_safe_haven.exceptions import DataSafeHavenValueError
 
 
-def current_ip_address() -> str:
+def current_ip_address(*, as_cidr: bool = False) -> str:
     """
     Get the IP address of the current device.
 
@@ -20,6 +20,8 @@ def current_ip_address() -> str:
         ip_address = requests.get("https://api.ipify.org", timeout=300).content.decode(
             "utf8"
         )
+        if as_cidr:
+            return str(ipaddress.IPv4Network(ip_address))
         return ip_address
     except requests.RequestException as exc:
         msg = "Could not determine IP address."
@@ -36,7 +38,7 @@ def ip_address_in_list(ip_address_list: Sequence[str]) -> bool:
     Raises:
         DataSafeHavenValueError: if the current IP address could not be determined
     """
-    ip_address = current_ip_address()
-    if ip_address not in [str(ipaddress.IPv4Address(ip)) for ip in ip_address_list]:
+    ip_address = current_ip_address(as_cidr=True)
+    if ip_address not in [str(ipaddress.IPv4Network(ip)) for ip in ip_address_list]:
         return False
     return True
--- tests/functions/test_network.py
+++ tests/functions/test_network.py
@@ -6,12 +6,17 @@ from data_safe_haven.functions import current_ip_address, ip_address_in_list
 
 
 class TestCurrentIpAddress:
-    def test_current_ip_address(self, requests_mock):
+    def test_output(self, requests_mock):
         requests_mock.get("https://api.ipify.org", text="1.2.3.4")
         ip_address = current_ip_address()
         assert ip_address == "1.2.3.4"
 
-    def test_current_ip_address_not_resolved(self, requests_mock):
+    def test_output_as_cidr(self, requests_mock):
+        requests_mock.get("https://api.ipify.org", text="1.2.3.4")
+        ip_address = current_ip_address(as_cidr=True)
+        assert ip_address == "1.2.3.4/32"
+
+    def test_request_not_resolved(self, requests_mock):
         requests_mock.get(
             "https://api.ipify.org", exc=requests.exceptions.ConnectTimeout
         )
@@ -21,15 +26,19 @@ class TestCurrentIpAddress:
 
 
 class TestIpAddressInList:
-    def test_ip_address_is_in_list(self, requests_mock):
+    def test_is_in_list(self, requests_mock):
         requests_mock.get("https://api.ipify.org", text="1.2.3.4")
         assert ip_address_in_list(["1.2.3.4", "2.3.4.5"])
 
-    def test_ip_address_is_not_in_list(self, requests_mock):
+    def test_is_not_in_list(self, requests_mock):
         requests_mock.get("https://api.ipify.org", text="1.2.3.4")
         assert not ip_address_in_list(["2.3.4.5"])
 
-    def test_ip_address_not_resolved(self, requests_mock):
+    def test_is_in_cidr_list(self, requests_mock):
+        requests_mock.get("https://api.ipify.org", text="1.2.3.4")
+        assert ip_address_in_list(["1.2.3.4/32", "2.3.4.5/32"])
+
+    def test_not_resolved(self, requests_mock):
         requests_mock.get(
             "https://api.ipify.org", exc=requests.exceptions.ConnectTimeout
         )
