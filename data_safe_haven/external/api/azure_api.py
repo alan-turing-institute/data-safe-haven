6ca4b80f2067e8658843e19a1002e03ff5472d9c
James Robinson <james.em.robinson@gmail.com>
2024-07-03 17:30:20 +0100

    :recycle: Add get_subscription function to AzureApi

--- data_safe_haven/external/api/azure_api.py
+++ data_safe_haven/external/api/azure_api.py
@@ -7,6 +7,7 @@ from typing import Any, cast
 from azure.core.credentials import TokenCredential
 from azure.core.exceptions import (
     AzureError,
+    ClientAuthenticationError,
     HttpResponseError,
     ResourceExistsError,
     ResourceNotFoundError,
@@ -48,7 +49,7 @@ from azure.mgmt.msi.v2022_01_31_preview.models import Identity
 from azure.mgmt.resource.resources.v2021_04_01 import ResourceManagementClient
 from azure.mgmt.resource.resources.v2021_04_01.models import ResourceGroup
 from azure.mgmt.resource.subscriptions import SubscriptionClient
-from azure.mgmt.resource.subscriptions.models import Location
+from azure.mgmt.resource.subscriptions.models import Location, Subscription
 from azure.mgmt.storage.v2021_08_01 import StorageManagementClient
 from azure.mgmt.storage.v2021_08_01.models import (
     BlobContainer,
@@ -63,7 +64,11 @@ from azure.mgmt.storage.v2021_08_01.models import (
 from azure.storage.blob import BlobClient, BlobServiceClient
 from azure.storage.filedatalake import DataLakeServiceClient
 
-from data_safe_haven.exceptions import DataSafeHavenAzureError
+from data_safe_haven.exceptions import (
+    DataSafeHavenAzureAPIAuthenticationError,
+    DataSafeHavenAzureError,
+    DataSafeHavenValueError,
+)
 from data_safe_haven.external.interface.azure_authenticator import AzureAuthenticator
 from data_safe_haven.external.interface.credentials import AzureApiCredentialLoader
 from data_safe_haven.exceptions import (
@@ -71,35 +76,35 @@ from data_safe_haven.exceptions import (
 )
 from data_safe_haven.logging import get_logger
 
-from .azure_cli import AzureCliSingleton
-
 
 class AzureApi:
     """Interface to the Azure REST API"""
 
     def __init__(self, subscription_name: str) -> None:
         self.logger = get_logger()
+        self.authenticator = AzureApiCredentialLoader()
         self.subscription_name = subscription_name
         self.subscription_id_: str | None = None
         self.tenant_id_: str | None = None
 
     @property
     def credential(self) -> TokenCredential:
-        return AzureApiCredentialLoader().credential
+        return self.authenticator.credential
 
     @property
     def subscription_id(self) -> str:
         if not self.subscription_id_:
-            self.subscription_id_ = AzureCliSingleton().subscription_id(
-                self.subscription_name
+            self.subscription_id_ = str(
+                self.get_subscription(self.subscription_name).subscription_id
             )
         return self.subscription_id_
 
     @property
     def tenant_id(self) -> str:
         if not self.tenant_id_:
-            authenticator = AzureAuthenticator(self.subscription_name)
-            self.tenant_id_ = authenticator.tenant_id
+            self.tenant_id_ = str(
+                self.get_subscription(self.subscription_name).tenant_id
+            )
         return self.tenant_id_
 
     def blob_client(
@@ -712,6 +717,19 @@ class AzureApi:
         except AzureError as exc:
             msg = f"Keys could not be loaded for {msg_sa} in {msg_rg}."
             raise DataSafeHavenAzureError(msg) from exc
+
+    def get_subscription(self, subscription_name: str) -> Subscription:
+        """Get the current Azure subscription."""
+        try:
+            subscription_client = SubscriptionClient(self.credential)
+            for subscription in subscription_client.subscriptions.list():
+                if subscription.display_name == subscription_name:
+                    return subscription
+        except ClientAuthenticationError as exc:
+            msg = "Failed to authenticate with Azure API."
+            raise DataSafeHavenAzureAPIAuthenticationError(msg) from exc
+        msg = f"Could not find subscription '{subscription_name}'"
+        raise DataSafeHavenValueError(msg)
 
     def import_keyvault_certificate(
         self,
