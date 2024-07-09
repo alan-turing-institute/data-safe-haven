6ad75e8178c964306095d8c95a0115052a4a0122
James Robinson <james.em.robinson@gmail.com>
2024-07-09 16:41:23 +0100

    :white_check_mark: Add tests for GraphApiCredential

--- tests/external/api/conftest.py
+++ tests/external/api/conftest.py
@@ -1,32 +1,94 @@
+import os
+
 import jwt
 import pytest
 from azure.core.credentials import AccessToken
-from azure.identity import AzureCliCredential
-from pytest import fixture
+from azure.identity import (
+    AuthenticationRecord,
+    AzureCliCredential,
+    DeviceCodeCredential,
+)
+
+from data_safe_haven.external.api.credentials import GraphApiCredential
 
 
 def pytest_configure():
     """Define constants for use across multiple tests"""
+    pytest.user_upn = "username@example.com"
     pytest.user_id = "80b4ccfd-73ef-41b7-bb22-8ec268ec040b"
 
 
-@fixture
-def jwt_token():
+@pytest.fixture
+def authentication_record():
+    return AuthenticationRecord(
+        tenant_id=pytest.guid_tenant,
+        client_id="14d82eec-204b-4c2f-b7e8-296a70dab67e",
+        authority="login.microsoftonline.com",
+        home_account_id=pytest.user_id,
+        username=pytest.user_upn,
+    )
+
+
+@pytest.fixture
+def azure_cli_token():
     return jwt.encode(
         {
             "name": "username",
             "oid": pytest.user_id,
-            "upn": "username@example.com",
+            "upn": pytest.user_upn,
             "tid": pytest.guid_tenant,
         },
         "key",
     )
 
 
-@fixture
-def mock_azureclicredential_get_token(mocker, jwt_token):
+@pytest.fixture
+def graph_api_token():
+    return jwt.encode(
+        {
+            "scp": "GroupMember.Read.All User.Read.All",
+            "tid": pytest.guid_tenant,
+        },
+        "key",
+    )
+
+
+@pytest.fixture
+def mock_azureclicredential_get_token(mocker, azure_cli_token):
     mocker.patch.object(
         AzureCliCredential,
         "get_token",
-        return_value=AccessToken(jwt_token, 0),
+        return_value=AccessToken(azure_cli_token, 0),
     )
+
+
+@pytest.fixture
+def mock_devicecodecredential_get_token(mocker, graph_api_token):
+    mocker.patch.object(
+        DeviceCodeCredential,
+        "get_token",
+        return_value=AccessToken(graph_api_token, 0),
+    )
+
+
+@pytest.fixture
+def mock_devicecodecredential_authenticate(mocker, authentication_record):
+    mocker.patch.object(
+        DeviceCodeCredential,
+        "authenticate",
+        return_value=authentication_record,
+    )
+
+
+@pytest.fixture
+def mock_graphapicredential_get_token(mocker, graph_api_token):
+    mocker.patch.object(
+        GraphApiCredential,
+        "get_token",
+        return_value=AccessToken(graph_api_token, 0),
+    )
+
+
+@pytest.fixture
+def tmp_config_dir(mocker, tmp_path):
+    mocker.patch.dict(os.environ, {"DSH_CONFIG_DIRECTORY": str(tmp_path)})
--- tests/external/api/test_credentials.py
+++ tests/external/api/test_credentials.py
@@ -1,7 +1,10 @@
 import pytest
-from azure.identity import AzureCliCredential
+from azure.identity import AzureCliCredential, DeviceCodeCredential
 
-from data_safe_haven.external.api.credentials import AzureSdkCredential
+from data_safe_haven.external.api.credentials import (
+    AzureSdkCredential,
+    GraphApiCredential,
+)
 
 
 class TestAzureSdkCredential:
@@ -14,9 +17,36 @@ class TestAzureSdkCredential:
         assert isinstance(credential.token, str)
 
     def test_decode_token(self, mock_azureclicredential_get_token):  # noqa: ARG002
-        token = AzureSdkCredential().token
-        decoded = AzureSdkCredential.decode_token(token)
+        credential = AzureSdkCredential()
+        decoded = credential.decode_token(credential.token)
         assert decoded["name"] == "username"
         assert decoded["oid"] == pytest.user_id
         assert decoded["upn"] == "username@example.com"
+        assert decoded["tid"] == pytest.guid_tenant
+
+
+class TestGraphApiCredential:
+    def test_get_credential(
+        self,
+        mock_devicecodecredential_get_token,  # noqa: ARG002
+        mock_devicecodecredential_authenticate,  # noqa: ARG002
+        tmp_config_dir,  # noqa: ARG002
+    ):
+        credential = GraphApiCredential(pytest.guid_tenant)
+        assert isinstance(credential.get_credential(), DeviceCodeCredential)
+
+    def test_get_token(
+        self,
+        mock_graphapicredential_get_token,  # noqa: ARG002
+    ):
+        credential = GraphApiCredential(pytest.guid_tenant)
+        assert isinstance(credential.token, str)
+
+    def test_decode_token(
+        self,
+        mock_graphapicredential_get_token,  # noqa: ARG002
+    ):
+        credential = GraphApiCredential(pytest.guid_tenant)
+        decoded = credential.decode_token(credential.token)
+        assert decoded["scp"] == "GroupMember.Read.All User.Read.All"
         assert decoded["tid"] == pytest.guid_tenant
