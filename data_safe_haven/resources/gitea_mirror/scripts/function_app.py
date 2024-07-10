7f8fe8769c8c74106788e54564f1f82a4c55f16b
Jim Madge <jmadge@turing.ac.uk>
2024-07-10 15:08:35 +0100

    Rewrite mirror creation as function app

---
+++ data_safe_haven/resources/gitea_mirror/scripts/function_app.py
@@ -0,0 +1,107 @@
+import logging
+
+import azure.functions as func
+import requests
+from requests.auth import HTTPBasicAuth
+
+app = func.FunctionApp()
+
+
+@app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
+def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Request received.")
+
+    address = req.params.get("address")
+    name = req.params.get('name')
+    password = req.params.get("password")
+    username = req.params.get("username")
+    logging.info(f"parameters: address={address}, name={name}, password={password}, username={username}")
+
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        address = req_body.get('address')
+        name = req_body.get('name')
+        password = req_body.get('password')
+        username = req_body.get('username')
+    logging.info(f"parameters: address={address}, name={name}, password={password}, username={username}")
+
+    if (None in [address, name, password, username]):
+        msg = "Required parameter not provided."
+        logging.critical(msg)
+        return func.HttpResponse(
+            msg,
+            status_code=400,
+        )
+
+    # gitea_host = "http://gitea_mirror.local"
+    gitea_host = "http://localhost:3000"
+    api_root = "/api/v1"
+    migrate_path = "/repos/migrate"
+    repos_path = "/repos"
+    extra_data = {
+        "description": f"Read-only mirror of {address}",
+        "mirror": True,
+        "mirror_interval": "10m",
+    }
+    timeout = 60
+
+    auth = HTTPBasicAuth(
+        username=username,
+        password=password,
+    )
+
+    logging.info("Sending request to create mirror.")
+
+    response = requests.post(
+        auth=auth,
+        data={
+            "clone_addr": address,
+            "repo_name": name,
+        }
+        | extra_data,
+        timeout=timeout,
+        url=gitea_host + api_root + migrate_path,
+    )
+
+    logging.info(f"Response status code: {response.status_code}.")
+    logging.debug(f"Response contents: {response.json()}.")
+    if response.status_code != requests.codes.ok:
+        return func.HttpResponse(
+            "Error creating repository.",
+            status_code=400,
+        )
+
+    # Some arguments of the migrate endpoint seem to be ignored or overwritten.
+    # We set repository settings here.
+    logging.info("Sending request to configure mirror repo.")
+
+    response = requests.patch(
+        auth=auth,
+        data={
+            "has_actions": False,
+            "has_issues": False,
+            "has_packages": False,
+            "has_projects": False,
+            "has_pull_requests": False,
+            "has_releases": False,
+            "has_wiki": False,
+        },
+        timeout=timeout,
+        url=gitea_host + api_root + repos_path + f"/{username}/{name}",
+    )
+
+    logging.info(f"Response status code: {response.status_code}.")
+    logging.debug(f"Response contents: {response.json()}.")
+    if response.status_code != requests.codes.ok:
+        return func.HttpResponse(
+            "Error configuring repository.",
+            status_code=400,
+        )
+
+    return func.HttpResponse(
+        "Mirror successfully created",
+        status_code=200,
+    )
