c41196abbf523508de3325c71cadf47a67512f6b
Jim Madge <jmadge@turing.ac.uk>
2024-07-10 11:06:53 +0100

    Add description

--- data_safe_haven/resources/gitea_mirror/scripts/create_mirror.py
+++ data_safe_haven/resources/gitea_mirror/scripts/create_mirror.py
@@ -3,19 +3,6 @@ import argparse
 import requests
 from requests.auth import HTTPBasicAuth
 
-# gitea_host = "http://gitea_mirror.local"
-gitea_host = "http://localhost:3000"
-api_root = "/api/v1"
-migrate_path = "/repos/migrate"
-extra_data = {
-    "issues": False,
-    "mirror": False,
-    "mirror_interval": "600",
-    "pull_requests": False,
-    "releases": False,
-    "wiki": False,
-}
-
 parser = argparse.ArgumentParser()
 parser.add_argument(
     'username'
@@ -31,9 +18,27 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+# gitea_host = "http://gitea_mirror.local"
+gitea_host = "http://localhost:3000"
+api_root = "/api/v1"
+migrate_path = "/repos/migrate"
+extra_data = {
+    "description": f"Read-only mirror of {args.address}.",
+    "issues": False,
+    "mirror": False,
+    "mirror_interval": "0",
+    "pull_requests": False,
+    "releases": False,
+    "wiki": False,
+}
+
 auth = HTTPBasicAuth(
     username=args.username,
     password=args.password,
+)
+
+print(
+    {"clone_addr": args.address, "repo_name": args.name} | extra_data
 )
 
 response = requests.post(
