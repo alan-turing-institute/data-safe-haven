fb4127827fec9077031ee671c588f2e77c5ba88b
Jim Madge <jmadge@turing.ac.uk>
2024-07-11 15:17:10 +0100

    Add delete mirror test

--- tests/resources/gitea_mirror/functions/test_function_app.py
+++ tests/resources/gitea_mirror/functions/test_function_app.py
@@ -1,7 +1,10 @@
-# import azure.functions as func
+import json
+
+import azure.functions as func
 
 from data_safe_haven.resources.gitea_mirror.functions.function_app import (
-    str_or_none
+    str_or_none,
+    delete_mirror,
 )
 
 
@@ -9,3 +12,27 @@ class TestStrOrNone:
     def test_str_or_none(self):
         assert str_or_none("hello") == "hello"
         assert str_or_none(None) is None
+
+
+class TestDeleteMirror:
+    def test_delete_mirror(self, requests_mock):
+        req = func.HttpRequest(
+            method="POST",
+            body=json.dumps({
+                "name": "repo",
+                "owner": "admin",
+                "password": "password",
+                "username": "username",
+            }).encode(),
+            url="/api/delete-mirror",
+        )
+
+        function = delete_mirror.build().get_user_function()
+
+        requests_mock.delete(
+            "http://localhost:3000/api/v1/repos/admin/repo",
+            status_code=204,
+        )
+
+        response = function(req)
+        assert response.status_code == 200
