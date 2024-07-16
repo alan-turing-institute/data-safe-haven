0595e25e75c459b06decb297c708a1af775f757c
Jim Madge <jmadge@turing.ac.uk>
2024-07-16 13:51:17 +0100

    Update test

--- tests/infrastructure/programs/sre/test_workspaces.py
+++ tests/infrastructure/programs/sre/test_workspaces.py
@@ -6,14 +6,10 @@ from data_safe_haven.infrastructure.programs.sre.workspaces import (
 class TestTemplateCloudInit:
     def test_template_cloudinit(self):
         cloudinit = SREWorkspacesComponent.template_cloudinit(
-            container_desired_state_private_key="PRIVATE_KEY",
-            container_desired_state_name="container",
-            storage_account_data_configuration_name="storageaccount",
-            container_desired_state_local_user="user",
+            storage_account_data_desired_state_name="storageaccount",
         )
 
-        assert "content: |\n      PRIVATE_KEY" in cloudinit
         assert (
-            "scp -i /root/.ssh/desired_state_rsa -r container.storageaccount.user@container.blob.core.windows.net:ansible"
+            '- ["storageaccount.blob.core.windows.net:/storageaccount/desiredstate", /desired_state, nfs, "ro,'
             in cloudinit
         )
