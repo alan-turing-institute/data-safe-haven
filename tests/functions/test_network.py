9e40578ee04f346642e294669dfc5c43c461e6dd
James Robinson <james.em.robinson@gmail.com>
2024-08-02 15:27:45 +0100

    :white_check_mark: Add network test

--- tests/functions/test_network.py
+++ tests/functions/test_network.py
@@ -33,6 +33,10 @@ class TestIpAddressInList:
         requests_mock.get("https://api.ipify.org", text="1.2.3.4")
         assert ip_address_in_list(["1.2.3.4/32", "2.3.4.5/32"])
 
+    def test_is_in_non_trivial_cidr_list(self, requests_mock):
+        requests_mock.get("https://api.ipify.org", text="1.2.3.4")
+        assert ip_address_in_list(["1.2.3.0/29", "2.3.4.0/29"])
+
     def test_not_resolved(self, requests_mock):
         requests_mock.get(
             "https://api.ipify.org", exc=requests.exceptions.ConnectTimeout
