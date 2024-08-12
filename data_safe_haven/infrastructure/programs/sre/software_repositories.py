3266ab3d707463211eed84db968c74e080286a23
Jim Madge <jmadge@turing.ac.uk>
2024-08-12 14:10:55 +0100

    Specify caddy patch version

--- data_safe_haven/infrastructure/programs/sre/software_repositories.py
+++ data_safe_haven/infrastructure/programs/sre/software_repositories.py
@@ -161,7 +161,7 @@ class SRESoftwareRepositoriesComponent(ComponentResource):
                 container_group_name=f"{stack_name}-container-group-software-repositories",
                 containers=[
                     containerinstance.ContainerArgs(
-                        image="caddy:2.8",
+                        image="caddy:2.8.4",
                         name="caddy"[:63],
                         ports=[
                             containerinstance.ContainerPortArgs(
