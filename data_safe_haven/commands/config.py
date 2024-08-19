6409b6dcbbf43ea76fa6f3aa85a812f44d84e860
Matt Craddock <5796417+craddm@users.noreply.github.com>
2024-08-19 11:10:56 +0000

    Do not flag finding no configurations as an error

--- data_safe_haven/commands/config.py
+++ data_safe_haven/commands/config.py
@@ -78,7 +78,7 @@ def available() -> None:
         raise typer.Exit(1) from exc
     if not blobs:
         logger.critical(f"No configurations found for context '{context.name}'.")
-        raise typer.Exit(1)
+        raise typer.Exit(0)
     pulumi_config = DSHPulumiConfig.from_remote(context)
     sre_status = {}
     for blob in blobs:
