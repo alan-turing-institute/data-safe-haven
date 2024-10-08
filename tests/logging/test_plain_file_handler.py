1f3e751a04c3223b069f52fea7dc670c2634d79e
Jim Madge <jmadge@turing.ac.uk>
2024-10-08 15:16:10 +0100

    Add test for alternative escape char formats

--- tests/logging/test_plain_file_handler.py
+++ tests/logging/test_plain_file_handler.py
@@ -1,3 +1,5 @@
+import pytest
+
 from data_safe_haven.logging.plain_file_handler import PlainFileHandler
 
 
@@ -5,7 +7,9 @@ class TestPlainFileHandler:
     def test_strip_rich_formatting(self):
         assert PlainFileHandler.strip_rich_formatting("[green]Hello[/]") == "Hello"
 
-    def test_strip_ansi_escapes(self):
+    @pytest.mark.parametrize("escape", ["\033", "\x1B", "\u001b", "\x1B"])
+    def test_strip_ansi_escapes(self, escape):
         assert (
-            PlainFileHandler.strip_ansi_escapes("\033[31;1;4mHello\033[0m") == "Hello"
+            PlainFileHandler.strip_ansi_escapes(f"{escape}[31;1;4mHello{escape}[0m")
+            == "Hello"
         )
