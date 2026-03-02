b5f4b7f575f71aabb7bcaaf365cafb878d1a580b
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2026-03-02 11:25:15 +0000

    Fixing lint errors

--- data_safe_haven/healthcheck/test_container_instance.py
+++ data_safe_haven/healthcheck/test_container_instance.py
@@ -8,13 +8,13 @@ from .healthcheck_utils import BaseContainerInstanceTest
 
 class TestContainerInstance(BaseContainerInstanceTest):
 
-    @pytest.mark.parametrize("output_key", ["apt_proxy_server", "sre_clamav_mirror", "remote_desktop"])
+    @pytest.mark.parametrize(
+        "output_key", ["apt_proxy_server", "sre_clamav_mirror", "remote_desktop"]
+    )
     def test_container_state(
         self,
         output_key: str,
         project_manager: SREProjectManager,
         subscription_name: AzureSubscriptionName,
     ) -> None:
-        self.check_container_state(
-            output_key, project_manager, subscription_name
-        )
+        self.check_container_state(output_key, project_manager, subscription_name)
--- data_safe_haven/infrastructure/programs/sre/clamav_mirror.py
+++ data_safe_haven/infrastructure/programs/sre/clamav_mirror.py
@@ -186,4 +186,3 @@ class SREClamAVMirrorComponent(ComponentResource):
         }
 
         self.register_outputs({})
-
